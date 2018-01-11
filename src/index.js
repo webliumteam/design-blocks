2c12cdae680f7e2a5d7a5077f4c58ecc5ce4c5d9
zhtbcgtw <ivanov.i@weblium.com>
2018-01-11 12:21:42 +0200

    add mods

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-block/src/component'
+import Component from 'wireframe-master/src/component'
 import style from './style.css'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const ExtendedWireframe = hoistStatics(withProps({style}))(Component)
+const ExtendedWireframe = hoistStatics(withProps({style, mods: {showSubtitle: true}}))(Component)
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  title: 'Design block title'
+  title: 'Design block title',
 }
 
 export default ExtendedWireframe
