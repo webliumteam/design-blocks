3da2ea7a70c77f8dc286a5197dca76bb8d341a34
Roman Lichkovskyi <roman.l@weblium.com>
2018-01-17 18:10:36 +0200

    add profile save

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'
 
 const config = {
   apiKey: 'AIzaSyB38AxG5lKznD4BD0KyYfuv_9bzL0UfNmA',
@@ -6,21 +6,27 @@ const config = {
   databaseURL: 'https://weblium-test-90a10.firebaseio.com',
   projectId: 'weblium-test-90a10',
   storageBucket: 'weblium-test-90a10.appspot.com',
-  messagingSenderId: '57590192789'
-};
+  messagingSenderId: '57590192789',
+}
 
-const app = firebase.initializeApp(config);
+const app = firebase.initializeApp(config)
 
-const provider = new firebase.auth.FacebookAuthProvider();
-const userCountRef = firebase.database().ref().child('userCount');
+const provider = new firebase.auth.FacebookAuthProvider()
+const userCountRef = firebase.database().ref().child('count')
 
 const firebasLogin = () => firebase.auth()
   .signInWithPopup(provider)
-  .then(result => result.additionalUserInfo.isNewUser && userCountRef
-    .transaction(current_value => (current_value || 0) + 1))
-  .catch(error => console.log(error));
+  .then((result) => {
+    if (!result.additionalUserInfo.isNewUser) return
+
+    const {profile} = result.additionalUserInfo
+    const {uid} = result.user
+    firebase.database().ref(`users/${uid}`).set(profile)
+    userCountRef.transaction(value => (value || 0) + 1)
+  })
+  .catch(error => console.log(error))
 
 export {
   firebasLogin,
-  userCountRef
+  userCountRef,
 }
