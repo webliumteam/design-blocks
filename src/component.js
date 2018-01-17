dee51058993b75a17734f0b48517dd687e7fcc89
Samogray Olexandr <oleksandrs@weblium.com>
2018-01-17 21:15:05 +0200

    back

--- src/component.js
+++ src/component.js
@@ -63,7 +63,7 @@ class Block extends React.Component {
             <Text bind="text4" />
           </p>
         </div>
-        <button onClick={window.history.back} title="home" className={css['button-home']}>
+        <button onClick={() => window.history.back()} title="home" className={css['button-home']}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 459 459">
             <path d="M178.5 140.25v-102L0 216.75l178.5 178.5V290.7c127.5 0 216.75 40.8 280.5 130.05-25.5-127.5-102-255-280.5-280.5z" />
           </svg>
