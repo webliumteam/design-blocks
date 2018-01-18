18f114ffb7d55da5909a4a959ae7f50b6532c0ec
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-18 17:20:36 +0200

    upblish on welbium

--- src/index.js
+++ src/index.js
@@ -9,4 +9,124 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  title: 'Blog',
+  blog: [
+    {
+      id: '21a46d6e-0354-4a96-9af0-928ddae32095',
+      title: 'The two most important questions when scoping your MVP',
+      description:
+        'Imagine yourself 3 months from now. You’ve poured your life savings into bringing your app to life. It’s jam packed with all of these features that you just know your users are going to love. You’ve been working on traction while your team has been building the app so you get a steady stream of […]',
+      category: 'Creative Process',
+      date: 'November 22, 2017',
+      picture: {
+        src: 'img/blog-pic-1.jpg',
+        alt: 'Article illustration photo',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Read more',
+      },
+    },
+    {
+      id: '21a46d6e-0354-4a96-9af0-928ddae32095',
+      title: 'The two most important questions when scoping your MVP',
+      description:
+        'Imagine yourself 3 months from now. You’ve poured your life savings into bringing your app to life. It’s jam packed with all of these features that you just know your users are going to love. You’ve been working on traction while your team has been building the app so you get a steady stream of […]',
+      category: 'Creative Process',
+      date: 'November 22, 2017',
+      picture: {
+        src: 'img/blog-pic-2.jpg',
+        alt: 'Article illustration photo',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Read more',
+      },
+    },
+    {
+      id: '21a46d6e-0354-4a96-9af0-928ddae32095',
+      title: 'The two most important questions when scoping your MVP',
+      description:
+        'Imagine yourself 3 months from now. You’ve poured your life savings into bringing your app to life. It’s jam packed with all of these features that you just know your users are going to love. You’ve been working on traction while your team has been building the app so you get a steady stream of […]',
+      category: 'Creative Process',
+      date: 'November 22, 2017',
+      picture: {
+        src: 'img/blog-pic-3.jpg',
+        alt: 'Article illustration photo',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Read more',
+      },
+    },
+  ],
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+  {
+    id: 'post-publish-date',
+    type: 'checkbox',
+    label: 'Date of publishing',
+    defaultValue: true,
+  },
+  {
+    id: 'post-image',
+    type: 'checkbox',
+    label: 'Post image',
+    defaultValue: true,
+  },
+  {
+    id: 'post-description',
+    type: 'checkbox',
+    label: 'Post main text',
+    defaultValue: true,
+  },
+  {
+    id: 'post-link',
+    type: 'checkbox',
+    label: 'Post link',
+    defaultValue: true,
+  },
+  {
+    id: 'block-button',
+    type: 'checkbox',
+    label: 'Block button',
+    defaultValue: false,
+  },
+]
+
 export default ExtendedWireframe
