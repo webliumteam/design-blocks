d613ab3fa776cb41dd4365b539fae936aa1a6473
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-18 17:39:45 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -7,8 +7,89 @@ import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(
-  Component
-)
+const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
+
+ExtendedWireframe.defaultContent = {
+  theme: 'dark',
+  title: 'Type Something',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+    },
+    src: 'cover_bg.jpg',
+  },
+  subtitle: 'Type something',
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Main button (L)',
+  },
+  'cta-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Additional button (L)',
+  },
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Title description',
+    defaultValue: true,
+  },
+  {
+    id: 'primary-btn',
+    type: 'checkbox',
+    label: 'Primary button',
+    defaultValue: true,
+  },
+  {
+    id: 'secondary-btn',
+    type: 'checkbox',
+    label: 'Secondary button',
+    defaultValue: false,
+  },
+  {
+    type: 'separator',
+  },
+  {
+    id: 'align',
+    type: 'radio-button-group',
+    name: 'Aligning',
+    defaultValue: 'left',
+    children: [
+      {
+        id: 'left',
+        label: 'left',
+      },
+      {
+        id: 'center',
+        label: 'center',
+      },
+      {
+        id: 'right',
+        label: 'right',
+      },
+    ],
+  },
+]
 
 export default ExtendedWireframe
