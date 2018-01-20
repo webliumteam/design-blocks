ca65166725c984379a4e97e6f3e27700916994ca
archangel9800 <pain_s@mail.ru>
2018-01-20 12:12:27 +0200

    init d/zapdos/team

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-zapdos-team/src/component'
 import style from './style.css'
 
 import options from './options.json'
