8eb7cdde5b4b16dfba91a56b9db51d8509ddaedd
Mykola Melnychenko <mykola.melnichenko@gmail.com>
2018-01-20 14:51:56 +0200

    add optional subtitle (#20) (#30)
    * add optional subtitle
    
    * fix typo
    
    * change option into modification
    
    * add optional icon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import style from './style.css'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const Wireframe = hoistStatics(withProps({style}))(Component)
+const Wireframe = hoistStatics(withProps(_.pipe(_.set('style', style))))(Component)
 
 export default Wireframe
