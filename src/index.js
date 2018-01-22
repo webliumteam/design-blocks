892005e81ebea093a0417aafbe9940f300ae3d0e
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-22 19:38:31 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -7,8 +7,103 @@ import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(
-  Component
-)
+const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
+
+ExtendedWireframe.defaultContent = {
+  theme: 'dark',
+  logo: {
+    text: {
+      value: 'Company Name',
+    },
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'footer_bg.jpg',
+  },
+  subtitle: 'Company Slogan',
+  copyright: 'Produced by Weblium.com. All rights Reserved',
+  additional: 'Demo preview for {Template name} {Concept name}',
+  menu: [
+    {
+      id: '10c06b19-6e0d-4ad1-b1d5-213501dd321c',
+      metadata: {
+        displayName: 'About us',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: '29f03a3e-117d-490e-963c-ace78d85258f',
+      metadata: {
+        displayName: 'Services',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: '8914ae89-ca69-4b81-8d70-68fefdb22f49',
+      metadata: {
+        displayName: 'Projects',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: '2bf3b2aa-061b-42af-afd2-48301733a536',
+      metadata: {
+        displayName: 'Testimonials',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'a24bbd34-4a84-451d-82bf-929f35264e2d',
+      metadata: {
+        displayName: 'Contact us',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+}
 
 export default ExtendedWireframe
