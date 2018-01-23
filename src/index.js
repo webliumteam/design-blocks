724f77f5e1bddf64561b56407c416ab65e516394
max-sindi <sindm93@mail.ru>
2018-01-23 17:05:35 +0200

    init d/deino/projects

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-master/src/component'
+import Component from 'wireframe-zapdos-projects/src/component'
 import style from './style.css'
 
 import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(
-  Component
-)
+const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 export default ExtendedWireframe
