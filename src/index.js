d65c2fb7794fb5cee29e79412f7192a65f78fc50
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-23 13:25:04 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -7,8 +7,112 @@ import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(
-  Component
-)
+const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
+
+ExtendedWireframe.defaultContent = {
+  title: 'About the Company',
+  'text-1': 'Follow us:',
+  subtitle: 'We provide a wide range of services to meet even the most daring requirements',
+  text:
+    'Our team consists of highly motivated and skilled specialists who know how to deal with any issue that you may come across. This creates a basis for lasting relationships with our clients built on trust and mutual understanding. We are devoted to creating unique and innovative solutions along with the high-quality supporting services.',
+  picture: {
+    resourceRef: 'about_img.png',
+    alt: 'Picture about the company',
+  },
+  button: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn More',
+  },
+  link: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'More about us',
+  },
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'youtube',
+        name: 'YouTube',
+        url: 'http://youtube.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      padding: 20,
+      color: '',
+      sizes: [10, 20, 30, 40],
+      size: '40px',
+    },
+  },
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'text',
+    type: 'checkbox',
+    label: 'Company main text',
+    defaultValue: true,
+  },
+  {
+    id: 'link',
+    type: 'checkbox',
+    label: 'About us link',
+    defaultValue: false,
+  },
+  {
+    id: 'button',
+    type: 'checkbox',
+    label: 'Contact us button',
+    defaultValue: true,
+  },
+  {
+    id: 'socialIcons',
+    type: 'checkbox',
+    label: 'Social media buttons',
+    defaultValue: false,
+  },
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Subtitle',
+    defaultValue: true,
+  },
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+]
 
 export default ExtendedWireframe
