03e0c77bb541ca23a654827aad7eb21f3f3abcad
nmelnychenko <mykola.melnichenko@gmail.com>
2018-01-24 19:56:32 +0200

    add optional top icon

--- src/component.js
+++ src/component.js
@@ -50,11 +50,14 @@ class Block extends React.Component {
   }
 
   render() {
-    const {components: {Collection, Text, Button}, style, $block} = this.props
+    const {components: {Collection, Text, Button, Icon}, style, $block} = this.props
     return (
       <section className={style.section}>
         {console.log(this.getOptionValue('disable-icons'))}
         <div className={style.section__inner}>
+          {this.getModifierValue('top-icon') && (
+            <Icon className={style['top-icon']} bind="topIcon" />
+          )}
           {this.getModifierValue('title') && (
             <h1 className={style.title}>
               <Text bind="title" />
@@ -103,7 +106,7 @@ class Block extends React.Component {
   }
 }
 
-Block.components = _.pick(['Collection', 'Text', 'Button', 'Icon', 'Image'])($editor.components)
+Block.components = _.pick(['Collection', 'Text', 'Button', 'Icon', 'Image', 'Icon'])($editor.components)
 
 Block.defaultContent = {
   articles: [
@@ -182,6 +185,10 @@ Block.defaultContent = {
     },
     textValue: 'Medium button',
   },
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
 }
 
 Block.modifierScheme = [
@@ -237,6 +244,12 @@ Block.modifierScheme = [
     id: 'icon-replacer-image',
     type: 'hidden',
     label: 'Image placeholder',
+    defaultValue: false,
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
     defaultValue: false,
   },
 ]
