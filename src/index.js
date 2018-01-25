7e112f2067790352724c11c9719d81160b19abdb
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-25 15:13:18 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -9,4 +9,58 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  theme: 'dark',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    src: '33.jpg',
+  },
+  title: 'Want to work with us?',
+  description:
+    'Join 13000 clients who already worked with us. Request a quote to get best solutions in your area.',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Illustration photo',
+  },
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Title description',
+    defaultValue: true,
+  },
+  {
+    id: 'picture',
+    type: 'hidden',
+    label: 'Picture',
+    defaultValue: false,
+  },
+]
+
+
 export default ExtendedWireframe
