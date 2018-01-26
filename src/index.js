514bb4b1b0aef6c2de4156ea7bbc3727833998ff
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-26 15:00:42 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -9,4 +9,63 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Description',
+    defaultValue: true,
+  },
+  {
+    id: 'heading',
+    type: 'checkbox',
+    label: 'Title',
+    defaultValue: true,
+  },
+  {
+    id: 'subheading',
+    type: 'hidden',
+    label: 'Subtitle',
+    defaultValue: false,
+  },
+  {
+    id: 'text',
+    type: 'checkbox',
+    label: 'Main text',
+    defaultValue: true,
+  },
+  {
+    id: 'align',
+    type: 'radio-button-group',
+    name: 'Aligning',
+    defaultValue: 'center',
+    children: [
+      {
+        id: 'left',
+        label: 'left',
+      },
+      {
+        id: 'center',
+        label: 'center',
+      },
+      {
+        id: 'right',
+        label: 'right',
+      },
+    ],
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
+    defaultValue: false,
+  },
+]
+
 export default ExtendedWireframe
