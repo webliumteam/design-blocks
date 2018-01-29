fa1bf63aea503021d3f76e7e41e025ba8d3a1742
artemvasilkin <artemvasilkin14@gmail.com>
2018-01-29 18:09:24 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -9,4 +9,122 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  projects: [
+    {
+      id: 'e9ec34d7-3cc6-49f3-a9ad-6ea69f59409c',
+      title: 'Project 1',
+      subtitle: 'UX DESIGN',
+      text:
+        'Innovative digital solution for a marketing company. We worked on UX for the company website to make it more effective and increase the number of leads.',
+      picture: {
+        resourceRef: 'iphone.jpg',
+        alt: 'Project 1',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+      },
+    },
+    {
+      id: '9310272a-1fa2-4876-b1db-a879bbd4bc52',
+      title: 'Project 2',
+      subtitle: 'BRANDING',
+      text:
+        'Branding of legal firm. Our work on this project included creating a logo, motto, website, advertising strategy, and other details that help to build a well-known brand.',
+      picture: {
+        resourceRef: 'iphone.jpg',
+        alt: 'Project 2',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+      },
+    },
+  ],
+  topIcon: {
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    fill: 'red',
+  },
+  title: '-  Our Projects  -',
+  subtitle:
+    'Each month, we usefully close our projects. Here is the list of our recent ones:',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'All projects',
+  },
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'title',
+    type: 'checkbox',
+    label: 'Block title',
+    defaultValue: true,
+  },
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Title description',
+    defaultValue: true,
+  },
+  {
+    id: 'block-button',
+    type: 'checkbox',
+    label: 'Block button',
+    defaultValue: true,
+  },
+  {
+    id: 'project-category',
+    type: 'checkbox',
+    label: 'Project category',
+    defaultValue: true,
+  },
+  {
+    id: 'project-description',
+    type: 'checkbox',
+    label: 'Project description',
+    defaultValue: true,
+  },
+  {
+    id: 'project-button',
+    type: 'checkbox',
+    label: 'Project button',
+    defaultValue: true,
+  },
+  {
+    id: 'top-icon',
+    type: 'hidden',
+    label: 'Top icon decorator',
+    defaultValue: false,
+  },
+]
+
 export default ExtendedWireframe
