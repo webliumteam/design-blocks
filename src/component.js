965833bc6df5ac07ec528439372aa1d3aaad95b7
nmelnychenko <mykola.melnichenko@gmail.com>
2018-01-31 15:45:08 +0200

    add class for type link

--- src/component.js
+++ src/component.js
@@ -71,21 +71,15 @@ class Block extends React.Component {
             <div className={style['btns-group']}>
               {this.getModifierValue('button-secondary') && (
                 <Button
-                  className={classNames(
-                    style.button,
-                    style['button--secondary'],
-                    style['button--size-md'],
-                  )}
+                  buttonClassName={style.button}
+                  linkClassName={style.link}
                   bind="button-1"
                 />
               )}
               {this.getModifierValue('button-primary') && (
                 <Button
-                  className={classNames(
-                    style.button,
-                    style['button--primary'],
-                    style['button--size-md'],
-                  )}
+                  buttonClassName={style.button}
+                  linkClassName={style.link}
                   bind="button-2"
                 />
               )}
@@ -162,6 +156,7 @@ Block.defaultContent = {
       },
     },
     textValue: 'Medium button',
+    type: 'secondary',
   },
   'button-2': {
     actionConfig: {
@@ -175,6 +170,7 @@ Block.defaultContent = {
       },
     },
     textValue: 'Medium button',
+    type: 'primary',
   },
   topIcon: {
     svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
