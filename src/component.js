54f9089d58a616b7d45dc8f9e4abc0a58b41c4b5
nmelnychenko <mykola.melnichenko@gmail.com>
2018-01-31 19:40:31 +0200

    rewrite buttons and links

--- src/component.js
+++ src/component.js
@@ -34,11 +34,15 @@ class Block extends React.Component {
             </p>
           )}
           {_.get('member-link')(modifier) && (
-            <Button className={style.link} bind={`team[${index}].more`} />
+            <a href="#" className={style.link}>
+              <Text bind={`team[${index}].more`} />
+            </a>
           )}
           {_.get('member-email')(modifier) && (
             <div className={style['item__email-wrapper']}>
-              <Button className={style.item__email} bind={`team[${index}].email`} />
+              <a href="#" className={style.item__email}>
+                <Text bind={`team[${index}].email`} />
+              </a>
             </div>
           )}
           {_.get('member-social')(modifier) && (
@@ -76,7 +80,8 @@ class Block extends React.Component {
           {this.getModifierValue('block-button') && (
             <div className={style['btns-group']}>
               <Button
-                className={classNames(style.button, style['button--size-md'], style['button--secondary'])}
+                buttonClassName={style.button}
+                linkClassName={style.link}
                 bind="cta"
               />
             </div>
@@ -102,30 +107,8 @@ Block.defaultContent = {
         src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
         alt: 'Andrew Shimmer photo',
       },
-      more: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-      },
-      email: {
-        actionConfig: {
-          action: 'external',
-          actions: {
-            external: {
-              url: 'mailto:shimmer@gmail.com',
-            },
-          },
-        },
-        textValue: 'shimmer@gmail.com',
-      },
+      more: 'Learn more',
+      email: 'shimmer@gmail.com',
       socialIcons: {
         networks: [
           {
@@ -166,30 +149,8 @@ Block.defaultContent = {
         src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
         alt: 'Ann Maisner photo',
       },
-      more: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-      },
-      email: {
-        actionConfig: {
-          action: 'external',
-          actions: {
-            external: {
-              url: 'mailto:maisner@gmail.com',
-            },
-          },
-        },
-        textValue: 'maisner@gmail.com',
-      },
+      more: 'Learn more',
+      email: 'maisner@gmail.com',
       socialIcons: {
         networks: [
           {
@@ -230,30 +191,8 @@ Block.defaultContent = {
         src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
         alt: 'Tomas Abbar photo',
       },
-      more: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-      },
-      email: {
-        actionConfig: {
-          action: 'external',
-          actions: {
-            external: {
-              url: 'mailto:abbar@gmail.com',
-            },
-          },
-        },
-        textValue: 'abbar@gmail.com',
-      },
+      more: 'Learn more',
+      email: 'abbar@gmail.com',
       socialIcons: {
         networks: [
           {
@@ -300,6 +239,7 @@ Block.defaultContent = {
       },
     },
     textValue: 'Medium button',
+    type: 'secondary',
   },
   topIcon: {
     svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
