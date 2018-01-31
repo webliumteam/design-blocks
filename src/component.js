c7fa1a248bbc61227a941f5c514a3a447d3b9db2
nmelnychenko <mykola.melnichenko@gmail.com>
2018-01-31 19:24:37 +0200

    rewrite button

--- src/component.js
+++ src/component.js
@@ -38,7 +38,8 @@ class Block extends React.Component {
             this.getContent()
           }
           <Button
-            className={classNames(style.button, style['button--primary'], style['button--size-lg'])}
+            buttonClassName={style.button}
+            linkClassName={style.link}
             bind="cta"
           />
           {this.getModifierValue('picture') && (
@@ -74,6 +75,8 @@ Block.defaultContent = {
       },
     },
     textValue: 'Request a quote',
+    type: 'primary',
+    size: 'lg',
   },
   picture: {
     src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
