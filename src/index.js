18be0603addbc875992388fd0c05d601fea262f8
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-02 14:23:05 +0200

    init.cp

--- src/index.js
+++ src/index.js
@@ -9,4 +9,99 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  theme: 'dark',
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    src: 'cover-bg.jpg',
+  },
+  title: 'Quantum Company',
+  subtitle: 'We can execute even the most unpredictable ideas! ',
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+    size: 'lg',
+  },
+  'cta-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'lg',
+  },
+  topCaption: 'welcome',
+}
+
+ExtendedWireframe.modifierScheme = [
+  {
+    id: 'subtitle',
+    type: 'checkbox',
+    label: 'Title description',
+    defaultValue: true,
+  },
+  {
+    id: 'primary-btn',
+    type: 'checkbox',
+    label: 'Button',
+    defaultValue: true,
+  },
+  {
+    id: 'secondary-btn',
+    type: 'hidden',
+    label: 'Additional button',
+    defaultValue: false,
+  },
+  {
+    type: 'separator',
+  },
+  {
+    id: 'align',
+    type: 'radio-button-group',
+    name: 'Aligning',
+    defaultValue: 'left',
+    children: [
+      {
+        id: 'left',
+        label: 'left',
+      },
+      {
+        id: 'center',
+        label: 'center',
+      },
+      {
+        id: 'right',
+        label: 'right',
+      },
+    ],
+  },
+  {
+    id: 'caption-decorator',
+    type: 'hidden',
+    label: 'Pretitle',
+    defaultValue: false,
+  },
+]
+
 export default ExtendedWireframe
