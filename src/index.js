c40051824f47b693d97c1ce9d93b47f33ea6a73e
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-05 18:42:38 +0200

    content fix

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
   theme: 'light',
   background: {
     type: 'color',
@@ -35,25 +36,6 @@ ExtendedWireframe.defaultContent = {
       id: '84957801-e554-42e1-ab7b-323f483e3f81',
     },
   ],
-  topIcon: {
-    svg:
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
-  },
-  title: 'Our Results in Numbers',
-  cta: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Medium button',
-  },
 }
 
 export default ExtendedWireframe
