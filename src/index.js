bb5e777a5e0a0de1f25c6f1757fe54ff889a7788
zhtbcgtw <ivanov.i@weblium.com>
2018-02-06 16:55:15 +0200

    replace modifierScheme array with object

--- src/index.js
+++ src/index.js
@@ -9,63 +9,25 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
-ExtendedWireframe.modifierScheme = [
-  {
-    id: 'title',
-    type: 'checkbox',
-    label: 'Block title',
-    defaultValue: true,
-  },
-  {
-    id: 'subtitle',
-    type: 'checkbox',
-    label: 'Description',
-    defaultValue: true,
-  },
-  {
-    id: 'heading',
-    type: 'checkbox',
-    label: 'Title',
-    defaultValue: true,
-  },
-  {
-    id: 'subheading',
-    type: 'hidden',
-    label: 'Subtitle',
-    defaultValue: false,
-  },
-  {
-    id: 'text',
-    type: 'checkbox',
-    label: 'Main text',
-    defaultValue: true,
-  },
-  {
-    id: 'align',
-    type: 'radio-button-group',
-    name: 'Aligning',
-    defaultValue: 'center',
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  align: {
     children: [
-      {
-        id: 'left',
-        label: 'left',
-      },
-      {
-        id: 'center',
-        label: 'center',
-      },
-      {
-        id: 'right',
-        label: 'right',
-      },
+      {id: 'left', label: 'left'},
+      {id: 'center', label: 'center'},
+      {id: 'right', label: 'right'},
     ],
+    defaultValue: 'center',
+    name: 'Aligning',
+    type: 'radio-button-group',
   },
-  {
-    id: 'top-icon',
-    type: 'hidden',
-    label: 'Top icon decorator',
-    defaultValue: false,
-  },
-]
+  heading: {defaultValue: true, label: 'Title', type: 'checkbox'},
+  subheading: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
+  subtitle: {defaultValue: true, label: 'Description', type: 'checkbox'},
+  text: {defaultValue: true, label: 'Main text', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
+}
+
 
 export default ExtendedWireframe
