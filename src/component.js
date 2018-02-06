e747062e056c7de6b48670063a7e4b012cd99c2b
zhtbcgtw <ivanov.i@weblium.com>
2018-02-06 17:13:00 +0200

    replace modifierScheme array with object

--- src/component.js
+++ src/component.js
@@ -162,43 +162,14 @@ Block.defaultContent = {
   },
 }
 
-Block.modifierScheme = [
-  {
-    id: 'subtitle',
-    type: 'checkbox',
-    label: 'Careers description',
-    defaultValue: true,
-  },
-  {
-    id: 'image',
-    type: 'checkbox',
-    label: 'Сareers photo',
-    defaultValue: true,
-  },
-  {
-    id: 'body',
-    type: 'checkbox',
-    label: 'Careers main text',
-    defaultValue: true,
-  },
-  {
-    id: 'icon',
-    type: 'checkbox',
-    label: 'Careers icon',
-    defaultValue: true,
-  },
-  {
-    id: 'button',
-    type: 'checkbox',
-    label: 'Button',
-    defaultValue: true,
-  },
-  {
-    id: 'top-icon',
-    type: 'hidden',
-    label: 'Top icon decorator',
-    defaultValue: false,
-  },
-]
+Block.modifierScheme = {
+  body: {defaultValue: true, label: 'Careers main text', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  icon: {defaultValue: true, label: 'Careers icon', type: 'checkbox'},
+  image: {defaultValue: true, label: 'Сareers photo', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Careers description', type: 'checkbox'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
+}
+
 
 export default Block
