66fb5b9203b508ab759ae1bb3f84a082369e418f
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-07 13:54:20 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -9,4 +9,58 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    src: 'about.jpg',
+  },
+  theme: 'dark',
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 20,
+      color: '#fff',
+      sizes: [10, 20, 30, 40],
+      size: '32px',
+    },
+  },
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  'article-picture': {defaultValue: false, label: 'Article picture', type: 'hidden'},
+  socialIcons: {defaultValue: true, label: 'Social media buttons', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
+}
+
 export default ExtendedWireframe
