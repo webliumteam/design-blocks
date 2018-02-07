2c921bf481f88ca100d4ba8b7a5b1575288d983b
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-07 12:44:36 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -9,4 +9,33 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#ffffff',
+  },
+  title: '- FAQ -',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'link',
+  },
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  'secondary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'FAQ description', type: 'checkbox'},
+}
+
 export default ExtendedWireframe
