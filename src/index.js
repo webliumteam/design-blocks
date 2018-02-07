9286f0950c99bb90a11fc065a1ef4eb97c45054e
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-07 12:10:29 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -9,4 +9,27 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  title: '- Our process -',
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'primary',
+  },
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+}
+
 export default ExtendedWireframe
