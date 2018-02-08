23d14677f70b359670a2cce005569982b15b44f0
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-08 13:59:41 +0200

    fix ui-kit & link

--- src/index.js
+++ src/index.js
@@ -10,20 +10,21 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
   title: 'Blog',
   blog: [
     {
       id: '21a46d6e-0354-4a96-9af0-928ddae32095',
-      title: 'The two most important questions when scoping your MVP',
+      title: 'How to Hire Only Professional and Dedicated Employees? Useful Tips from Our HR Director.',
       description:
-        'Imagine yourself 3 months from now. You’ve poured your life savings into bringing your app to life. It’s jam packed with all of these features that you just know your users are going to love. You’ve been working on traction while your team has been building the app so you get a steady stream of […]',
+        'Our HR Director shares his experience how to fill positions with the best candidates, where to find talents, and how to attract professionals to your business. ',
       category: 'Creative Process',
-      date: 'November 22, 2017',
+      date: 'September 22, 2017',
       picture: {
-        resourceRef: 'blog-pic-1.jpg',
+        src: 'blog-pic-1.jpg',
         alt: 'Article illustration photo',
       },
-      cta: {
+      link: {
         actionConfig: {
           action: 'link',
           actions: {
@@ -34,21 +35,22 @@ ExtendedWireframe.defaultContent = {
             },
           },
         },
-        textValue: 'Read more',
+        textValue: 'Learn more',
+        type: 'link',
       },
     },
     {
-      id: '21a46d6e-0354-4a96-9af0-928ddae32095',
-      title: 'The two most important questions when scoping your MVP',
+      id: '41db0c4f-46b3-4835-8e77-062de63d018a',
+      title: 'How to Achieve Higher Profits in Retail with Our New Product?',
       description:
-        'Imagine yourself 3 months from now. You’ve poured your life savings into bringing your app to life. It’s jam packed with all of these features that you just know your users are going to love. You’ve been working on traction while your team has been building the app so you get a steady stream of […]',
+        'Do you want to achieve higher profits this year? Our new product will help you get what you want. In this article, you will find out how to use it to get more benefits.',
       category: 'Creative Process',
-      date: 'November 22, 2017',
+      date: 'September 22, 2017',
       picture: {
-        resourceRef: 'blog-pic-2.jpg',
+        src: 'blog-pic-2.jpg',
         alt: 'Article illustration photo',
       },
-      cta: {
+      link: {
         actionConfig: {
           action: 'link',
           actions: {
@@ -59,21 +61,22 @@ ExtendedWireframe.defaultContent = {
             },
           },
         },
-        textValue: 'Read more',
+        textValue: 'Learn more',
+        type: 'link',
       },
     },
     {
-      id: '21a46d6e-0354-4a96-9af0-928ddae32095',
-      title: 'The two most important questions when scoping your MVP',
+      id: 'fff4b459-6e4f-4645-901e-4c1b1aa94e51',
+      title: 'Top 5 Tips to Improve Your Engineering Department.',
       description:
-        'Imagine yourself 3 months from now. You’ve poured your life savings into bringing your app to life. It’s jam packed with all of these features that you just know your users are going to love. You’ve been working on traction while your team has been building the app so you get a steady stream of […]',
+        'You engineers can bring you better results! Get to know how to improve engineering department to make a new step for your company growth. ',
       category: 'Creative Process',
-      date: 'November 22, 2017',
+      date: 'September 22, 2017',
       picture: {
-        resourceRef: 'blog-pic-3.jpg',
+        src: 'blog-pic-3.jpg',
         alt: 'Article illustration photo',
       },
-      cta: {
+      link: {
         actionConfig: {
           action: 'link',
           actions: {
@@ -84,28 +87,11 @@ ExtendedWireframe.defaultContent = {
             },
           },
         },
-        textValue: 'Read more',
+        textValue: 'Learn more',
+        type: 'link',
       },
     },
   ],
-  topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
-  },
-  subtitle: 'Subtitle here',
-  cta: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'All blog posts',
-  },
 }
 
 ExtendedWireframe.modifierScheme = {
