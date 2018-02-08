b6fc6bb540358aa783351f4ab4754c62d14eda71
JulBeet <julbeet@gmail.com>
2018-02-08 15:20:29 +0200

    Design markup added

--- src/index.js
+++ src/index.js
@@ -11,10 +11,121 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'Depositphotos_10627386_original.jpg',
+  },
+  theme: 'dark',
+  cover: [
+    {
+      title: 'Yoga for Everyone',
+      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+      id: 'c4e3de5f-776a-405b-a034-dc2307069714',
+    },
+    {
+      title: 'Yoga for Everyone',
+      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+      id: '73263c9a-8d0e-41dc-85e1-66c6db9ea9d3',
+    },
+    {
+      title: 'Yoga for Everyone',
+      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+      id: 'b5f64fb3-d94e-4ecf-b7ff-a4a5700a2457',
+    },
+    {
+      title: 'Yoga for Everyone',
+      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+      id: '73263c9a-8d0e-41dc-85e1-66c6db9ea9d3',
+    },
+    {
+      title: 'Yoga for Everyone',
+      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Main button (L)',
+        type: 'primary',
+      },
+      id: 'b5f64fb3-d94e-4ecf-b7ff-a4a5700a2457',
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  arrows: {defaultValue: false, label: 'Navigation arrows', type: 'checkbox'},
+  dots: {defaultValue: true, label: 'Navigation indicators', type: 'checkbox'},
+  'primary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
+
 
 export default ExtendedWireframe
