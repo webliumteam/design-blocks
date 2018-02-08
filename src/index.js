0064ea173a6e27409ef8f87595bbd5d65655e4c6
Igor Dejak <i.dejak@betinvest.com>
2018-02-08 16:47:08 +0200

    quote for deino

--- src/index.js
+++ src/index.js
@@ -11,10 +11,28 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'quote_bg.jpg',
+  },
+  blockquote:
+    'We must let go of the life we have planned, so as to accept the one that is waiting for us.',
+  author: 'Joseph Campbell',
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  author: {
+    id: 'author',
+    type: 'checkbox',
+    label: 'Quote author',
+    defaultValue: true,
+  },
 }
 
 export default ExtendedWireframe
