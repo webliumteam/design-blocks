174217c364966ca209a760b6d14584c033b6da04
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-08 19:04:19 +0200

    init.co

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ ExtendedWireframe.defaultContent = {
   theme: 'dark',
   cover: [
     {
-      title: 'Yoga for Everyone',
-      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      title: 'Quantum Company',
+      subtitle: 'We implement Innovative Projects',
       cta: {
         actionConfig: {
           action: 'link',
@@ -35,14 +35,15 @@ ExtendedWireframe.defaultContent = {
             },
           },
         },
-        textValue: 'Main button (L)',
+        textValue: 'Request a quote',
         type: 'primary',
+        size: 'lg',
       },
       id: 'c4e3de5f-776a-405b-a034-dc2307069714',
     },
     {
-      title: 'Yoga for Everyone',
-      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      title: 'Quantum Company',
+      subtitle: 'We implement Innovative Projects',
       cta: {
         actionConfig: {
           action: 'link',
@@ -54,14 +55,15 @@ ExtendedWireframe.defaultContent = {
             },
           },
         },
-        textValue: 'Main button (L)',
+        textValue: 'Request a quote',
         type: 'primary',
+        size: 'lg',
       },
       id: '73263c9a-8d0e-41dc-85e1-66c6db9ea9d3',
     },
     {
-      title: 'Yoga for Everyone',
-      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
+      title: 'Quantum Company',
+      subtitle: 'We implement Innovative Projects',
       cta: {
         actionConfig: {
           action: 'link',
@@ -73,46 +75,9 @@ ExtendedWireframe.defaultContent = {
             },
           },
         },
-        textValue: 'Main button (L)',
+        textValue: 'Request a quote',
         type: 'primary',
-      },
-      id: 'b5f64fb3-d94e-4ecf-b7ff-a4a5700a2457',
-    },
-    {
-      title: 'Yoga for Everyone',
-      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Main button (L)',
-        type: 'primary',
-      },
-      id: '73263c9a-8d0e-41dc-85e1-66c6db9ea9d3',
-    },
-    {
-      title: 'Yoga for Everyone',
-      subtitle: 'Mind & Body Harmony Start a Healthy Lifestyle',
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Main button (L)',
-        type: 'primary',
+        size: 'lg',
       },
       id: 'b5f64fb3-d94e-4ecf-b7ff-a4a5700a2457',
     },
@@ -121,7 +86,7 @@ ExtendedWireframe.defaultContent = {
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  arrows: {defaultValue: false, label: 'Navigation arrows', type: 'checkbox'},
+  arrows: {defaultValue: false, label: 'Navigation arrows', type: 'hidden'},
   dots: {defaultValue: true, label: 'Navigation indicators', type: 'checkbox'},
   'primary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
   subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
