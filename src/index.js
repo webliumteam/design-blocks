bc7af1c4b55f6b2a8fa9f873bc6d0f35f06b5630
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-02-08 11:54:34 +0200

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,8 +11,134 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  theme: 'dark',
+  menu: [
+    {
+      id: 'home',
+      metadata: {
+        displayName: 'Home',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'sports',
+      metadata: {
+        displayName: 'Sports',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'concerts',
+      metadata: {
+        displayName: 'Concerts',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'theater',
+      metadata: {
+        displayName: 'Theater',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'music',
+      metadata: {
+        displayName: 'Music',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'contacts',
+      metadata: {
+        displayName: 'Contacts',
+        actionConfig: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+  logo: {
+    text: {
+      value: 'TSO',
+      tagName: 'h2',
+    },
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Find tickets online',
+    type: 'secondary',
+    size: 'sm',
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
 }
 
-ExtendedWireframe.modifierScheme = []
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  logo: {defaultValue: true, label: 'Company name', type: 'checkbox'},
+}
 
 export default ExtendedWireframe
