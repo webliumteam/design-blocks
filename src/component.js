5d48b6d998954c082c647070de02730c79dac8a6
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-14 17:20:58 +0200

    rewrite Text elements

--- src/component.js
+++ src/component.js
@@ -23,32 +23,12 @@ class Block extends React.Component {
             <Icon className={style['top-icon']} bind="topIcon" />
           )}
           <header className={style.section__header}>
-            {this.getModifierValue('title') && (
-              <h1 className={style.title}>
-                <Text bind="title" />
-              </h1>
-            )}
-            {this.getModifierValue('subtitle') && (
-              <p className={style.subtitle}>
-                <Text bind="subtitle" />
-              </p>
-            )}
+            {this.getModifierValue('title') && <Text tagName="h1" className={style.title} bind="title" />}
+            {this.getModifierValue('subtitle') && <Text tagName="p" className={style.subtitle} bind="subtitle" />}
           </header>
-          {this.getModifierValue('heading') && (
-            <h2 className={style.heading}>
-              <Text bind="heading" />
-            </h2>
-          )}
-          {this.getModifierValue('subheading') && (
-            <p className={style.subheading}>
-              <Text bind="subheading" />
-            </p>
-          )}
-          {this.getModifierValue('text') && (
-            <p className={style.text}>
-              <Text bind="text" />
-            </p>
-          )}
+          {this.getModifierValue('heading') && <Text tagName="h2" className={style.heading} bind="heading" />}
+          {this.getModifierValue('subheading') && <Text tagName="p" className={style.subheading} bind="subheading" />}
+          {this.getModifierValue('text') && <Text tagName="p" className={style.text} bind="text" />}
         </div>
       </section>
     )
@@ -63,14 +43,26 @@ Block.defaultContent = {
       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
     fill: 'red',
   },
-  title: 'Our Values',
-  heading: 'High Standards',
-  subtitle:
-    'We are a customer-oriented company, and our values drive us to achieve more.',
-  subheading:
-    'We always aim to establish long-term relationships with customers. That’s why we believe that quality always comes first. This notion is the cornerstone of our mission, solidified by the highest standards we apply in every operation.',
-  text:
-    'We always work hard to improve our goods and services to achieve higher results for our clients. We love challenging tasks that require an innovative approach. Thus, if your business needs a unique solution, we are ready to fine-tune our existing products to meet all your demands. Our company is a group of devotees sharing mutual priorities and values. We know that only those committed to their work can provide the best-quality products and services. That’s why every person that wants to join our team should undergo rigorous selection.',
+  title: {
+    content: 'Our Values',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'We are a customer-oriented company, and our values drive us to achieve more.',
+    type: 'subtitle',
+  },
+  heading: {
+    content: 'High Standards',
+    type: 'heading',
+  },
+  subheading: {
+    content: 'We always aim to establish long-term relationships with customers. That’s why we believe that quality always comes first. This notion is the cornerstone of our mission, solidified by the highest standards we apply in every operation.',
+    type: 'subheading',
+  },
+  text: {
+    content: 'We always work hard to improve our goods and services to achieve higher results for our clients. We love challenging tasks that require an innovative approach. Thus, if your business needs a unique solution, we are ready to fine-tune our existing products to meet all your demands. Our company is a group of devotees sharing mutual priorities and values. We know that only those committed to their work can provide the best-quality products and services. That’s why every person that wants to join our team should undergo rigorous selection.',
+    type: 'text',
+  },
 }
 
 Block.modifierScheme = {
