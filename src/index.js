da157123acdc67558fb9e61d875abcc2ae1847d9
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-15 12:36:36 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -12,70 +12,13 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  title: 'About the Company',
-  'text-1': 'Follow us:',
-  subtitle: 'We provide a wide range of services to meet even the most daring requirements',
-  text:
-    'Our team consists of highly motivated and skilled specialists who know how to deal with any issue that you may come across. This creates a basis for lasting relationships with our clients built on trust and mutual understanding. We are devoted to creating unique and innovative solutions along with the high-quality supporting services.',
   picture: {
     resourceRef: 'about_img.png',
     alt: 'Picture about the company',
   },
-  button: {
+  'button-1': {
+    ...Component.defaultContent['button-1'],
     type: 'secondary-alt',
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn More',
-  },
-  link: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'More about us',
-  },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
   },
 }
 
@@ -85,7 +28,7 @@ ExtendedWireframe.modifierScheme = {
   button: {defaultValue: true, label: 'Contact us button', type: 'checkbox'},
   link: {defaultValue: false, label: 'About us link', type: 'hidden'},
   socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'hidden'},
-  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
   text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
   title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
