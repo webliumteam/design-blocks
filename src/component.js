f04f995faf1d01dbcf1780ec2f77005c8ab700af
efimenko bohdan <bogdane@weblium.com>
2018-02-15 14:50:59 +0200

    change text component

--- src/component.js
+++ src/component.js
@@ -19,13 +19,11 @@ class Block extends React.Component {
             pictureClassName={style.item__picture}
             imgClassName={style.item__image}
             bind={`awards[${index}].picture`}
-            size={{'min-width: 320px': 450, 'min-width: 480px': 358}}
+            size={{'min-width: 480px': 358, 'min-width: 320px': 450}}
           />
         </div>
         {showHeading && (
-          <p className={style.item__title}>
-            <Text bind={`awards[${index}].title`} />
-          </p>
+          <Text bind={`awards[${index}].title`} className={style.item__title} tagName="p" />
         )}
       </article>
     )
@@ -41,14 +39,10 @@ class Block extends React.Component {
             <Icon className={style['top-icon']} bind="topIcon" />
           )}
           {this.getModifierValue('title') && (
-            <h1 className={style.title}>
-              <Text bind="title" />
-            </h1>
+            <Text bind="title" className={style.title} tagName="h1" />
           )}
           {this.getModifierValue('subtitle') && (
-            <p className={style.subtitle}>
-              <Text bind="subtitle" />
-            </p>
+            <Text bind="subtitle" className={style.subtitle} tagName="p" />
           )}
           <Collection
             className={style['items-wrapper']}
@@ -78,35 +72,39 @@ Block.components = _.pick(['Collection', 'Text', 'Button', 'Image', 'Icon'])($ed
 Block.defaultContent = {
   awards: [
     {
-      id: '8130928a-e7c6-4e26-a3ca-a820fa36380e',
-      title: 'R&D Leader in Tech (2017)',
+      title: {
+        content: 'R&D Leader in Tech (2017)',
+        type: 'heading',
+      },
       picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Award illustration photo',
+        alt: 'R&D Leader in Tech photo',
       },
     },
     {
-      id: '057c27bf-4977-4b28-bf35-f4a6db60d5aa',
-      title: 'Most Innovative B2B Solutions (2016)',
+      title: {
+        content: 'Most Innovative B2B Solutions (2016)',
+        type: 'heading',
+      },
       picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Award illustration photo',
+        alt: 'Most Innovative B2B Solutions photo',
       },
     },
     {
-      id: 'c8e93041-896b-4b78-b8d0-37e21a79070f',
-      title: 'Best Customer-Focused Approach on the World Conference (2014)',
+      title: {
+        content: 'Best Customer-Focused Approach on the World Conference (2014)',
+        type: 'heading',
+      },
       picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Award illustration photo',
+        alt: 'Best Customer-Focused Approach on the World Conference photo',
       },
     },
     {
-      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d',
-      title: 'Leader of Innovation (2012)',
+      title: {
+        content: 'Leader of Innovation (2012)',
+        type: 'heading',
+      },
       picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Award illustration photo',
+        alt: 'Leader of Innovation photo',
       },
     },
   ],
@@ -114,9 +112,14 @@ Block.defaultContent = {
     svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
     fill: 'red',
   },
-  title: 'Awards',
-  subtitle:
-    'The excellence of our services has been proved by the following awards:',
+  title: {
+    content: 'Awards',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'The excellence of our services has been proved by the following awards:',
+    type: 'subtitle',
+  },
   cta: {
     actionConfig: {
       action: 'link',
