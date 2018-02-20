e3c3b86373a22a13ba40d91ae823d170a9a24ea8
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-20 18:24:12 +0200

    finish first iteration

--- src/index.js
+++ src/index.js
@@ -9,4 +9,35 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  picture: {
+    resourceRef: 'img_pic.jpg',
+    alt: 'Illustration',
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'careers-bg.jpg',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary-alt',
+    size: 'md',
+  },
+}
+
 export default ExtendedWireframe
