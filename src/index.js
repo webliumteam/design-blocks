5f28e5be12bb17a2c9809614e94327daaa352e33
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-02-20 13:56:16 +0200

    change design text content

--- src/index.js
+++ src/index.js
@@ -11,10 +11,121 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  blog: [
+    {
+      title: {
+        content: 'How to Hire Professional and Dedicated Employees? Useful Tips from Our HR Director.',
+        type: 'heading',
+      },
+      description: {
+        content: 'Our HR Director shares his experience how to fill positions with the best candidates, where to find talents, how to attract experts to your business.',
+        type: 'text',
+      },
+      category: {
+        content: 'Creative Process',
+        type: 'caption',
+      },
+      date: {
+        content: 'September 21, 2017',
+        type: 'caption',
+      },
+      picture: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Article illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'How to Achieve Higher Profits in Retail with Our New Product?',
+        type: 'heading',
+      },
+      description: {
+        content: 'Do you want to achieve higher profits this year? Our new product will help you get what you want. In this article, you will find how to get more benefits from it.',
+        type: 'text',
+      },
+      category: {
+        content: 'Creative Process',
+        type: 'caption',
+      },
+      date: {
+        content: 'September 22, 2017',
+        type: 'caption',
+      },
+      picture: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Article illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Explore Top 5 Tips How to Improve Your Engineering Department.',
+        type: 'heading',
+      },
+      description: {
+        content: 'You engineers can bring you better results! Get to know how to improve engineering department to make a new step for your company growth.',
+        type: 'text',
+      },
+      category: {
+        content: 'Creative Process',
+        type: 'caption',
+      },
+      date: {
+        content: 'September 23, 2017',
+        type: 'caption',
+      },
+      picture: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Article illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'block-button': {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
