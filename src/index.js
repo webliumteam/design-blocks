41d5b931120f6bade7f971d1d4d323246177fde8
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-21 15:32:38 +0200

    rewrite content

--- src/index.js
+++ src/index.js
@@ -13,111 +13,24 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   blog: [
     {
-      title: {
-        content: 'Interserve\'s £200m Haymarket Edinburgh scheme hit by delays',
-        type: 'heading',
-      },
-      description: {
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipis cing elit. Aenean commodo ligula eget dolor. Aenean massa.',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 21, 2017',
-        type: 'caption',
-      },
+      ...Component.defaultContent.blog[0],
       picture: {
         src: 'img1.jpg',
         alt: 'Article illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
-      title: {
-        content: 'How to install or repair drywall for a kitchen backsplash ?',
-        type: 'heading',
-      },
-      description: {
-        content: 'Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
+      ...Component.defaultContent.blog[1],
       picture: {
         src: 'img2.jpg',
         alt: 'Article illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
-      title: {
-        content: 'Spurs reveal rival plans for Lendlease\'s High Road West site',
-        type: 'heading',
-      },
-      description: {
-        content: 'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 23, 2017',
-        type: 'caption',
-      },
+      ...Component.defaultContent.blog[2],
       picture: {
         src: 'img3.jpg',
         alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
   ],
@@ -125,7 +38,7 @@ ExtendedWireframe.defaultContent = {
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  'block-button': {defaultValue: false, label: 'Button', type: 'checkbox'},
+  'block-button': {defaultValue: false, label: 'Button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
