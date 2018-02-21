0e657d2905cd83230336399f7baa9fe690533f96
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-21 19:12:01 +0200

    rewrite content and button:

--- src/index.js
+++ src/index.js
@@ -9,4 +9,30 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  logo: {
+    text: {
+      value: 'Company Name',
+      tagName: 'h2',
+      fontSize: 26,
+    },
+  },
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+    size: 'md',
+  },
+}
+
 export default ExtendedWireframe
