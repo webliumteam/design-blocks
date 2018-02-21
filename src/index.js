197ce539641742636841f1b22ded34ddf599449f
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-21 13:57:26 +0200

    add

--- src/index.js
+++ src/index.js
@@ -9,4 +9,59 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#ffffff',
+  },
+  gallery: [
+    {
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'image-1.jpg',
+      },
+    },
+    {
+      picture: {
+        ...Component.defaultContent.gallery[1].picture,
+        resourceRef: 'image-2.jpg',
+      },
+    },
+    {
+      picture: {
+        ...Component.defaultContent.gallery[2].picture,
+        resourceRef: 'image-3.jpg',
+      },
+    },
+    {
+      picture: {
+        ...Component.defaultContent.gallery[3].picture,
+        resourceRef: 'image-4.jpg',
+      },
+    },
+    {
+      picture: {
+        ...Component.defaultContent.gallery[4].picture,
+        resourceRef: 'image-5.jpg',
+      },
+    },
+    {
+      picture: {
+        ...Component.defaultContent.gallery[5].picture,
+        resourceRef: 'image-6.jpg',
+      },
+    },
+  ],
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  'body-text': {defaultValue: false, label: 'Image description', type: 'hidden'},
+  caption: {defaultValue: false, label: 'Caption after body', type: 'hidden'},
+  'secondary-button': {defaultValue: false, label: 'Button', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Gallery description', type: 'hidden'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
+}
+
 export default ExtendedWireframe
