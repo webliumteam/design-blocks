90723fec7f5ced140cf0d9db1639bdabef4d4447
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-22 11:32:22 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
   theme: 'dark',
   background: {
     type: 'image',
@@ -21,143 +22,33 @@ ExtendedWireframe.defaultContent = {
   },
   services: [
     {
-      title: {
-        content: 'Construction Management',
-        type: 'heading',
-      },
-      description: {
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipis cing elit. Aenean commodo ligula eget dolor. Aenean massa.',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[0],
       picture: {
+        ...Component.defaultContent.services[0].picture,
         resourceRef: 'pic1.jpg',
-        alt: 'Service illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'Design and Build',
-        type: 'heading',
-      },
-      description: {
-        content: 'Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui ut eros et nisl sagittis vestibul.',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[1],
       picture: {
+        ...Component.defaultContent.services[1].picture,
         resourceRef: 'pic2.jpg',
-        alt: 'Service illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'General Constructing',
-        type: 'heading',
-      },
-      description: {
-        content: 'Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Phasellus accumsan cursus velit ac placerat dolor lectus quis orci.',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[2],
       picture: {
+        ...Component.defaultContent.services[2].picture,
         resourceRef: 'pic3.jpg',
-        alt: 'Service illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
   ],
-  topIcon: {
-    svg:
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
-  },
-  title: {
-    content: 'Services We Provide',
-    type: 'blockTitle',
-  },
-
-  subtitle: {
-    content: 'We deliver all kind of services that support small and micro businesses. Here are some of them:',
-    type: 'subtitle',
-  },
-  'button-1': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Request a quote',
-    type: 'primary',
-  },
-  'button-2': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'All Services',
-    type: 'secondary',
-  },
 }
 
 ExtendedWireframe.modifierScheme = {
-  body: {defaultValue: true, label: 'Service main text', type: 'checkbox'},
-  'button-primary': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
-  'button-secondary': {defaultValue: true, label: 'Button', type: 'checkbox'},
-  heading: {defaultValue: true, label: 'Service title', type: 'checkbox'},
-  link: {defaultValue: true, label: 'Link', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Services description', type: 'checkbox'},
-  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
+  ...Component.modifierScheme,
+  'button-primary': {defaultValue: false, label: 'Additional button', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Services description', type: 'hidden'},
 }
-
 
 export default ExtendedWireframe
