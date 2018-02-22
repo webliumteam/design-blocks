4899be1f327c88c11744e2dcf28da42658f94a46
efimenko bohdan <bogdane@weblium.com>
2018-02-22 13:06:40 +0200

    change component

--- src/component.js
+++ src/component.js
@@ -3,102 +3,134 @@ import $editor from 'weblium/editor'
 class Block extends React.Component {
   static propTypes = {
     components: PropTypes.object.isRequired,
-    $block: PropTypes.object.isRequired,
     style: PropTypes.object.isRequired,
   }
 
-  getModifierValue = path => _.get(['modifier', path], this.props.$block)
+  collectionItem = ({index, children, className}) => {
+    const {components: {Text, Image}, style} = this.props
+    return (
+      <article className={classNames(style.item, className)}>
+        {children}
 
-  getOptionValue = (path, defaultValue = false) =>
-    _.getOr(defaultValue, ['options', path], this.props.$block)
-
-  getImageSize = fullWidth =>
-    fullWidth
-      ? {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 1170}
-      : {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 570}
-
-  wrapImage = Component => <div className={this.props.style.image__wrapper}>{Component}</div>
+        <div className={style.item__inner}>
+          <Image
+            pictureClassName={style.item__pic}
+            imgClassName={style.item__img}
+            bind={`testimonials[${index}].picture`}
+          />
+          <div className={style.item__content}>
+            <Text bind={`testimonials[${index}].description`} className={style.item__desc} tagName="p" />
+            <Text bind={`testimonials[${index}].date`} className={style.item__time} tagName="time" />
+            <Text bind={`testimonials[${index}].title`} className={style.item__title} tagName="h2" />
+            <Text bind={`testimonials[${index}].position`} className={style.item__position} tagName="p" />
+          </div>
+        </div>
+      </article>
+    )
+  }
 
   render() {
-    const {components: {Text, Image, Button, SocialIcons}, style: css} = this.props
-    const columnLayout = !(
-      this.getModifierValue('title') ||
-      this.getModifierValue('subtitle') ||
-      this.getModifierValue('text') ||
-      this.getModifierValue('socialIcons')
-    )
-    const showButtonGroups = this.getModifierValue('link') || this.getModifierValue('button')
-    const ImageComponent = (
-      <Image
-        pictureClassName={css.article__picture}
-        bind="picture"
-        size={this.getImageSize(columnLayout)}
-      />
-    )
+    const {components: {Collection, Text, Button}, style} = this.props
     return (
-      <section className={classNames(css.section, {[css['section--column']]: columnLayout})}>
-        <div className={css.section__inner}>
-          <article className={css.article}>
-            {this.getOptionValue('image_wrapper')
-              ? this.wrapImage(ImageComponent)
-              : ImageComponent}
-            <div className={css.article__content}>
-              {this.getModifierValue('title') && (
-                <h1 className={css.article__title}>
-                  <Text bind="title" />
-                </h1>
-              )}
-              {this.getModifierValue('subtitle') && (
-                <p className={css.article__subtitle}>
-                  <Text bind="subtitle" />
-                </p>
-              )}
-              {this.getModifierValue('text') && (
-                <p className={css.article__text}>
-                  <Text bind="text" />
-                </p>
-              )}
-              {this.getModifierValue('socialIcons') && (
-                <div className={css.article__socials}>
-                  <h2 className={css['social-title']}>Follow us: </h2>
-                  <SocialIcons bind="socialIcons" />
-                </div>
-              )}
-              {showButtonGroups && (
-                <div className={css['btns-group']}>
-                  {this.getModifierValue('link') && <Button className={css.link} bind="link" />}
-                  {this.getModifierValue('button') && (
-                    <Button
-                      className={classNames(
-                        css.button,
-                        css['button--primary'],
-                        css['button--size-md'],
-                      )}
-                      bind="button"
-                    />
-                  )}
-                </div>
-              )}
-            </div>
-          </article>
+      <section className={style.section}>
+        <div className={style.section__inner}>
+          <Text bind="title" className={style.title} tagName="h1" />
+          <p >
+            <Text bind="subtitle" className={style.subtitle} tagName="p" />
+          </p>
+          <Collection
+            className={style['items-wrapper']}
+            bind="testimonials"
+            Item={this.collectionItem}
+          />
+          <div className={style['btns-group']}>
+            <Button
+              linkClassName={style.link}
+              buttonClassName={style.button}
+              bind="button"
+            />
+          </div>
         </div>
       </section>
     )
   }
 }
 
-Block.components = _.pick(['Text', 'Image', 'Button', 'SocialIcons'])($editor.components)
+Block.components = _.pick(['Collection', 'Text', 'Button', 'Image'])($editor.components)
 
 Block.defaultContent = {
-  title: 'About The Company',
-  'text-1': 'Follow us:',
-  subtitle:
-    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
-  text:
-    'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
-  picture: {
-    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-    alt: 'Picture about the company',
+  testimonials: [
+    {
+      title: {
+        content: 'Amanda Peterson',
+        type: 'heading',
+      },
+      date: {
+        content: 'October 28, 2017',
+        type: 'caption',
+      },
+      description: {
+        content: '“I was happy to work with Quantum! Their efficient solutions helped us to improve a plenty of business processes. Now our clients receive their products faster so that they are more satisfied with our services. They deserve the highest grade!”',
+        type: 'text',
+      },
+      position: {
+        content: 'Marketing Director at Junk Food Co.',
+        type: 'caption',
+      },
+      picture: {
+        alt: 'Amanda Peterson review',
+      },
+    },
+    {
+      title: {
+        content: 'Jeff Gordons',
+        type: 'heading',
+      },
+      date: {
+        content: 'October 28, 2017',
+        type: 'caption',
+      },
+      description: {
+        content: '“Our company started to work with Quantum 4 years ago. We had only positive experience and implemented a lot of projects together. We’re totally satisfied and now we know that we can rely on Quantum experts any time.”',
+        type: 'text',
+      },
+      position: {
+        content: 'CEO at Clear Thinking',
+        type: 'caption',
+      },
+      picture: {
+        alt: 'Jeff Gordons review',
+      },
+    },
+    {
+      title: {
+        content: 'Elizabeth Smith',
+        type: 'heading',
+      },
+      date: {
+        content: 'October 28, 2017',
+        type: 'caption',
+      },
+      description: {
+        content: '“Quantum is a reliable company that applies high technology and creates exclusive programs for their clients. We highly appreciate this approach and we will continue our cooperation.”',
+        type: 'text',
+      },
+      position: {
+        content: 'Director of Media at Legal Company',
+        type: 'caption',
+      },
+      picture: {
+        alt: 'Elizabeth Smith review',
+      },
+    },
+  ],
+  title: {
+    content: 'Testimonials',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Find out why so many companies prefer us over others!',
+    type: 'subtitle',
   },
   button: {
     actionConfig: {
@@ -111,60 +143,9 @@ Block.defaultContent = {
         },
       },
     },
-    textValue: 'Contact us',
-  },
-  link: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'More about us',
-  },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
+    textValue: 'Learn more',
+    type: 'secondary',
   },
 }
-
-Block.modifierScheme = {
-  button: {defaultValue: true, label: 'Contact us button', type: 'checkbox'},
-  link: {defaultValue: false, label: 'About us link', type: 'checkbox'},
-  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Subtitle', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
-  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
-}
-
 
 export default Block
