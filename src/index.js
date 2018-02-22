ae138755249cee39f8f3a6ae79ab0576285481a4
Slipachenko <slipachenko@Mac-mini-Monster-2.local>
2018-02-22 12:18:00 +0200

    correct index.js

--- src/index.js
+++ src/index.js
@@ -15,219 +15,68 @@ ExtendedWireframe.defaultContent = {
     type: 'image',
     position: {
       type: 'cover',
-      cover: '50% 50%',
+      cover: '100% 50%',
     },
     resourceRef: 'bg.jpg',
   },
   team: [
     {
-      name: {
-        content: 'Andrew Shimmer',
-        type: 'heading',
-      },
-      position: {
-        content: 'CEO',
-        type: 'caption',
-      },
-      description: {
-        content: 'Andrew Shimmer has a 15-year experience in IT development and marketing. He has successfully run companies specializing in innovative technologies.',
-        type: 'text',
-      },
+      ...Component.defaultContent.team[0],
       picture: {
-        src: 'pic1.png',
-        alt: 'Andrew Shimmer photo',
-      },
-      more: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-      email: {
-        content: '<a href="mailto:shimmer@gmail.com">shimmer@gmail.com</a>',
-        type: 'caption',
+        ...Component.defaultContent.team[0].picture,
+        resourceRef: 'pic1.png',
       },
       socialIcons: {
-        networks: [
-          {
-            id: 'twitter',
-            name: 'Twitter',
-            url: 'https://twitter.com/',
-          },
-          {
-            id: 'linkedin',
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/',
-          },
-          {
-            id: 'facebook',
-            name: 'Facebook',
-            url: 'https://facebook.com/',
-          },
-        ],
-        target: '_blank',
+        ...Component.defaultContent.team[0].socialIcons,
         design: {
+          ...Component.defaultContent.team[0].socialIcons.design,
           border: null,
           innerFill: true,
-          preset: 'preset001',
           offset: 0,
-          color: '#595959',
-          sizes: [15, 25, 35, 45],
+          color: '#505050',
           size: 30,
         },
       },
     },
     {
-      name: {
-        content: 'Ann Maisner',
-        type: 'heading',
-      },
-      position: {
-        content: 'Marketing Director',
-        type: 'caption',
-      },
-      description: {
-        content: 'Ann is a high-skilled professional with over 10 years of experience in marketing. She has been rewarded by the Association of Marketing as one of the most successful directors. ',
-        type: 'text',
-      },
+      ...Component.defaultContent.team[1],
       picture: {
-        src: 'pic2.png',
-        alt: 'Ann Maisner photo',
-      },
-      more: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-      email: {
-        content: '<a href="mailto:maisner@gmail.com">maisner@gmail.com</a>',
-        type: 'caption',
+        ...Component.defaultContent.team[1].picture,
+        resourceRef: 'pic2.png',
       },
       socialIcons: {
-        networks: [
-          {
-            id: 'twitter',
-            name: 'Twitter',
-            url: 'https://twitter.com/',
-          },
-          {
-            id: 'linkedin',
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/',
-          },
-          {
-            id: 'facebook',
-            name: 'Facebook',
-            url: 'https://facebook.com/',
-          },
-        ],
-        target: '_blank',
+        ...Component.defaultContent.team[1].socialIcons,
         design: {
+          ...Component.defaultContent.team[1].socialIcons.design,
           border: null,
           innerFill: true,
-          preset: 'preset001',
           offset: 0,
-          color: '#595959',
-          sizes: [15, 25, 35, 45],
+          color: '#505050',
           size: 30,
         },
       },
     },
     {
-      name: {
-        content: 'Tomas Abbar',
-        type: 'heading',
-      },
-      position: {
-        content: 'Product Manager',
-        type: 'caption',
-      },
-      description: {
-        content: 'For 13 years, Tomas has been taking a position of product manager at Quantum. He successfully launched several best-selling products. Now he is working on 3 new products.',
-        type: 'text',
-      },
+      ...Component.defaultContent.team[2],
       picture: {
-        src: 'pic3.png',
-        alt: 'Tomas Abbar photo',
-      },
-      more: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-      email: {
-        content: '<a href="mailto:abbar@gmail.com">abbar@gmail.com</a>',
-        type: 'caption',
+        ...Component.defaultContent.team[2].picture,
+        resourceRef: 'pic3.png',
       },
       socialIcons: {
-        networks: [
-          {
-            id: 'twitter',
-            name: 'Twitter',
-            url: 'https://twitter.com/',
-          },
-          {
-            id: 'linkedin',
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/',
-          },
-          {
-            id: 'facebook',
-            name: 'Facebook',
-            url: 'https://facebook.com/',
-          },
-        ],
-        target: '_blank',
+        ...Component.defaultContent.team[2].socialIcons,
         design: {
+          ...Component.defaultContent.team[2].socialIcons.design,
           border: null,
           innerFill: true,
-          preset: 'preset001',
           offset: 0,
-          color: '#595959',
-          sizes: [15, 25, 35, 45],
+          color: '#505050',
           size: 30,
         },
       },
     },
   ],
   cta: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'All Team Members',
+    ...Component.defaultContent.cta,
     type: 'secondary-alt',
   },
 }
