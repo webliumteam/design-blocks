4d2b13d33cd5d5a0071aa64e245408b9fc900544
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-22 12:22:58 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,4 +9,32 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  'plan-cta-1': {
+    type: 'secondary',
+    textValue: 'Order now',
+  },
+  'plan-cta-2': {
+    type: 'secondary',
+    textValue: 'Order now',
+  },
+  'plan-cta-3': {
+    type: 'secondary',
+    textValue: 'Order now',
+  },
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  'additional-btn': {defaultValue: false, label: 'Button', type: 'hidden'},
+  'plan-additional-info': {
+    defaultValue: false,
+    label: 'Product additional information',
+    type: 'hidden',
+  },
+  'plan-icon': {defaultValue: false, label: 'Product icon', type: 'hidden'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+}
+
 export default ExtendedWireframe
