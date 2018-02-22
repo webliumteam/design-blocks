da9feea3a7ca4823d309a2348432944695584551
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-22 10:34:13 +0200

    remove 100vh

--- src/index.js
+++ src/index.js
@@ -9,4 +9,18 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  theme: 'dark',
+  background: {
+    type: 'image',
+    src: 'mountains.png',
+  },
+}
+
+ExtendedWireframe.modifierScheme = {
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  'primary-btn': {defaultValue: true, label: 'Button', type: 'checkbox'},
+}
+
 export default ExtendedWireframe
