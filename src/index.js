35ca50f177718ccefdcd9d3b5102f1e0b00d1105
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-22 12:35:19 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,4 +9,48 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#ffffff',
+  },
+  services: [
+    {
+      ...Component.defaultContent.services[0],
+      picture: {
+        ...Component.defaultContent.services[0].picture,
+        resourceRef: 'services-img1.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.services[1],
+      picture: {
+        ...Component.defaultContent.services[1].picture,
+        resourceRef: 'services-img2.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.services[2],
+      picture: {
+        ...Component.defaultContent.services[2].picture,
+        resourceRef: 'services-img3.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.services[3],
+      picture: {
+        ...Component.defaultContent.services[3].picture,
+        resourceRef: 'services-img4.jpg',
+      },
+    },
+  ],
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  'button-primary': {defaultValue: false, label: 'Additional button', type: 'hidden'},
+  link: {defaultValue: false, label: 'Link', type: 'hidden'},
+}
+
 export default ExtendedWireframe
