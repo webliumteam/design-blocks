7df52666adcbcdcc5de156a4db548ad2a6154986
Samogray Olexandr <oleksandrs@weblium.com>
2018-02-23 18:30:49 +0200

    inner

--- src/component.js
+++ src/component.js
@@ -80,8 +80,10 @@ class Block extends React.Component {
             {this.state.users &&
               this.state.users.map(item => (
                 <li className={css.list__item}>
-                  <span>{item.position}.&nbsp;</span>
-                  <span className={css['list__item-inner']}>{item.displayName}</span>
+                  <div className={css['list__item-inner']}>
+                    <span>{item.position}.&nbsp;</span>
+                    <span className={css['list__item-inner']}>{item.displayName}</span>
+                  </div>
                   <a href={item.profileUrl} className={css.list__link}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96.124 96.123">
                       <path d="M72.089.02L59.624 0C45.62 0 36.57 9.285 36.57 23.656v10.907H24.037a1.96 1.96 0 0 0-1.96 1.961v15.803a1.96 1.96 0 0 0 1.96 1.96H36.57v39.876a1.96 1.96 0 0 0 1.96 1.96h16.352a1.96 1.96 0 0 0 1.96-1.96V54.287h14.654a1.96 1.96 0 0 0 1.96-1.96l.006-15.803a1.963 1.963 0 0 0-1.961-1.961H56.842v-9.246c0-4.444 1.059-6.7 6.848-6.7l8.397-.003a1.96 1.96 0 0 0 1.959-1.96V1.98A1.96 1.96 0 0 0 72.089.02z" />
