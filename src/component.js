78e4e8cfb7b89d368971a5e02b9a0c9cf46c4f96
nmelnychenko <mykola.melnichenko@gmail.com>
2018-02-23 18:27:54 +0200

    finish first iteration

--- src/component.js
+++ src/component.js
@@ -12,94 +12,177 @@ class Block extends React.Component {
   getOptionValue = (path, defaultValue = false) =>
     _.getOr(defaultValue, ['options', path], this.props.$block)
 
-  getImageSize = fullWidth =>
-    fullWidth
-      ? {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 1170}
-      : {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 570}
+  collectionItem = ({index, children, modifier, className}) => {
+    const {components: {Text, Icon, Button}, style} = this.props
 
-  wrapImage = Component => <div className={this.props.style.image__wrapper}>{Component}</div>
-
-  render() {
-    const {components: {Text, Image, Button, SocialIcons}, style: css} = this.props
-    const columnLayout = !(
-      this.getModifierValue('title') ||
-      this.getModifierValue('subtitle') ||
-      this.getModifierValue('text') ||
-      this.getModifierValue('socialIcons')
-    )
-    const showButtonGroups = this.getModifierValue('link') || this.getModifierValue('button')
-    const ImageComponent = (
-      <Image
-        pictureClassName={css.article__picture}
-        bind="picture"
-        size={this.getImageSize(columnLayout)}
-      />
-    )
     return (
-      <section className={classNames(css.section, {[css['section--column']]: columnLayout})}>
-        <div className={css.section__inner}>
-          <article className={css.article}>
-            {this.getOptionValue('image_wrapper')
-              ? this.wrapImage(ImageComponent)
-              : ImageComponent}
-            <div className={css.article__content}>
-              {this.getModifierValue('title') && (
-                <h1 className={css.article__title}>
-                  <Text bind="title" />
-                </h1>
+      <article className={classNames(style.item, className)}>
+        {children}
+        <div className={style.item__inner}>
+          {this.getModifierValue('item-icon') && <Icon className={style.item__icon} bind={`items[${index}].icon`} />}
+          <div className={style.item__content}>
+            <div className={style.item__info}>
+              <Text tagName="h3" className={style.item__title} bind={`items[${index}].heading`} />
+              {this.getModifierValue('employment') && (
+                <Text tagName="p" className={style.item__text} bind={`items[${index}].employment`} />
               )}
-              {this.getModifierValue('subtitle') && (
-                <p className={css.article__subtitle}>
-                  <Text bind="subtitle" />
-                </p>
-              )}
-              {this.getModifierValue('text') && (
-                <p className={css.article__text}>
-                  <Text bind="text" />
-                </p>
-              )}
-              {this.getModifierValue('socialIcons') && (
-                <div className={css.article__socials}>
-                  <h2 className={css['social-title']}>Follow us: </h2>
-                  <SocialIcons bind="socialIcons" />
-                </div>
-              )}
-              {showButtonGroups && (
-                <div className={css['btns-group']}>
-                  {this.getModifierValue('link') && <Button className={css.link} bind="link" />}
-                  {this.getModifierValue('button') && (
-                    <Button
-                      className={classNames(
-                        css.button,
-                        css['button--primary'],
-                        css['button--size-md'],
-                      )}
-                      bind="button"
-                    />
-                  )}
-                </div>
+              {this.getModifierValue('position') && (
+                <Text tagName="p" className={style.item__text} bind={`items[${index}].location`} />
               )}
             </div>
-          </article>
+            <Button
+              linkClassName={style.link}
+              buttonClassName={style.button}
+              className={style.item__link}
+              bind={`items[${index}].link`}
+            />
+          </div>
+        </div>
+      </article>
+    )
+  }
+
+  render() {
+    const {components: {Text, Collection, Button}, style: css, $block} = this.props
+
+    return (
+      <section className={classNames(css.section)}>
+        <div className={css.section__inner}>
+          <div className={css.header}>
+            <Text tagName="h1" className={css.title} bind="title" />
+            {this.getModifierValue('subtitle') && <Text tagName="p" className={css.subtitle} bind="subtitle" />}
+          </div>
+        </div>
+        <Collection
+          className={css['items-wrapper']}
+          TagName="div"
+          bind="items"
+          Item={this.collectionItem}
+          itemProps={{
+            modifier: $block.modifier,
+          }}
+        />
+        <div className={css['btns-group']}>
+          <Button
+            linkClassName={css.link}
+            buttonClassName={css.button}
+            bind="button"
+          />
         </div>
       </section>
     )
   }
 }
 
-Block.components = _.pick(['Text', 'Image', 'Button', 'SocialIcons'])($editor.components)
+Block.components = _.pick(['Text', 'Icon', 'Button', 'Collection'])($editor.components)
 
 Block.defaultContent = {
-  title: 'About The Company',
-  'text-1': 'Follow us:',
-  subtitle:
-    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
-  text:
-    'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
-  picture: {
-    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-    alt: 'Picture about the company',
+  title: {
+    content: 'Careers',
+    type: 'blockTitle',
   },
+  subtitle: {
+    content: 'We’re always looking for experts and talents who want to grow personally and professionally in our company. Please check the open positions: ',
+    type: 'subtitle',
+  },
+  items: [
+    {
+      icon: {
+        svg: "<svg viewBox='0 0 70 70'><circle cx='410' cy='775' r='35' fill-rule='evenodd' transform='translate(-375 -740)'/></svg>",
+        fill: '',
+      },
+      heading: {
+        content: 'Web Developer',
+        type: 'heading',
+      },
+      employment: {
+        content: 'Full time',
+        type: 'text',
+      },
+      location: {
+        content: 'London',
+        type: 'text',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        type: 'link',
+        textValue: 'More about vacancy',
+      },
+    },
+    {
+      icon: {
+        svg: "<svg viewBox='0 0 70 70'><circle cx='410' cy='775' r='35' fill-rule='evenodd' transform='translate(-375 -740)'/></svg>",
+        fill: '',
+      },
+      heading: {
+        content: '3D designer',
+        type: 'heading',
+      },
+      employment: {
+        content: 'Remote work',
+        type: 'text',
+      },
+      location: {
+        content: 'Amsterdam',
+        type: 'text',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        type: 'link',
+        textValue: 'More about vacancy',
+      },
+    },
+    {
+      icon: {
+        svg: "<svg viewBox='0 0 70 70'><circle cx='410' cy='775' r='35' fill-rule='evenodd' transform='translate(-375 -740)'/></svg>",
+        fill: '',
+      },
+      heading: {
+        content: 'Office manager',
+        type: 'heading',
+      },
+      employment: {
+        content: 'Full time',
+        type: 'text',
+      },
+      location: {
+        content: 'Amsterdam',
+        type: 'text',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        type: 'link',
+        textValue: 'More about vacancy',
+      },
+    },
+  ],
   button: {
     actionConfig: {
       action: 'link',
@@ -111,60 +194,17 @@ Block.defaultContent = {
         },
       },
     },
-    textValue: 'Contact us',
-  },
-  link: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'More about us',
-  },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
+    type: 'secondary',
+    size: 'md',
+    textValue: 'Learn more',
   },
 }
 
 Block.modifierScheme = {
-  button: {defaultValue: true, label: 'Contact us button', type: 'checkbox'},
-  link: {defaultValue: false, label: 'About us link', type: 'checkbox'},
-  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Subtitle', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
-  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Careers description', type: 'checkbox'},
+  'item-icon': {defaultValue: true, label: 'Vacancy icon', type: 'checkbox'},
+  employment: {defaultValue: true, label: 'Type of employment (full-time/part-time)', type: 'checkbox'},
+  position: {defaultValue: true, label: 'Vacancy location', type: 'checkbox'},
 }
-
 
 export default Block
