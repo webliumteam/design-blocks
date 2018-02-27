4766f601e3fe2c74eecdb505a2fc8f512a7d1795
artemvasilkin <artemvasilkin14@gmail.com>
2018-02-27 11:52:59 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -11,41 +11,40 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    content: '- Awards -',
+    type: 'blockTitle',
+  },
   awards: [
     {
-      id: '8130928a-e7c6-4e26-a3ca-a820fa36380e',
-      title: 'R&D Leader in Tech (2017)',
+      ...Component.defaultContent.awards[0],
       picture: {
+        ...Component.defaultContent.awards[0].picture,
         resourceRef: 'awards_01.jpg',
-        alt: 'Award illustration photo',
       },
     },
     {
-      id: '057c27bf-4977-4b28-bf35-f4a6db60d5aa',
-      title: 'Most Innovative B2B Solutions (2016)',
+      ...Component.defaultContent.awards[1],
       picture: {
+        ...Component.defaultContent.awards[1].picture,
         resourceRef: 'awards_02.jpg',
-        alt: 'Award illustration photo',
       },
     },
     {
-      id: 'c8e93041-896b-4b78-b8d0-37e21a79070f',
-      title: 'Best Customer-Focused Approach on the World Conference (2014)',
+      ...Component.defaultContent.awards[2],
       picture: {
+        ...Component.defaultContent.awards[2].picture,
         resourceRef: 'awards_03.jpg',
-        alt: 'Award illustration photo',
       },
     },
     {
-      id: '3ea9fdda-c0e6-418e-9bfe-7bf1b06a534d',
-      title: 'Leader of Innovation (2012)',
+      ...Component.defaultContent.awards[3],
       picture: {
+        ...Component.defaultContent.awards[3].picture,
         resourceRef: 'awards_04.jpg',
-        alt: 'Award illustration photo',
       },
     },
   ],
-  title: '- Awards -',
 }
 
 ExtendedWireframe.modifierScheme = {
