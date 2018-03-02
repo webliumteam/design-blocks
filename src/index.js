616d20bb451a55bf358acdbf8482926073921f0f
artemvasilkin <artemvasilkin14@gmail.com>
2018-03-02 16:13:09 +0200

    ready for qa

--- src/index.js
+++ src/index.js
@@ -9,4 +9,181 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: '- TEAM -',
+  },
+  team: [
+    {
+      ...Component.defaultContent.team[0],
+      name: {
+        ...Component.defaultContent.team[0].name,
+        content: 'ANDREW SHIMMER',
+      },
+      position: {
+        ...Component.defaultContent.team[0].position,
+        content: 'CEO',
+      },
+      picture: {
+        ...Component.defaultContent.team[0].picture,
+        resourceRef: 'team_01.png',
+      },
+      more: {
+        ...Component.defaultContent.team[0].more,
+        type: 'link',
+      },
+      socialIcons: {
+        networks: [
+          {
+            id: 'facebook',
+            name: 'Facebook',
+            url: 'https://facebook.com/',
+          },
+          {
+            id: 'twitter',
+            name: 'Twitter',
+            url: 'https://twitter.com/',
+          },
+          {
+            id: 'instagram',
+            name: 'Instagram',
+            url: 'https://instagram.com/',
+          },
+          {
+            id: 'linkedin',
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/',
+          },
+        ],
+        target: '_blank',
+        design: {
+          border: 'circle',
+          innerFill: true,
+          preset: 'preset001',
+          offset: 20,
+          color: 'var(--ui-dark-accent-color)',
+          sizes: [15, 25, 35, 45],
+          size: '32px',
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.team[1],
+      name: {
+        ...Component.defaultContent.team[1].name,
+        content: 'ANN MAISNER',
+      },
+      position: {
+        ...Component.defaultContent.team[1].position,
+        content: 'MARKETING DIRECTOR',
+      },
+      picture: {
+        ...Component.defaultContent.team[1].picture,
+        resourceRef: 'team_02.png',
+      },
+      more: {
+        ...Component.defaultContent.team[1].more,
+        type: 'link',
+      },
+      socialIcons: {
+        networks: [
+          {
+            id: 'facebook',
+            name: 'Facebook',
+            url: 'https://facebook.com/',
+          },
+          {
+            id: 'twitter',
+            name: 'Twitter',
+            url: 'https://twitter.com/',
+          },
+          {
+            id: 'instagram',
+            name: 'Instagram',
+            url: 'https://instagram.com/',
+          },
+          {
+            id: 'linkedin',
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/',
+          },
+        ],
+        target: '_blank',
+        design: {
+          border: 'circle',
+          innerFill: true,
+          preset: 'preset001',
+          offset: 20,
+          color: 'var(--ui-dark-accent-color)',
+          sizes: [15, 25, 35, 45],
+          size: '32px',
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.team[2],
+      name: {
+        ...Component.defaultContent.team[2].name,
+        content: 'TOMAS ABBAR',
+      },
+      position: {
+        ...Component.defaultContent.team[2].position,
+        content: 'PRODUCT MANAGER',
+      },
+      picture: {
+        ...Component.defaultContent.team[2].picture,
+        resourceRef: 'team_03.png',
+      },
+      more: {
+        ...Component.defaultContent.team[2].more,
+        type: 'link',
+      },
+      socialIcons: {
+        networks: [
+          {
+            id: 'facebook',
+            name: 'Facebook',
+            url: 'https://facebook.com/',
+          },
+          {
+            id: 'twitter',
+            name: 'Twitter',
+            url: 'https://twitter.com/',
+          },
+          {
+            id: 'instagram',
+            name: 'Instagram',
+            url: 'https://instagram.com/',
+          },
+          {
+            id: 'linkedin',
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/',
+          },
+        ],
+        target: '_blank',
+        design: {
+          border: 'circle',
+          innerFill: true,
+          preset: 'preset001',
+          offset: 20,
+          color: 'var(--ui-dark-accent-color)',
+          sizes: [15, 25, 35, 45],
+          size: '32px',
+        },
+      },
+    },
+  ],
+  cta: {
+    ...Component.defaultContent.cta,
+    type: 'secondary',
+  },
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+}
+
 export default ExtendedWireframe
