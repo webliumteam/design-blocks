fe920a4347096661849f21993cbd5fef432e6b9e
efimenko bohdan <bogdane@weblium.com>
2018-03-05 11:04:01 +0200

    complete block

--- src/component.js
+++ src/component.js
@@ -9,97 +9,124 @@ class Block extends React.Component {
 
   getModifierValue = path => _.get(['modifier', path], this.props.$block)
 
-  getOptionValue = (path, defaultValue = false) =>
-    _.getOr(defaultValue, ['options', path], this.props.$block)
+  collectionItem = ({index, children, className, modifier}) => {
+    const {components: {Text, Image}, style} = this.props
+    return (
+      <article className={classNames(style.article, className)}>
+        {children}
 
-  getImageSize = fullWidth =>
-    fullWidth
-      ? {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 1170}
-      : {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 570}
-
-  wrapImage = Component => <div className={this.props.style.image__wrapper}>{Component}</div>
+        <Image
+          pictureClassName={style.article__picture}
+          imgClassName={style.article__image}
+          bind={`steps[${index}].picture`}
+          size={
+            {
+              'min-width: 992px': 170,
+              'min-width: 768px': 100,
+              'min-width: 320px': 70,
+            }
+          }
+        />
+        <div className={style.article__content}>
+          <Text bind={`steps[${index}].heading`} className={style.article__title} tagName="h2" />
+          {_.get('body')(modifier) && (
+            <Text bind={`steps[${index}].description`} className={style.article__text} tagName="p" />
+          )}
+        </div>
+      </article>
+    )
+  }
 
   render() {
-    const {components: {Text, Image, Button, SocialIcons}, style: css} = this.props
-    const columnLayout = !(
-      this.getModifierValue('title') ||
-      this.getModifierValue('subtitle') ||
-      this.getModifierValue('text') ||
-      this.getModifierValue('socialIcons')
-    )
-    const showButtonGroups = this.getModifierValue('link') || this.getModifierValue('button')
-    const ImageComponent = (
-      <Image
-        pictureClassName={css.article__picture}
-        bind="picture"
-        size={this.getImageSize(columnLayout)}
-      />
-    )
+    const {components: {Text, Button, Collection}, style, $block} = this.props
     return (
-      <section className={classNames(css.section, {[css['section--column']]: columnLayout})}>
-        <div className={css.section__inner}>
-          <article className={css.article}>
-            {this.getOptionValue('image_wrapper')
-              ? this.wrapImage(ImageComponent)
-              : ImageComponent}
-            <div className={css.article__content}>
+      <section className={style.section}>
+        <div className={style.section__inner}>
+          {(this.getModifierValue('title') || this.getModifierValue('subtitle')) && (
+            <header className={style.section__header}>
               {this.getModifierValue('title') && (
-                <h1 className={css.article__title}>
-                  <Text bind="title" />
-                </h1>
+                <Text bind="title" className={style.title} tagName="h1" />
               )}
               {this.getModifierValue('subtitle') && (
-                <p className={css.article__subtitle}>
-                  <Text bind="subtitle" />
-                </p>
+                <Text bind="subtitle" className={style.subtitle} tagName="p" />
               )}
-              {this.getModifierValue('text') && (
-                <p className={css.article__text}>
-                  <Text bind="text" />
-                </p>
-              )}
-              {this.getModifierValue('socialIcons') && (
-                <div className={css.article__socials}>
-                  <h2 className={css['social-title']}>Follow us: </h2>
-                  <SocialIcons bind="socialIcons" />
-                </div>
-              )}
-              {showButtonGroups && (
-                <div className={css['btns-group']}>
-                  {this.getModifierValue('link') && <Button className={css.link} bind="link" />}
-                  {this.getModifierValue('button') && (
-                    <Button
-                      className={classNames(
-                        css.button,
-                        css['button--primary'],
-                        css['button--size-md'],
-                      )}
-                      bind="button"
-                    />
-                  )}
-                </div>
-              )}
+            </header>
+          )}
+          <Collection
+            className={style['articles-wrapper']}
+            bind="steps"
+            Item={this.collectionItem}
+            itemProps={{
+              modifier: $block.modifier,
+            }}
+          />
+          {this.getModifierValue('button') && (
+            <div className={style['btns-group']}>
+              <Button
+                linkClassName={style.link}
+                buttonClassName={style.button}
+                bind="button"
+              />
             </div>
-          </article>
+          )}
         </div>
       </section>
     )
   }
 }
 
-Block.components = _.pick(['Text', 'Image', 'Button', 'SocialIcons'])($editor.components)
+Block.components = _.pick(['Text', 'Button', 'Image', 'Collection'])($editor.components)
 
 Block.defaultContent = {
-  title: 'About The Company',
-  'text-1': 'Follow us:',
-  subtitle:
-    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
-  text:
-    'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
-  picture: {
-    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-    alt: 'Picture about the company',
+  title: {
+    content: 'Our process',
+    type: 'blockTitle',
   },
+  subtitle: {
+    content: 'We are sure that the process of our cooperation with clients should be clear and simple. Take these 3 steps to enjoy the benefits of working with us!',
+    type: 'subtitle',
+  },
+  steps: [
+    {
+      heading: {
+        content: 'Step 1. Choose a plan',
+        type: 'heading',
+      },
+      description: {
+        content: 'We provide you with full information about each plan. There are a few available options to choose from.',
+        type: 'text',
+      },
+      picture: {
+        alt: 'Illustraion for step 1',
+      },
+    },
+    {
+      heading: {
+        content: 'Step 2. Proceed with the payment',
+        type: 'heading',
+      },
+      description: {
+        content: 'We guarantee the security of all payments.',
+        type: 'text',
+      },
+      picture: {
+        alt: 'Illustraion for step 2',
+      },
+    },
+    {
+      heading: {
+        content: 'Step 3. Enjoy our products',
+        type: 'heading',
+      },
+      description: {
+        content: 'You will get access to all options immediately.',
+        type: 'text',
+      },
+      picture: {
+        alt: 'Illustraion for step 3',
+      },
+    },
+  ],
   button: {
     actionConfig: {
       action: 'link',
@@ -111,59 +138,16 @@ Block.defaultContent = {
         },
       },
     },
-    textValue: 'Contact us',
-  },
-  link: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'More about us',
-  },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
+    textValue: 'Learn more',
+    type: 'secondary',
   },
 }
 
 Block.modifierScheme = {
-  button: {defaultValue: true, label: 'Contact us button', type: 'checkbox'},
-  link: {defaultValue: false, label: 'About us link', type: 'checkbox'},
-  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Subtitle', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
   title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Process description', type: 'checkbox'},
+  body: {defaultValue: true, label: 'Step main text', type: 'checkbox'},
+  button: {defaultValue: false, label: 'Button', type: 'checkbox'},
 }
 
 
