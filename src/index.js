daf65f5f6f15cb66db33ca4032471f649be98b7a
KonstantinKH <kkhomenko9@gmail.com>
2018-03-07 09:36:22 +0200

    changed

--- src/index.js
+++ src/index.js
@@ -11,10 +11,66 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  heading: {
+    content: 'Welcome to Our Company ',
+    type: 'heading',
+  },
+  subheading: {
+    content: 'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
+    type: 'subheading',
+  },
+  text: {
+    content: 'Our Company is the world’s leading manufacturer. We are also a leading financial services provider. We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
+    type: 'text',
+  },
+  'social-icons': {
+    networks: [
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'null',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 15,
+      color: '#9b9b9b',
+      sizes: [10, 20, 30, 40],
+      size: 30,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: true},
+  subheading: {defaultValue: true},
 }
 
 export default ExtendedWireframe
