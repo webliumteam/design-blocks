feec1090a24247e01061a333bfc14411563d205c
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-07 11:41:48 +0200

    design

--- src/index.js
+++ src/index.js
@@ -11,6 +11,247 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'dark-shade-color',
+  },
+  logo: {
+    image: {
+      resourceRef: 'logo.png',
+      alt: 'logo',
+    },
+  },
+  description: {
+    type: 'text',
+    brightness: 30,
+    content: 'Company description about how awesome we are. Company description about how awesome we are. Company description about how awesome we are.',
+  },
+  menu: [
+    {
+      id: 'about',
+      metadata: {
+        displayName: 'About',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        displayName: 'Services',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'careers',
+      metadata: {
+        displayName: 'Careers',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'projects',
+      metadata: {
+        displayName: 'Projects',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'testimonials',
+      metadata: {
+        displayName: 'Testimonials',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'blog',
+      metadata: {
+        displayName: 'Blog',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'approach',
+      metadata: {
+        displayName: 'Our approach',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'industries',
+      metadata: {
+        displayName: 'Industries',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'team',
+      metadata: {
+        displayName: 'Our team',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'resources',
+      metadata: {
+        displayName: 'Resources',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'press',
+      metadata: {
+        displayName: 'Press',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'contacts-us',
+      metadata: {
+        displayName: 'Contact us',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+  socialIcons: {
+    networks: [
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'null',
+      innerFill: false,
+      preset: 'preset001',
+      offset: 15,
+      color: '#909aa3',
+      sizes: [10, 20, 30, 40],
+      size: 30,
+    },
+  },
+  copyright: {
+    type: 'text',
+    content: 'Produced by Weblium.com. All rights Reserved',
+    brightness: 30,
+  },
+  additional: {
+    type: 'text',
+    content: 'Demo preview for {Template name} {Concept name}',
+    brightness: 30,
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
