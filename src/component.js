c2d13efba6453540b538dbf12867cbe9b1aece2a
nmelnychenko <mykola.melnichenko@gmail.com>
2018-03-07 23:22:56 +0200

    start work with whyW3

--- src/component.js
+++ src/component.js
@@ -12,94 +12,103 @@ class Block extends React.Component {
   getOptionValue = (path, defaultValue = false) =>
     _.getOr(defaultValue, ['options', path], this.props.$block)
 
-  getImageSize = fullWidth =>
-    fullWidth
-      ? {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 1170}
-      : {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 570}
+  collectionItem = ({index, children, className, modifier}) => {
+    const {components: {Text, Image}, style: css} = this.props
 
-  wrapImage = Component => <div className={this.props.style.image__wrapper}>{Component}</div>
+    return (
+      <article className={classNames(css.item, className)}>
+        {children}
+        <Image pictureClassName={css.item__picture} bind={`items[${index}].image`} />
+        <Text tagName="h2" className={css.item__heading} bind={`items[${index}].heading`} />
+        {this.getModifierValue('item-body') && (
+          <Text tagName="p" className={css.item__text} bind={`items[${index}].text`} />
+        )}
+      </article>
+    )
+  }
 
   render() {
-    const {components: {Text, Image, Button, SocialIcons}, style: css} = this.props
-    const columnLayout = !(
-      this.getModifierValue('title') ||
-      this.getModifierValue('subtitle') ||
-      this.getModifierValue('text') ||
-      this.getModifierValue('socialIcons')
-    )
-    const showButtonGroups = this.getModifierValue('link') || this.getModifierValue('button')
-    const ImageComponent = (
-      <Image
-        pictureClassName={css.article__picture}
-        bind="picture"
-        size={this.getImageSize(columnLayout)}
-      />
-    )
+    const {components: {Text, Collection, Button}, style: css, $block} = this.props
+    const showButtonGroups = this.getModifierValue('primary-button') || this.getModifierValue('secondary-button')
+
     return (
-      <section className={classNames(css.section, {[css['section--column']]: columnLayout})}>
+      <section className={css.section}>
         <div className={css.section__inner}>
-          <article className={css.article}>
-            {this.getOptionValue('image_wrapper')
-              ? this.wrapImage(ImageComponent)
-              : ImageComponent}
-            <div className={css.article__content}>
-              {this.getModifierValue('title') && (
-                <h1 className={css.article__title}>
-                  <Text bind="title" />
-                </h1>
-              )}
-              {this.getModifierValue('subtitle') && (
-                <p className={css.article__subtitle}>
-                  <Text bind="subtitle" />
-                </p>
-              )}
-              {this.getModifierValue('text') && (
-                <p className={css.article__text}>
-                  <Text bind="text" />
-                </p>
-              )}
-              {this.getModifierValue('socialIcons') && (
-                <div className={css.article__socials}>
-                  <h2 className={css['social-title']}>Follow us: </h2>
-                  <SocialIcons bind="socialIcons" />
-                </div>
-              )}
-              {showButtonGroups && (
-                <div className={css['btns-group']}>
-                  {this.getModifierValue('link') && <Button className={css.link} bind="link" />}
-                  {this.getModifierValue('button') && (
-                    <Button
-                      className={classNames(
-                        css.button,
-                        css['button--primary'],
-                        css['button--size-md'],
-                      )}
-                      bind="button"
-                    />
-                  )}
-                </div>
-              )}
-            </div>
-          </article>
+          {this.getModifierValue('title') && (
+            <Text tagName="h1" className={css.title} bind="title" />
+          )}
+          {this.getModifierValue('subtitle') && (
+            <Text tagName="p" className={css.subtitle} bind="subtitle" />
+          )}
+          <Collection
+            className={css['items-wrapper']}
+            bind="items"
+            Item={this.collectionItem}
+            itemProps={{
+              modifier: $block.modifier,
+            }}
+          />
         </div>
       </section>
     )
   }
 }
 
-Block.components = _.pick(['Text', 'Image', 'Button', 'SocialIcons'])($editor.components)
+Block.components = _.pick(['Text', 'Image', 'Button', 'Collection'])($editor.components)
 
 Block.defaultContent = {
-  title: 'About The Company',
-  'text-1': 'Follow us:',
-  subtitle:
-    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
-  text:
-    'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
-  picture: {
-    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-    alt: 'Picture about the company',
+  title: {
+    type: 'blockTitle',
+    content: 'Why Choose Us',
   },
+  subtitle: {
+    type: 'subtitle',
+    content: 'Still have some hesitations whether cooperation with us is worth the trouble? Check the reasons why you should choose us among other companies!',
+  },
+  items: [
+    {
+      image: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Picture about the company',
+      },
+      heading: {
+        type: 'heading',
+        content: '',
+      },
+      text: {
+        type: 'text',
+        content: '',
+      },
+    },
+    {
+      image: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Picture about the company',
+      },
+      heading: {
+        type: 'heading',
+        content: '',
+      },
+      text: {
+        type: 'text',
+        content: '',
+      },
+    },
+    {
+      image: {
+        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+        alt: 'Picture about the company',
+      },
+      heading: {
+        type: 'heading',
+        content: '',
+      },
+      text: {
+        type: 'text',
+        content: '',
+      },
+    },
+  ],
   button: {
     actionConfig: {
       action: 'link',
@@ -126,45 +135,13 @@ Block.defaultContent = {
     },
     textValue: 'More about us',
   },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
-  },
 }
 
 Block.modifierScheme = {
+  title: {defaultValue: true, label: 'Title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
+  'item-body': {defaultValue: true, label: 'Advantage description', type: 'checkbox'},
   button: {defaultValue: true, label: 'Contact us button', type: 'checkbox'},
-  link: {defaultValue: false, label: 'About us link', type: 'checkbox'},
-  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Subtitle', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
-  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
-
 
 export default Block
