76b5df019daeca664e72ad2fae31e18c490cc8c8
artemvasilkin <artemvasilkin14@gmail.com>
2018-03-12 16:56:30 +0200

    update content

--- src/index.js
+++ src/index.js
@@ -9,4 +9,33 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'cover-bg.jpg',
+    overlay: {
+      type: 'gradient',
+      gradient: {
+        angle: 90,
+        firstColor: 'var(--ui-dark-accent-color)',
+        secondColor: 'var(--ui-light-accent-color)',
+      },
+      opacity: 0.76,
+    },
+  },
+  'cta-1': {
+    ...Component.defaultContent['cta-1'],
+    type: 'primary',
+  },
+  'cta-2': {
+    ...Component.defaultContent['cta-2'],
+    type: 'secondary',
+  },
+}
+
 export default ExtendedWireframe
