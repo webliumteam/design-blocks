9bdd3501349e18d6dfef1b888c70afe7c69bce55
efimenko bohdan <bogdane@weblium.com>
2018-03-13 11:38:18 +0200

    begin create

--- src/component.js
+++ src/component.js
@@ -1,4 +1,5 @@
 import $editor from 'weblium/editor'
+import Media from 'react-media'
 
 class Block extends React.Component {
   static propTypes = {
@@ -7,162 +8,168 @@ class Block extends React.Component {
     style: PropTypes.object.isRequired,
   }
 
+  state = {
+    active: 0,
+  }
+
   getModifierValue = path => _.get(['modifier', path], this.props.$block)
 
-  getOptionValue = (path, defaultValue = false) =>
-    _.getOr(defaultValue, ['options', path], this.props.$block)
+  toggleItemVisible = index => () => {
+    if (index !== this.state.active) this.setState({active: index})
+  }
 
-  getImageSize = fullWidth =>
-    fullWidth
-      ? {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 1170}
-      : {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 570}
+  collectionItem = ({index, children, className}) => {
+    const {components: {Text, Image}, style} = this.props
 
-  wrapImage = Component => <div className={this.props.style.image__wrapper}>{Component}</div>
+    return (
+      <li className={classNames(style['tabs-item'], className)} onClick={this.toggleItemVisible(index)} role="presentation">
+        {children}
+
+        <button type="button" role="tab" className={style['tabs-item__button']}>
+          <Image
+            pictureClassName={style['tabs-item__picture']}
+            imgClassName={style['tabs-item__image']}
+            bind={`items[${this.state.active}].itemPicture`}
+            size={
+              {
+              'min-width: 320px': 80,
+              }
+            }
+          />
+          <div className={style['tabs-item__content']}>
+            <Text bind={`items[${index}].itemTitle`} tagName="h2" className="tabs-item__title" />
+            <Text bind={`items[${index}].itemPosition`} tagName="small" className="tabs-item__position" />
+          </div>
+        </button>
+      </li>
+    )
+  }
 
   render() {
-    const {components: {Text, Image, Button, SocialIcons}, style: css} = this.props
-    const columnLayout = !(
-      this.getModifierValue('title') ||
-      this.getModifierValue('subtitle') ||
-      this.getModifierValue('text') ||
-      this.getModifierValue('socialIcons')
-    )
-    const showButtonGroups = this.getModifierValue('link') || this.getModifierValue('button')
-    const ImageComponent = (
-      <Image
-        pictureClassName={css.article__picture}
-        bind="picture"
-        size={this.getImageSize(columnLayout)}
-      />
-    )
+    const {components: {Text, Image, Collection, Button}, style, $block} = this.props
+
     return (
-      <section className={classNames(css.section, {[css['section--column']]: columnLayout})}>
-        <div className={css.section__inner}>
-          <article className={css.article}>
-            {this.getOptionValue('image_wrapper')
-              ? this.wrapImage(ImageComponent)
-              : ImageComponent}
-            <div className={css.article__content}>
-              {this.getModifierValue('title') && (
-                <h1 className={css.article__title}>
-                  <Text bind="title" />
-                </h1>
-              )}
-              {this.getModifierValue('subtitle') && (
-                <p className={css.article__subtitle}>
-                  <Text bind="subtitle" />
-                </p>
-              )}
-              {this.getModifierValue('text') && (
-                <p className={css.article__text}>
-                  <Text bind="text" />
-                </p>
-              )}
-              {this.getModifierValue('socialIcons') && (
-                <div className={css.article__socials}>
-                  <h2 className={css['social-title']}>Follow us: </h2>
-                  <SocialIcons bind="socialIcons" />
-                </div>
-              )}
-              {showButtonGroups && (
-                <div className={css['btns-group']}>
-                  {this.getModifierValue('link') && <Button className={css.link} bind="link" />}
-                  {this.getModifierValue('button') && (
-                    <Button
-                      className={classNames(
-                        css.button,
-                        css['button--primary'],
-                        css['button--size-md'],
-                      )}
-                      bind="button"
-                    />
-                  )}
-                </div>
-              )}
-            </div>
+      <section className={style.section}>
+        <Media query="(min-width: 992px)">
+          <article className={style.item} role="tabpanel">
+            <Image
+              wrapperClassName={style['item__picture-wrapper']}
+              pictureClassName={style.item__picture}
+              imageClassName={style.item__image}
+              bind={`items[${this.state.active}].itemPicture`}
+              size={
+                {
+                'min-width: 992px': 470,
+                'min-width: 768px': 300,
+                'min-width: 480px': 800,
+                'min-width: 320px': 480,
+                }
+              }
+            />
+            <Text bind={`items[${this.state.active}].itemPosition`} className={style.item__position} tagName="small" />
+            <Text bind={`items[${this.state.active}].itemTitle`} className={style.item__title} tagName="h2" />
+            <Text bind={`items[${this.state.active}].itemContent`} className={style.item__text} tagName="p" />
+            <Text bind={`items[${this.state.active}].itemEmail`} />
+            <Button
+              buttonClassName={style.button}
+              linkClassName={style.link}
+              bind={`items[${this.state.active}].itemLink`}
+            />
           </article>
-        </div>
+
+          <Collection
+            className={style.tabs}
+            bind="items"
+            Item={this.collectionItem}
+            itemProps={{
+              modifier: $block.modifier,
+            }}
+          />
+        </Media>
       </section>
     )
   }
 }
 
-Block.components = _.pick(['Text', 'Image', 'Button', 'SocialIcons'])($editor.components)
+Block.components = _.pick(['Text', 'Image', 'Collection', 'Button'])($editor.components)
 
 Block.defaultContent = {
   title: 'About The Company',
-  'text-1': 'Follow us:',
-  subtitle:
-    'Our Company is the world’s leading manufacturer. We are also a leading financial services provider.',
-  text:
-    'We are in it for the long haul—for our customers and for our world. Our customers can be found in virtually every corner of the earth, and we realize our success comes directly from helping our customers be successful. We take seriously our responsibility to give back to the communities in which we work and live.',
-  picture: {
-    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-    alt: 'Picture about the company',
-  },
-  button: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
+  items: [
+    {
+      itemTitle: {
+        content: 'Andrew Shimmer',
+        type: 'heading',
+      },
+      itemContent: {
+        content: 'Andrew Shimmer has a 15-year experience in IT and marketing. He’s been successfully running companies specializing in high technologies. He won a bunch of international awards due to the excellent leadership skills and prominent achievements in business.',
+        type: 'text',
+      },
+      itemPosition: {
+        content: 'CEO',
+        type: 'caption',
+      },
+      itemEmail: {
+        content: '<a href="mailto:mysite@weblium.com">mysite@weblium.com</a>',
+        type: 'caption',
+      },
+      itemPicture: {
+        alt: 'Andrew Shimmer photo',
+      },
+      itemLink: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
         },
+        textValue: 'Learn more',
+        type: 'link',
       },
     },
-    textValue: 'Contact us',
-  },
-  link: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
+    {
+      itemTitle: {
+        content: 'Tomas Abbar',
+        type: 'heading',
+      },
+      itemContent: {
+        content: '2Andrew Shimmer has a 15-year experience in IT and marketing. He’s been successfully running companies specializing in high technologies. He won a bunch of international awards due to the excellent leadership skills and prominent achievements in business.',
+        type: 'text',
+      },
+      itemPosition: {
+        content: 'Product Manager',
+        type: 'caption',
+      },
+      itemEmail: {
+        content: '<a href="mailto:mysite@weblium.com">mysite@weblium.com</a>',
+        type: 'caption',
+      },
+      itemPicture: {
+        alt: 'Andrew Shimmer photo',
+      },
+      itemLink: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
         },
+        textValue: 'Learn more',
+        type: 'link',
       },
     },
-    textValue: 'More about us',
-  },
-  socialIcons: {
-    networks: [
-      {
-        id: 'facebook',
-        name: 'Facebook',
-        url: 'http://facebook.com/',
-      },
-      {
-        id: 'instagram',
-        name: 'Instagram',
-        url: 'http://instagram.com/',
-      },
-      {
-        id: 'youtube',
-        name: 'YouTube',
-        url: 'http://youtube.com/',
-      },
-    ],
-    target: '_blank',
-    design: {
-      border: 'circle',
-      innerFill: true,
-      preset: 'preset001',
-      padding: 20,
-      color: '',
-      sizes: [10, 20, 30, 40],
-      size: '40px',
-    },
-  },
+  ],
 }
 
 Block.modifierScheme = {
-  button: {defaultValue: true, label: 'Contact us button', type: 'checkbox'},
-  link: {defaultValue: false, label: 'About us link', type: 'checkbox'},
-  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Subtitle', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
   title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
