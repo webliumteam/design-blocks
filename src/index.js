119c3bf93b738c728ef2da039659a4e712dbe0db
tangerine69 <mrtangerine69@gmail.com>
2018-03-14 13:22:37 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,203 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  logo: {
+    image: {
+      resourceRef: 'Logo.png',
+      alt: 'Logo',
+    },
+  },
+  description: {
+    type: 'text',
+    content: 'Our company will go the extra mile to analyze the particular project in detail, and offer a business solution that will meet your requirements.',
+  },
+  menu: [
+    {
+      id: 'about',
+      metadata: {
+        displayName: 'About',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'projects',
+      metadata: {
+        displayName: 'Projects',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'testimonials',
+      metadata: {
+        displayName: 'Testimonials',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'careers',
+      metadata: {
+        displayName: 'Careers',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'safety',
+      metadata: {
+        displayName: 'Safety',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'blog',
+      metadata: {
+        displayName: 'Blog',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        displayName: 'Services',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'special-offers',
+      metadata: {
+        displayName: 'Special Offers',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'contacts',
+      metadata: {
+        displayName: 'Contacts',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+  follow: {
+    type: 'heading',
+    content: 'Follow us',
+  },
+  socialIcons: {
+    networks: [
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: null,
+      innerFill: true,
+      preset: 'preset001',
+      offset: 0,
+      color: '#9e9e9e',
+      sizes: [10, 20, 30, 40],
+      size: 35,
+    },
+  },
+  copyright: {
+    type: 'caption',
+    content: '© Produced by Weblium. All Rights Reserved',
+  },
+  additional: {
+    type: 'caption',
+    content: 'Demo Preview for Plugin LLC Electrician Concept',
+  },
+  background: {
+    type: 'color',
+    color: '#252936',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  description: {defaultValue: false, label: 'Company main text', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
