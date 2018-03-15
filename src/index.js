9c3bdf1a1e3e739f5eab355ba05faa88fb65f214
artemvasilkin <artemvasilkin14@gmail.com>
2018-03-15 11:11:07 +0200

    update content and styles

--- src/index.js
+++ src/index.js
@@ -9,4 +9,33 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
+ExtendedWireframe.defaultContent = {
+  ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '0% -50%',
+    },
+    resourceRef: 'bg-header.jpg',
+    overlay: {
+      type: 'color',
+      color: 'dark-accent-color',
+      opacity: 0.76,
+    },
+  },
+  logo: {
+    image: {
+      resourceRef: 'asam.png',
+      alt: 'asam.',
+    },
+  },
+  theme: 'dark',
+}
+
+ExtendedWireframe.modifierScheme = {
+  ...Component.modifierScheme,
+  button: {defaultValue: false, label: 'Button', type: 'hidden'},
+}
+
 export default ExtendedWireframe
