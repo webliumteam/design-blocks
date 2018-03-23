2aa73296911f94ca02b96c03f7c947e79148ffbc
Sabina <sabina.mamedova90@gmail.com>
2018-03-23 18:30:14 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -10,11 +10,144 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  ...Component.defaultContent,
+  // background: {
+  //   type: 'color',
+  //   color: '#d8d8d8',
+  // },
+
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'slide.jpg',
+  },
+  cover: [
+    {
+      title: {
+        content: 'Think In Motion',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We create. We Develop. We Inspire.',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Get in Touch',
+        type: 'primary',
+      },
+      'cta-2': {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Who We Are',
+        type: 'secondary',
+      },
+    },
+    {
+      title: {
+        content: 'Think In Motion',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We create. We Develop. We Inspire.',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Get in Touch',
+        type: 'primary',
+      },
+      'cta-2': {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Who We Are',
+        type: 'secondary',
+      },
+    },
+    {
+      title: {
+        content: 'Think In Motion',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We create. We Develop. We Inspire.',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Get in Touch',
+        type: 'primary',
+      },
+      'cta-2': {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Who We Are',
+        type: 'secondary',
+      },
+    },
+  ],
 }
 
+
 ExtendedWireframe.modifierScheme = {
-  ...Component.modifierScheme,
+  arrows: {defaultValue: false, label: 'Navigation arrows', type: 'checkbox'},
+  dots: {defaultValue: true, label: 'Navigation indicators', type: 'checkbox'},
+  'primary-button': {defaultValue: true, label: 'Primary button', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'hidden'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
