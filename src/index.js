4738e36c3df325a38044eff718a62055d1b14586
nmelnychenko <mykola.melnichenko@gmail.com>
2018-03-26 17:29:30 +0300

    start work/ui-kit bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-series-3-cover/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,45 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+    overlay: {
+      type: 'color',
+      color: '#161619',
+      opacity: 0.2,
+    },
+  },
+  formBackground: {
+    type: 'color',
+    color: 'rgb(19, 33, 143, 0.78)',
+  },
+  title: {
+    content: 'Safe and Secure<br>Transportation',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Products and services of the highest quality',
+    type: 'subtitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'header-alignment': {
+    children: [
+      {id: 'left', label: 'Left'},
+      {id: 'right', label: 'Right'},
+    ],
+    defaultValue: 'right',
+    name: 'Header alignment',
+    type: 'radio-button-group',
+    style: 'buttons',
+  },
 }
 
 export default ExtendedWireframe
