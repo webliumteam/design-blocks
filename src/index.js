025e5ce5e6f83923ac116bba2d81d78909fb34bf
artemvasilkin <artemvasilkin14@gmail.com>
2018-03-26 13:45:15 +0300

    ready for QA

--- src/index.js
+++ src/index.js
@@ -26,61 +26,25 @@ ExtendedWireframe.defaultContent = {
   },
   cover: [
     {
-      title: 'Type Something',
-      subtitle: 'Type Something',
+      ...Component.defaultContent.cover[0],
       cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'MAIN BUTTON (L)',
-        type: 'primary',
+        ...Component.defaultContent.cover[0].cta,
+        size: 'lg',
       },
-      id: 'c4e3de5f-776a-405b-a034-dc2307069714',
     },
     {
-      title: 'Type Something',
-      subtitle: 'Type Something',
+      ...Component.defaultContent.cover[1],
       cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'MAIN BUTTON (L)',
-        type: 'primary',
+        ...Component.defaultContent.cover[1].cta,
+        size: 'lg',
       },
-      id: '73263c9a-8d0e-41dc-85e1-66c6db9ea9d3',
     },
     {
-      title: 'Type Something',
-      subtitle: 'Type Something',
+      ...Component.defaultContent.cover[2],
       cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'MAIN BUTTON (L)',
-        type: 'primary',
+        ...Component.defaultContent.cover[2].cta,
+        size: 'lg',
       },
-      id: 'b5f64fb3-d94e-4ecf-b7ff-a4a5700a2457',
     },
   ],
 }
