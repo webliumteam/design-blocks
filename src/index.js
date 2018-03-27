ce219eb38bc288ac510f497db52caf79b51f5a28
Slipachenko <slipachenko@Mac-mini-Monster-2.local>
2018-03-27 16:53:34 +0300

    change style

--- src/index.js
+++ src/index.js
@@ -22,192 +22,9 @@ ExtendedWireframe.defaultContent = {
     },
   },
   description: {
-    type: 'text',
-    brightness: 30,
-    content: 'Company description about how awesome we are. Company description about how awesome we are. Company description about how awesome we are.',
+    ...Component.defaultContent.description,
+    brightness: 10,
   },
-  menu: [
-    {
-      id: 'about',
-      metadata: {
-        displayName: 'About',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'services',
-      metadata: {
-        displayName: 'Services',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'careers',
-      metadata: {
-        displayName: 'Careers',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'projects',
-      metadata: {
-        displayName: 'Projects',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'testimonials',
-      metadata: {
-        displayName: 'Testimonials',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'blog',
-      metadata: {
-        displayName: 'Blog',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'approach',
-      metadata: {
-        displayName: 'Our approach',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'industries',
-      metadata: {
-        displayName: 'Industries',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'team',
-      metadata: {
-        displayName: 'Our team',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'resources',
-      metadata: {
-        displayName: 'Resources',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'press',
-      metadata: {
-        displayName: 'Press',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-    {
-      id: 'contacts-us',
-      metadata: {
-        displayName: 'Contact us',
-        clickAction: {
-          action: 'page',
-          target: '_self',
-          actions: {
-            page: '',
-            link: '',
-            block: '',
-          },
-        },
-      },
-    },
-  ],
   socialIcons: {
     networks: [
       {
@@ -237,20 +54,18 @@ ExtendedWireframe.defaultContent = {
       innerFill: false,
       preset: 'preset001',
       offset: 15,
-      color: '#909aa3',
+      color: '#b3bbc2',
       sizes: [10, 20, 30, 40],
       size: 30,
     },
   },
   copyright: {
-    type: 'text',
-    content: 'Produced by Weblium.com. All rights Reserved',
-    brightness: 30,
+    ...Component.defaultContent.copyright,
+    brightness: 40,
   },
   additional: {
-    type: 'text',
-    content: 'Demo preview for {Template name} {Concept name}',
-    brightness: 30,
+    ...Component.defaultContent.additional,
+    brightness: 40,
   },
 }
 
