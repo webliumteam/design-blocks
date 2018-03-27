342ff20f8b765d0ce69c101d9d6dbd757bcb186f
artemvasilkin <artemvasilkin14@gmail.com>
2018-03-27 17:09:33 +0300

    ready for QA

--- src/index.js
+++ src/index.js
@@ -16,86 +16,64 @@ ExtendedWireframe.defaultContent = {
     type: 'image',
     position: {
       type: 'cover',
-      cover: '50% 50%',
+      cover: '25% 50%',
     },
     resourceRef: 'img-1-pizza.png',
   },
   cover: [
     {
-      title: 'Pizza',
-      subtitle: 'whith Seafood',
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Order online',
-        type: 'primary',
+      ...Component.defaultContent.cover[0],
+      title: {
+        ...Component.defaultContent.cover[0].title,
+        content: 'QUANTUM COMPANY',
       },
-      id: 'c4e3de5f-776a-405b-a034-dc2307069714',
-      background: {
-        type: 'image',
-        position: {
-          type: 'cover',
-          cover: '50% 50%',
-        },
-        resourceRef: 'img-1-pizza.png',
+      subtitle: {
+        ...Component.defaultContent.cover[0].subtitle,
+        content: 'WE IMPLEMENT INNOVATIVE PROJECTS',
+        color: 'light-accent-color',
+      },
+      cta: {
+        ...Component.defaultContent.cover[0].cta,
+        size: 'lg',
       },
     },
     {
-      title: 'Meet the legend',
-      subtitle: 'burger california',
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Request a quote',
-        type: 'primary',
+      ...Component.defaultContent.cover[0],
+      title: {
+        ...Component.defaultContent.cover[0].title,
+        content: 'QUANTUM COMPANY',
       },
-      id: '73263c9a-8d0e-41dc-85e1-66c6db9ea9d3',
+      subtitle: {
+        ...Component.defaultContent.cover[0].subtitle,
+        content: 'WE IMPLEMENT INNOVATIVE PROJECTS',
+        color: 'light-accent-color',
+      },
+      cta: {
+        ...Component.defaultContent.cover[0].cta,
+        size: 'lg',
+      },
     },
     {
-      title: 'burger',
-      subtitle: 'the Mexican',
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Request a quote',
-        type: 'primary',
+      ...Component.defaultContent.cover[0],
+      title: {
+        ...Component.defaultContent.cover[0].title,
+        content: 'QUANTUM COMPANY',
       },
-      id: 'b5f64fb3-d94e-4ecf-b7ff-a4a5700a2457',
+      subtitle: {
+        ...Component.defaultContent.cover[0].subtitle,
+        content: 'WE IMPLEMENT INNOVATIVE PROJECTS',
+        color: 'light-accent-color',
+      },
+      cta: {
+        ...Component.defaultContent.cover[0].cta,
+        size: 'lg',
+      },
     },
   ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  arrows: {defaultValue: true, label: 'Navigation arrows', type: 'checkbox'},
-  dots: {defaultValue: true, label: 'Navigation indicators', type: 'checkbox'},
-  'primary-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
-  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
-
 }
 
 export default ExtendedWireframe
