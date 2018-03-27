65c1a65289f7c9b78cbc7d03e9e445eda6d6094d
KonstantinKH <kkhomenko9@gmail.com>
2018-03-27 16:24:28 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -11,10 +11,185 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  logo: {
+    image: {
+      resourceRef: 'logo.png',
+      alt: 'logo',
+    },
+  },
+  background: {
+    type: 'color',
+    color: '#13194d',
+  },
+  menu: [
+    {
+      id: 'about',
+      metadata: {
+        displayName: 'Industries',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'Case Studies',
+      metadata: {
+        displayName: 'Testimonials',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'team',
+      metadata: {
+        displayName: 'Investors',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        displayName: 'Services',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'careers',
+      metadata: {
+        displayName: 'About Us',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'projects',
+      metadata: {
+        displayName: 'News',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'blog',
+      metadata: {
+        displayName: 'solutions',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+    {
+      id: 'approach',
+      metadata: {
+        displayName: 'Careers',
+        clickAction: {
+          action: 'page',
+          target: '_self',
+          actions: {
+            page: '',
+            link: '',
+            block: '',
+          },
+        },
+      },
+    },
+  ],
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+      {
+        id: 'linkedin',
+        name: 'LinkedIn',
+        url: 'http://linkedin.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'circle',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 15,
+      color: '#9b9b9b',
+      sizes: [10, 20, 30, 40],
+      size: 30,
+    },
+  },
+  copyright: {
+    type: 'caption',
+    content: 'Produced by Weblium. All Rights Reserved',
+  },
+  additional: {
+    type: 'caption',
+    content: 'Demo Preview for Acrozz Logistics Company Concept',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  description: {defaultValue: false},
 }
 
 export default ExtendedWireframe
