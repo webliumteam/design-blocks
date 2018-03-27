5c027ace5e6e89350c2a0c3a324df8122d3b6963
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-03-27 14:59:44 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,34 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Ask our experts<br> about your freight',
+    color: 'var(--ui-light-shade-color)',
+  },
+  description: {
+    ...Component.defaultContent.description,
+    content: 'Our experts are always ready to help. Get in touch with us and we\'ll find the right solution to meet your needs.',
+    color: 'var(--ui-light-shade-color)',
+  },
+  cta: {
+    ...Component.defaultContent.cta,
+    textValue: 'Get a quote',
+    type: 'primary',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
