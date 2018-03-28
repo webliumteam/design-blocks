4be143f44187ed112494c73f6d6e2b04e965d01b
nataliadiadkova <nataliadiadkova@gmail.com>
2018-03-28 10:41:52 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,77 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  awards: [
+    {
+      title: {
+        content: 'R&D Leader in Tech (2017)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'awards-img-1.png',
+        alt: 'R&D Leader in Tech photo',
+      },
+    },
+    {
+      title: {
+        content: 'Most Innovative B2B Solutions (2016)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'awards-img-2.png',
+        alt: 'Most Innovative B2B Solutions photo',
+      },
+    },
+    {
+      title: {
+        content: 'Best Service Provider (2014)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'awards-img-3.png',
+        alt: 'Best Service Provider',
+      },
+    },
+    {
+      title: {
+        content: 'Leader of Innovation (2012)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'awards-img-4.png',
+        alt: 'Leader of Innovation photo',
+      },
+    },
+    {
+      title: {
+        content: 'Leader of Innovation (2012)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'awards-img-5.png',
+        alt: 'Leader of Innovation photo',
+      },
+    },
+    {
+      title: {
+        content: 'Leader of Innovation (2012)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'awards-img-6.png',
+        alt: 'Leader of Innovation photo',
+      },
+    },
+  ],
+  title: {
+    content: 'Our Certificates',
+    type: 'blockTitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: false, label: 'Awards title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
