fac81e6836f0a5ecef2814bc4936a0d2a968548f
artemvasilkin <artemvasilkin14@gmail.com>
2018-03-30 16:33:46 +0300

    add bgs to collection

--- src/component.js
+++ src/component.js
@@ -87,135 +87,145 @@ class Block extends React.Component {
 Block.components = _.pick(['Collection', 'Text', 'Button', 'Logo'])($editor.components)
 
 Block.defaultContent = {
-  partners: [
-    {
-      title: {
-        content: 'Samsung',
-        type: 'heading',
-      },
-      desc: {
-        content: '<span style="font-weight: bold">Samsung.</span> It doesn’t need a lot of words. High-quality tech products with superior design.',
-        type: 'text',
-      },
-      logo: {
-        text: {
-          value: 'Samsung',
-          tagName: 'h2',
-          fontSize: 24,
+  partners: {
+    background: {
+      'nth-child': [
+        ['n', {
+          color: 'light-shade-color',
+          type: 'color',
+        }],
+      ],
+    },
+    items: [
+      {
+        title: {
+          content: 'Samsung',
+          type: 'heading',
         },
-      },
-      picture: {
-        alt: 'Samsung logo',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
+        desc: {
+          content: '<span style="font-weight: bold">Samsung.</span> It doesn’t need a lot of words. High-quality tech products with superior design.',
+          type: 'text',
+        },
+        logo: {
+          text: {
+            value: 'Samsung',
+            tagName: 'h2',
+            fontSize: 24,
           },
         },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-    },
-    {
-      title: {
-        content: 'Ford',
-        type: 'heading',
-      },
-      desc: {
-        content: '<span style="font-weight: bold">Ford.</span> This is our moving force, literally! 100% reliable and credible.',
-        type: 'text',
-      },
-      logo: {
-        text: {
-          value: 'Ford',
-          tagName: 'h2',
-          fontSize: 24,
+        picture: {
+          alt: 'Samsung logo',
         },
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
+        cta: {
+          actionConfig: {
+            action: 'link',
+            actions: {
+              link: {
+                type: '',
+                innerPage: '',
+                url: '',
+              },
             },
           },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-    },
-    {
-      title: {
-        content: 'Medical Family',
-        type: 'heading',
-      },
-      desc: {
-        content: '<span style="font-weight: bold">Medical Family.</span> Ensuring good health and medical care for our team for 5 years.',
-        type: 'text',
-      },
-      logo: {
-        text: {
-          value: 'Medical Family',
-          tagName: 'h2',
-          fontSize: 24,
+          textValue: 'Learn more',
+          type: 'link',
         },
       },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
+      {
+        title: {
+          content: 'Ford',
+          type: 'heading',
+        },
+        desc: {
+          content: '<span style="font-weight: bold">Ford.</span> This is our moving force, literally! 100% reliable and credible.',
+          type: 'text',
+        },
+        logo: {
+          text: {
+            value: 'Ford',
+            tagName: 'h2',
+            fontSize: 24,
           },
         },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-    },
-    {
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: '<span style="font-weight: bold">Coffee Break Now.</span> We love their delicious coffee and flavored cakes!',
-        type: 'text',
-      },
-      logo: {
-        text: {
-          value: 'Coffee Break Now',
-          tagName: 'h2',
-          fontSize: 24,
-        },
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
+        cta: {
+          actionConfig: {
+            action: 'link',
+            actions: {
+              link: {
+                type: '',
+                innerPage: '',
+                url: '',
+              },
             },
           },
+          textValue: 'Learn more',
+          type: 'link',
         },
-        textValue: 'Learn more',
-        type: 'link',
       },
-    },
-  ],
+      {
+        title: {
+          content: 'Medical Family',
+          type: 'heading',
+        },
+        desc: {
+          content: '<span style="font-weight: bold">Medical Family.</span> Ensuring good health and medical care for our team for 5 years.',
+          type: 'text',
+        },
+        logo: {
+          text: {
+            value: 'Medical Family',
+            tagName: 'h2',
+            fontSize: 24,
+          },
+        },
+        cta: {
+          actionConfig: {
+            action: 'link',
+            actions: {
+              link: {
+                type: '',
+                innerPage: '',
+                url: '',
+              },
+            },
+          },
+          textValue: 'Learn more',
+          type: 'link',
+        },
+      },
+      {
+        title: {
+          content: 'Coffee Break Now',
+          type: 'heading',
+        },
+        desc: {
+          content: '<span style="font-weight: bold">Coffee Break Now.</span> We love their delicious coffee and flavored cakes!',
+          type: 'text',
+        },
+        logo: {
+          text: {
+            value: 'Coffee Break Now',
+            tagName: 'h2',
+            fontSize: 24,
+          },
+        },
+        cta: {
+          actionConfig: {
+            action: 'link',
+            actions: {
+              link: {
+                type: '',
+                innerPage: '',
+                url: '',
+              },
+            },
+          },
+          textValue: 'Learn more',
+          type: 'link',
+        },
+      },
+    ],
+  },
   title: {
     content: 'Partners',
     type: 'blockTitle',
