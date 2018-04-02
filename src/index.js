04330a535faa2e69025e7506bb360d9936928986
Slipachenko <slipachenko@Mac-mini-Monster-2.local>
2018-04-02 15:06:46 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -11,10 +11,32 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: '#dddfe8',
+  },
+  title: {
+    content: 'RAM&nbspUAV',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'UNMANNED AERIAL SYSTEM',
+    type: 'subtitle',
+  },
+  picture: {
+    resourceRef: 'plane1.png',
+    alt: 'Picture about the company',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'article-picture': {defaultValue: true, label: 'Article picture', type: 'hidden'},
+  button: {defaultValue: false, label: 'Button', type: 'hidden'},
+  'additional-button': {defaultValue: false, label: 'Additional button', type: 'hidden'},
+  socialIcons: {defaultValue: false, label: 'Social media buttons', type: 'hidden'},
+  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
+  text: {defaultValue: false, label: 'Company main text', type: 'hidden'},
 }
 
 export default ExtendedWireframe
