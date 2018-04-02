15bc7cd9a85e0cad08c48fd9906ac5ffec0af90b
artemvasilkin <artemvasilkin14@gmail.com>
2018-04-02 17:17:20 +0300

    ready for QA

--- src/index.js
+++ src/index.js
@@ -11,10 +11,36 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Want to Work With Us?',
+  },
+  description: {
+    ...Component.defaultContent.description,
+    content: 'Get a full-scale analysis of your business and recommended <br> solutions to increase your profits.',
+  },
+  cta: {
+    ...Component.defaultContent.cta,
+    type: 'primary',
+    textValue: 'Request an analysis',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {
+    ...Component.modifierScheme.subtitle,
+    defaultValue: true,
+    type: 'checkbox',
+  },
 }
 
 export default ExtendedWireframe
