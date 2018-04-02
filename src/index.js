4f6584c4e56dd5d6d5c435ff2389ad000557f4bf
Slipachenko <slipachenko@Mac-mini-Monster-2.local>
2018-04-02 18:15:00 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -11,10 +11,26 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
+  address: {defaultValue: false, label: 'Address text block', type: 'hidden'},
+  email: {defaultValue: false, label: 'E-mail text block', type: 'hidden'},
+  logo: {defaultValue: false, label: 'Logo', type: 'hidden'},
+  map: {defaultValue: true, label: 'Map', type: 'checkbox'},
+  phone: {defaultValue: false, label: 'Phone text block', type: 'hidden'},
+  social: {defaultValue: false, label: 'Social Media Buttons', type: 'hidden'},
+  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
