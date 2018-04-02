1a84abc1f522361a4de8e31ed09be8b69a922395
Slipachenko <slipachenko@Mac-mini-Monster-2.local>
2018-04-02 17:30:31 +0300

    add style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-zapdos-about/src/component'
+import Component from 'wireframe-series-2-about/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,52 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  // background: {
+  //   type: 'image',
+  //   position: {
+  //     type: 'cover',
+  //     cover: '50% 50%',
+  //   },
+  //   resourceRef: 'bg.jpg',
+  // },
+  title: {
+    content: 'RAM UAV',
+    type: 'heroTitle',
+  },
+  'top-caption': {
+    content: 'About',
+    type: 'heading',
+  },
+  text: {
+    content: 'Our team consists of highly motivated and skilled specialists who know how to deal with any issue that you may come across. This creates a basis for lasting relationships with our clients built on trust and mutual understanding. <br> <br> We are devoted to creating unique and innovative solutions along with the high-quality supporting services. ',
+    type: 'text',
+  },
+  'button-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn More',
+    type: 'primary',
+    size: 'lg',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  heading: {defaultValue: false, label: 'Company title', type: 'checkbox'},
+  subheading: {defaultValue: false, label: 'Company description', type: 'checkbox'},
+  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
+  'social-icons': {defaultValue: false, label: 'Social media buttons', type: 'hidden'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
+  'additional-button': {defaultValue: false, label: 'Additional button', type: 'hidden'},
+  'top-caption': {defaultValue: true, label: 'Top caption', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
