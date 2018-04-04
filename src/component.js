e6b328743433945bb83710bd0c768255c9a878e7
nmelnychenko <mykola.melnichenko@gmail.com>
2018-04-04 10:46:51 +0300

    published on co

--- src/component.js
+++ src/component.js
@@ -4,24 +4,65 @@ class Wireframe extends React.Component {
   static propTypes = {
     components: PropTypes.object.isRequired,
     style: PropTypes.object.isRequired,
+    $block: PropTypes.object.isRequired,
   }
 
+  getModifierValue = path => _.get(['modifier', path], this.props.$block)
+
   render() {
-    const {components: {Text}, style} = this.props
+    const {components: {Text, Button}, style} = this.props
+    const headerContent = this.getModifierValue('title') || this.getModifierValue('subtitle')
 
     return (
-      <Text bind="title" className={style.title} tagName="h1" />
+      <section className={style.section}>
+        <div className={style.section__inner}>
+          {headerContent && (
+            <div className={style.section__header}>
+              {this.getModifierValue('title') && (
+                <Text tagName="h1" className={style.title} bind="title" />
+              )}
+              {this.getModifierValue('subtitle') && (
+                <Text tagName="p" className={style.subtitle} bind="subtitle" />
+              )}
+            </div>
+          )}
+          <div className={style['btns-group']}>
+            <Button
+              className={style.button}
+              linkClassName={style.link}
+              bind="cta"
+            />
+          </div>
+        </div>
+      </section>
     )
   }
 }
 
-Wireframe.components = _.pick(['Text'])($editor.components)
+Wireframe.components = _.pick(['Text', 'Button'])($editor.components)
 
 Wireframe.defaultContent = {
+  background: {
+    type: 'color',
+    color: 'light-accent-color',
+  },
   title: {
-    content: 'Hello world',
+    content: 'Want to work with us?',
     type: 'blockTitle',
   },
+  subtitle: {
+    content: 'Press the button and receive a full analysis of your business within 2 days!',
+    type: 'text',
+  },
+  cta: {
+    textValue: 'Request a quote',
+    type: 'primary',
+  },
+}
+
+Wireframe.modifierScheme = {
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default Wireframe
