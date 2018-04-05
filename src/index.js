a6caf51d4b9c8b855904435af12b67d312dd6206
JulBeet <julbeet@gmail.com>
2018-04-05 20:09:17 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,6 +11,56 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'lior-mazliah-111379-unsplash.jpg',
+
+    overlay: {
+      type: 'color',
+      color: '#000',
+      opacity: 0.8,
+    },
+  },
+  logo: {
+    text: {
+      value: 'Quantum Company',
+      fontSize: 24,
+      color: 'var(--ui-brand-color)',
+    },
+  },
+  socialIcons: {
+    networks: [
+      {
+        id: 'facebook',
+        name: 'Facebook',
+        url: 'http://facebook.com/',
+      },
+      {
+        id: 'twitter',
+        name: 'Twitter',
+        url: 'http://twitter.com/',
+      },
+      {
+        id: 'instagram',
+        name: 'Instagram',
+        url: 'http://instagram.com/',
+      },
+    ],
+    target: '_blank',
+    design: {
+      border: 'softRect',
+      innerFill: true,
+      preset: 'preset001',
+      offset: 15,
+      color: 'var(--ui-dark-accent-color)',
+      sizes: [10, 20, 30, 40],
+      size: 30,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
