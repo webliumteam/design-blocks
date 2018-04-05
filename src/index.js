cd649b35a6689309d36a6a8c549366c9c6eac5ab
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-04-05 21:18:01 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,69 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'img-bg.jpg',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'transparent',
+  },
+  title: {
+    content: 'Get in Touch with Our Logistics Expert',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Complete the form below and receive a full analysis of your business within 2 days!',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: '',
+        placeholder: 'Name',
+        required: false,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: '',
+        placeholder: 'E-mail',
+        required: false,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: '',
+        placeholder: 'Phone',
+        required: false,
+      },
+      {
+        type: 'textArea',
+        id: 'contactForm_message',
+        title: '',
+        placeholder: 'Message',
+        required: false,
+      },
+    ],
+    submitButton: {
+      title: 'Send',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
