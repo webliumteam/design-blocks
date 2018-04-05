16f4b3f5e61d5f93de57ee8d7b20a410b33ad22c
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-04-05 22:38:50 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,85 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'img-bg.jpg',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'light-shade-color',
+  },
+  title: {
+    content: 'BeZee Company',
+    type: 'heroTitle',
+  },
+  subtitle: {
+    content: 'Products and services of the highest quality',
+    type: 'subtitle',
+  },
+  formTitle: {
+    content: 'Want to work with us?',
+    type: 'heading',
+  },
+  formDescription: {
+    content: 'Send a request and get a complete list of our offers!',
+    type: 'subheading',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: 'Phone number ',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: '',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Request a quote',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
+  headerButton: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Our services',
+    type: 'primary',
+    size: 'md',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
