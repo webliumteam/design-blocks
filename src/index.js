37ee6d71a221bd1cb7b2a47985bc2e2b5cc06f12
KonstantinKH <kkhomenko9@gmail.com>
2018-04-06 00:45:02 +0300

    changed

--- src/index.js
+++ src/index.js
@@ -11,10 +11,69 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Quantum E-commerce',
+        type: 'heading',
+      },
+      text: {
+        content: 'Multipurpose program that will perfectly suit online stores and online retail business. You can add up to 2000 items with a detailed description, characteristics, and photos. Also, the shopping cart, clients’ feedback, rating, and related items option are available.',
+        type: 'text',
+      },
+      picture: {
+        src: 'sharon-mccutcheon-524649-unsplash.jpg',
+        alt: 'Quantum E-commerce illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Aftersales',
+        type: 'heading',
+      },
+      text: {
+        content: 'Aftersales assistance that includes marketing campaigns, additional digital solutions, and educational courses. Our specialists will help to find out what will work for your business and decide on the steps that are to be taken immediately. ',
+        type: 'text',
+      },
+      picture: {
+        src: 'boris-smokrovic-174784-unsplash.jpg',
+        alt: 'Quantum Aftersales illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true},
 }
 
 export default ExtendedWireframe
