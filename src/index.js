8eb2a3c57438f603dd8748c4ed965a112eaa01bd
KonstantinKH <kkhomenko9@gmail.com>
2018-04-06 02:22:07 +0300

    changed

--- src/index.js
+++ src/index.js
@@ -11,6 +11,120 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Service 1',
+        type: 'heading',
+      },
+      description: {
+        content: 'Multipurpose assistance for online stores and offline retail businesses.',
+        type: 'text',
+      },
+      picture: {
+        src: 'jamie-street-94242-unsplash.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Service 2',
+        type: 'heading',
+      },
+      description: {
+        content: 'Developing marketing and business strategy to help company grow fast.',
+        type: 'text',
+      },
+      picture: {
+        src: 'sora-sagano-582926-unsplash.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Service 3',
+        type: 'heading',
+      },
+      description: {
+        content: 'Building brand awareness for micro and small businesses to increase potential reach.',
+        type: 'text',
+      },
+      picture: {
+        src: 'marc-fulgar-59958-unsplash.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+    {
+      title: {
+        content: 'Service 4',
+        type: 'heading',
+      },
+      description: {
+        content: 'Aftersales support in marketing, sales, and staff training for enhanced performance.',
+        type: 'text',
+      },
+      picture: {
+        src: 'maarten-deckers-295105-unsplash.jpg',
+        alt: 'Service illustration photo',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'link',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
