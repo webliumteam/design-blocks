f284fc253dcb49484e49b81bc16e65e13190c12b
KonstantinKH <kkhomenko9@gmail.com>
2018-04-05 23:35:37 +0300

    changed

--- src/index.js
+++ src/index.js
@@ -11,10 +11,69 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  services: [
+    {
+      title: {
+        content: 'Manufacturing',
+        type: 'heading',
+      },
+      text: {
+        content: 'We use only high-quality materials for good manufacturing. All products are designed and manufactured in line with your preferences. Just choose the materials, send us design requirements, and we will contact with you shortly to discuss the details.',
+        type: 'text',
+      },
+      picture: {
+        src: 'khai-sze-ong-308080-unsplash.jpg',
+        alt: 'Manufacturing illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+    {
+      title: {
+        content: 'Delivery and Support',
+        type: 'heading',
+      },
+      text: {
+        content: 'We provide a package of services that ensure the aftersales technical service and diagnostics. We guarantee that our goods will please you for many years, and you will enjoy decent support of our specialists.',
+        type: 'text',
+      },
+      picture: {
+        src: 'andre-benz-243080-unsplash.jpg',
+        alt: 'Delivery and Support illustration',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true},
 }
 
 export default ExtendedWireframe
