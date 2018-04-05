eedfc7927f30ac2a890502c332d6db33d24d937f
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-04-05 17:50:13 +0300

    finish block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,62 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  items: [
+    {
+      image: {
+        resourceRef: 'pic1.jpg',
+        alt: 'Picture about the company',
+        overlay: {
+          type: 'color',
+          color: '#fff',
+          opacity: '0.5',
+        },
+      },
+      heading: {
+        type: 'heading',
+        content: 'Open-minded',
+      },
+      text: {
+        type: 'text',
+        content: 'We always look for new ideas, explore various spheres, and apply a positive approach',
+      },
+    },
+    {
+      image: {
+        resourceRef: 'pic2.jpg',
+        alt: 'Picture about the company',
+      },
+      heading: {
+        type: 'heading',
+        content: 'Long-term cooperation',
+      },
+      text: {
+        type: 'text',
+        content: 'We believe that effective cooperation should last for ages',
+      },
+    },
+    {
+      image: {
+        resourceRef: 'pic3.jpg',
+        alt: 'Picture about the company',
+      },
+      heading: {
+        type: 'heading',
+        content: 'Turnkey solutions',
+      },
+      text: {
+        type: 'text',
+        content: 'Every good or service we provide is empowered with the top-notch features to deliver the best result to our customers',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Why us description', type: 'checkbox'},
+  'main-button': {defaultValue: true, label: 'Primary button', type: 'checkbox'},
+  'additional-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
