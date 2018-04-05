37469fdfd92d63cfb78f2e434237b02ac0116e13
Kirill Donenko <kirill.donenko@gmail.com>
2018-04-06 01:57:23 +0300

    styles of block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,149 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    content: 'Careers',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'We’re always looking for talents who want to grow personally and professionally in our company. Please check the open positions: ',
+    type: 'subtitle',
+  },
+  items: [
+    {
+      icon: {
+        svg: "<svg viewBox='0 0 70 70' fill='currentColor'><circle cx='35' cy='35' r='35' /></svg>",
+        fill: '#676767',
+      },
+      heading: {
+        content: 'Web Developer',
+        type: 'heading',
+      },
+      employment: {
+        content: 'Full time',
+        type: 'text',
+      },
+      location: {
+        content: 'London',
+        type: 'text',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        type: 'link',
+        textValue: 'More about vacancy',
+      },
+    },
+    {
+      icon: {
+        svg: "<svg viewBox='0 0 70 70' fill='currentColor'><circle cx='35' cy='35' r='35' /></svg>",
+        fill: '#676767',
+      },
+      heading: {
+        content: '3D designer',
+        type: 'heading',
+      },
+      employment: {
+        content: 'Remote work',
+        type: 'text',
+      },
+      location: {
+        content: 'Amsterdam',
+        type: 'text',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        type: 'link',
+        textValue: 'More about vacancy',
+      },
+    },
+    {
+      icon: {
+        svg: "<svg viewBox='0 0 70 70' fill='currentColor'><circle cx='35' cy='35' r='35' /></svg>",
+        fill: '#676767',
+      },
+      heading: {
+        content: 'Office manager',
+        type: 'heading',
+      },
+      employment: {
+        content: 'Full time',
+        type: 'text',
+      },
+      location: {
+        content: 'Amsterdam',
+        type: 'text',
+      },
+      link: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        type: 'link',
+        textValue: 'More about vacancy',
+      },
+    },
+  ],
+  button: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    type: 'secondary',
+    size: 'md',
+    textValue: 'Learn more',
+  },
+  'top-caption': {
+    content: 'sapien ut libero',
+    type: 'text',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Careers description', type: 'checkbox'},
+  'item-icon': {defaultValue: true, label: 'Vacancy icon', type: 'checkbox'},
+  employment: {defaultValue: true, label: 'Type of employment (full-time/part-time)', type: 'checkbox'},
+  position: {defaultValue: true, label: 'Vacancy location', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
+  'top-caption': {defaultValue: false, label: 'Top caption', type: 'hidden'},
 }
 
 export default ExtendedWireframe
