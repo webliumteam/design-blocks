a6853ed99c315012cdbf67015eba011c02d36d10
JulBeet <julbeet@gmail.com>
2018-04-05 17:39:28 +0300

    deleted greyness

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'var(--ui-dark-shade-color)',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
