a5cd9800f1c9706be51e107d34571659789f627f
Igor Dejak <i.dejak@betinvest.com>
2018-04-05 16:18:42 +0300

    process for poliwag

--- src/index.js
+++ src/index.js
@@ -11,6 +11,50 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  steps: [
+    {
+      heading: {
+        content: 'Step 1. Choose a plan',
+        type: 'heading',
+      },
+      description: {
+        content: 'We provide you with full information about each plan. There are a few available options to choose from.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'process01.jpg',
+        alt: 'Illustraion for step 1',
+      },
+    },
+    {
+      heading: {
+        content: 'Step 2. Proceed with the payment',
+        type: 'heading',
+      },
+      description: {
+        content: 'We guarantee the security of all payments.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'process02.jpg',
+        alt: 'Illustraion for step 2',
+      },
+    },
+    {
+      heading: {
+        content: 'Step 3. Enjoy our products',
+        type: 'heading',
+      },
+      description: {
+        content: 'You will get access to all options immediately.',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'process03.jpg',
+        alt: 'Illustraion for step 3',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
