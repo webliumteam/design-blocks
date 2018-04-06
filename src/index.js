89943ebe7e1d93fcd3fe6e26a40ebf8b56351485
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-04-06 22:23:49 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,73 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'img-bg.jpg',
+    overlay: {
+      type: 'color',
+      color: '#000',
+      opacity: 0.5,
+    },
+  },
+  title: {
+    content: 'Quantum Company',
+    type: 'heroTitle',
+  },
+  subtitle: {
+    content: 'We can execute even the most unpredictable ideas! ',
+    type: 'subtitle',
+  },
+  'cta-1': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+    size: 'lg',
+  },
+  'cta-2': {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
+    size: 'lg',
+  },
+  topCaption: 'welcome',
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  align: {
+    children: [
+      {id: 'left', label: 'left'},
+      {id: 'center', label: 'center'},
+      {id: 'right', label: 'right'},
+    ],
+    defaultValue: 'center',
+    name: 'Aligning',
+    type: 'radio-button-group',
+    style: 'buttons',
+  },
 }
 
 export default ExtendedWireframe
