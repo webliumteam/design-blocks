9298d07f6aaa54ae878213e09dbf701804a78f10
VladReznik <vlad_escape@ukr.net>
2018-04-06 14:34:30 +0300

    FIrst

--- src/index.js
+++ src/index.js
@@ -11,6 +11,40 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'picture.jpg',
+  },
+  title: {
+    content: 'Subscribe Now',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'Enter your email and get our latest news, special offers, and useful content about our products and services.',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        placeholder: 'Email',
+        required: false,
+      },
+    ],
+    submitButton: {
+      title: 'subscribe',
+      textValue: 'subscribe',
+      type: 'primary',
+      size: 'lg',
+    },
+    className: 'form',
+    buttonClassName: 'button',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
