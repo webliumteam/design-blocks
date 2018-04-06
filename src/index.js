7f287b895be562537834f157b6d37b078a31410c
Alexey <C:\Users\User\AppData\Roaming\The Bat!\Mail>
2018-04-06 22:34:58 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,135 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'img-bg.jpg',
+    overlay: {
+      type: 'color',
+      color: '#000',
+      opacity: 0.55,
+    },
+  },
+  cover: [
+    {
+      title: {
+        content: 'Quantum Company',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We implement Innovative Projects',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+      'cta-2': {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'secondary',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Company',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We implement Innovative Projects',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+      'cta-2': {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'secondary',
+      },
+    },
+    {
+      title: {
+        content: 'Quantum Company',
+        type: 'heroTitle',
+      },
+      subtitle: {
+        content: 'We implement Innovative Projects',
+        type: 'subtitle',
+      },
+      cta: {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Request a quote',
+        type: 'primary',
+      },
+      'cta-2': {
+        actionConfig: {
+          action: 'link',
+          actions: {
+            link: {
+              type: '',
+              innerPage: '',
+              url: '',
+            },
+          },
+        },
+        textValue: 'Learn more',
+        type: 'secondary',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
