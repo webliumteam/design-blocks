6c375fc0837460783053467dde6896b89f7def7c
Kirill Donenko <kirill.donenko@gmail.com>
2018-04-06 09:14:11 +0300

    styles of block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,53 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  awards: [
+    {
+      title: {
+        content: 'Most Innovative Solutions for Companies (2016)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'img-1.jpg',
+        alt: 'Award "Most Innovative Solutions for Companies"',
+      },
+    },
+    {
+      title: {
+        content: 'Best Service Provider (2014)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'img-2.jpg',
+        alt: 'Award "Best Service Provider"',
+      },
+    },
+    {
+      title: {
+        content: 'Comfortable and Modern Office Design (2012)',
+        type: 'heading',
+      },
+      picture: {
+        resourceRef: 'img-3.jpg',
+        alt: 'Award "Comfortable and Modern Office Design"',
+      },
+    },
+  ],
+  title: {
+    content: 'Awards',
+    type: 'blockTitle',
+  },
+  subtitle: {
+    content: 'We’re proud of our awards! Look, what we’ve already won:',
+    type: 'subtitle',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Awards description', type: 'checkbox'},
+  heading: {defaultValue: true, label: 'Award title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
