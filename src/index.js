38ac6c948df2a3e05c972d452e6275e84bff970f
max-sindi <britva.okkamaa@gmail.com>
2018-04-06 19:20:15 +0300

    design done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,74 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  contacts: [
+    {
+      title: {
+        content: 'Head office in London',
+        type: 'heading',
+      },
+      address: {
+        content: 'Level 17, Dashwood House<br>London EC2M 1QS ',
+        type: 'text',
+      },
+      phone: {
+        content: '<a href="tel:2221111111">(222) 111 11 11</a>',
+        type: 'text',
+      },
+      email: {
+        content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'item-1.jpg',
+        alt: 'Location illustration photo',
+      },
+    },
+    {
+      title: {
+        content: 'Vienna office ',
+        type: 'heading',
+      },
+      address: {
+        content: 'Kaerntner Ring 5-7<br>1010 Vienna',
+        type: 'text',
+      },
+      phone: {
+        content: '<a href="tel:2221111111">(222) 111 11 11</a>',
+        type: 'text',
+      },
+      email: {
+        content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'item-2.jpg',
+        alt: 'Location illustration photo',
+      },
+    },
+    {
+      title: {
+        content: 'Amsterdam office',
+        type: 'heading',
+      },
+      address: {
+        content: 'Keizersgracht 62-64<br>1015 CS Amsterdam',
+        type: 'text',
+      },
+      phone: {
+        content: '<a href="tel:2221111111">(222) 111 11 11</a>',
+        type: 'text',
+      },
+      email: {
+        content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
+        type: 'text',
+      },
+      picture: {
+        resourceRef: 'item-3.jpg',
+        alt: 'Location illustration photo',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
