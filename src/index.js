201840cb2418806bc6c8188c014d10582079e588
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-04-10 17:00:01 +0300

    finish block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,56 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  numbers: [
+    {
+      body: {
+        content: 'increase in profits of our customers',
+        type: 'text',
+      },
+      number: {
+        content: '128%',
+        type: 'headingLg',
+      },
+      picture: {
+        resourceRef: 'pic1.jpg',
+        alt: 'Illustration for number',
+      },
+    },
+    {
+      body: {
+        content: 'higher customer satisfaction',
+        type: 'text',
+      },
+      number: {
+        content: '527%',
+        type: 'headingLg',
+      },
+      picture: {
+        resourceRef: 'pic2.jpg',
+        alt: 'Illustration for number',
+      },
+    },
+    {
+      body: {
+        content: 'of clients found new sales markets',
+        type: 'text',
+      },
+      number: {
+        content: '92%',
+        type: 'headingLg',
+      },
+      picture: {
+        resourceRef: 'pic3.jpg',
+        alt: 'Illustration for number',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Numbers description', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
