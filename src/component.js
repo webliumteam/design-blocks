8e5bef9188fcbe44aa92778a07ab62e10929928a
nmelnychenko <mykola.melnichenko@gmail.com>
2018-04-10 16:44:32 +0300

    finish first iteration

--- src/component.js
+++ src/component.js
@@ -14,29 +14,50 @@ class Wireframe extends React.Component {
 
   collectionItem = ({index, children, className, modifier}) => {
     const {components: {Text, Button}, style} = this.props
+    const withoutLine = !this.getModifierValue('line')
+    const withoutDay = !this.getModifierValue('day')
+    const withoutType = !this.getModifierValue('type')
+    const withoutTime = !this.getModifierValue('time')
+    const locInfo = this.getModifierValue('location') || this.getModifierValue('time')
+
     return (
-      <div className={classNames(style.item, className)}>
+      <div className={classNames(style.item, withoutLine && style['item--no-line'],className)}>
         {children}
-        <div className={style.item__desc}>
-          <Text bind={`events[${index}].heading`} style={style.item__heading} tagName="h2" />
-          <Text bind={`events[${index}].desc-caption`} style={style['item__desc-caption']} tagName="p" />
+        <div className={style.item__main}>
+          <div className={classNames(style.item__desc, withoutType && style['item__desc--bottom'])}>
+            <Text bind={`events[${index}].heading`} style={style.item__heading} tagName="h2" />
+            {this.getModifierValue('type') && (
+              <Text bind={`events[${index}].descCaption`} style={style['item__desc-caption']} tagName="p" />
+            )}
+          </div>
+          {locInfo && (
+            <div className={classNames(style.loc, withoutTime && style['loc--bottom'])}>
+              {this.getModifierValue('location') && (
+                <Text bind={`events[${index}].address`} className={style.loc__address} tagName="p" />
+              )}
+              {this.getModifierValue('time') && (
+                <Text bind={`events[${index}].time`} className={style.loc__time} />
+              )}
+            </div>
+          )}
         </div>
-        <time className={style.date}>
+        <time className={classNames(style.date, withoutDay && style['date--bottom'])}>
           <Text bind={`events[${index}].day`} className={style.date__day} tagName="span" />
           <span className={style.date__text}>
             <Text bind={`events[${index}].month`} className={style.date__month} tagName="span" />
-            <Text bind={`events[${index}].week-day`} className={style['date__week-day']} tagName="span" />
+            {this.getModifierValue('day') && (
+              <Text bind={`events[${index}].weekDay`} className={style['date__week-day']} tagName="span" />
+            )}
           </span>
         </time>
-        <div className={style.loc}>
-          <Text bind={`events[${index}].address`} className={style.loc__address} tagName="p" />
-          <Text bind={`events[${index}].time`} className={style.loc__time} />
+        <div className={style['item__button-wrapper']}>
+          <Button
+            className={style.item__button}
+            buttonClassName={style.button}
+            linkClassName={style.link}
+            bind={`events[${index}].cta`}
+          />
         </div>
-        <Button
-          className={style.button}
-          linkClassName={style.link}
-          bind={`events[${index}].cta`}
-        />
       </div>
     )
   }
@@ -45,9 +66,9 @@ class Wireframe extends React.Component {
     const {components: {Text, Button, Collection}, style, $block} = this.props
 
     return (
-      <section className="section">
-        <div className="section__inner">
-          <div className="section__header">
+      <section className={style.section}>
+        <div className={style.section__inner}>
+          <div className={style.section__header}>
             <Text bind="title" className={style.title} tagName="h1" />
             {this.getModifierValue('subtitle') && (
               <Text bind="subtitle" className={style.subtitle} tagName="p" />
@@ -55,7 +76,7 @@ class Wireframe extends React.Component {
           </div>
           <Collection
             className={style['items-wrapper']}
-            bind="steps"
+            bind="events"
             Item={this.collectionItem}
             itemProps={{
               modifier: $block.modifier,
@@ -76,17 +97,139 @@ class Wireframe extends React.Component {
   }
 }
 
-Wireframe.components = _.pick(['Text'])($editor.components)
+Wireframe.components = _.pick(['Text', 'Button', 'Collection'])($editor.components)
 
 Wireframe.defaultContent = {
   title: {
     content: 'Events',
     type: 'blockTitle',
   },
+  subtitle: {
+    content: 'We regularly host different kinds of events to satisfy the desires and demands even of all our clients. Do you want to know more about our activities? Follow the updates and never miss the workshops and seminars from our professionals.',
+    type: 'subtitle',
+  },
+  events: [
+    {
+      heading: {
+        type: 'heading',
+        content: 'Adult Sunday Design Groups',
+      },
+      descCaption: {
+        type: 'caption',
+        content: 'Workshop',
+      },
+      day: {
+        type: 'heroTitle',
+        content: '01',
+      },
+      month: {
+        type: 'text',
+        content: 'December',
+      },
+      weekDay: {
+        type: 'caption',
+        content: 'Friday',
+      },
+      address: {
+        type: 'text',
+        content: '<b>Central Park</b> Brooklyn, NY 10036',
+      },
+      time: {
+        type: 'caption',
+        content: '10:30 - 14:00',
+      },
+      cta: {
+        type: 'primary',
+        size: 'md',
+        textValue: 'Learn more',
+      },
+    },
+    {
+      heading: {
+        type: 'heading',
+        content: 'Product release trends in 2018',
+      },
+      descCaption: {
+        type: 'caption',
+        content: 'Lection',
+      },
+      day: {
+        type: 'heroTitle',
+        content: '22',
+      },
+      month: {
+        type: 'text',
+        content: 'December',
+      },
+      weekDay: {
+        type: 'caption',
+        content: 'Friday',
+      },
+      address: {
+        type: 'text',
+        content: '<b>Central Park</b> Brooklyn, NY 10036',
+      },
+      time: {
+        type: 'caption',
+        content: '10:30 - 14:00',
+      },
+      cta: {
+        type: 'primary',
+        size: 'md',
+        textValue: 'Learn more',
+      },
+    },
+    {
+      heading: {
+        type: 'heading',
+        content: 'UX workshop',
+      },
+      descCaption: {
+        type: 'caption',
+        content: 'Workshop',
+      },
+      day: {
+        type: 'heroTitle',
+        content: '29',
+      },
+      month: {
+        type: 'text',
+        content: 'December',
+      },
+      weekDay: {
+        type: 'caption',
+        content: 'Friday',
+      },
+      address: {
+        type: 'text',
+        content: '<b>Central Park</b> Brooklyn, NY 10036',
+      },
+      time: {
+        type: 'caption',
+        content: '10:30 - 14:00',
+      },
+      cta: {
+        type: 'primary',
+        size: 'md',
+        textValue: 'Learn more',
+      },
+    },
+  ],
+  button: {
+    type: 'secondary',
+    size: 'md',
+    textValue: 'Show all events',
+  },
 }
 
 Wireframe.modifierScheme = {
-  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  subtitle: {defaultValue: true, label: 'Subtitle', type: 'checkbox'},
+  line: {defaultValue: true, label: 'Line', type: 'checkbox'},
+  location: {defaultValue: true, label: 'Event`s location', type: 'checkbox'},
+  day: {defaultValue: true, label: 'Day of week', type: 'checkbox'},
+  type: {defaultValue: true, label: 'Event`s type', type: 'checkbox'},
+  time: {defaultValue: true, label: 'Event`s time', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default Wireframe
