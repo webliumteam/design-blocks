5229b27e04f19433469529f76bf7477880d2de55
kirilldonenko <kirill.donenko@gmail.com>
2018-04-10 13:02:06 +0300

    min styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-series-1-quote/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,45 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+    overlay: {
+      type: 'color',
+      color: 'brand-color',
+      opacity: 0.2,
+    },
+  },
+  blockquote: {
+    content: 'There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.',
+    type: 'quote',
+  },
+  author: {
+    content: 'Ray Goforth',
+    type: 'text',
+  },
+  picture: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Illustration photo',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  author: {
+    type: 'checkbox',
+    label: 'Quote author',
+    defaultValue: true,
+  },
+  picture: {
+    type: 'hidden',
+    label: 'Picture',
+    defaultValue: false,
+  },
 }
 
 export default ExtendedWireframe
