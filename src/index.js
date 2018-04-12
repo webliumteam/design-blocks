c27b143da9013d3b293621b81bdcac0d6c00087d
kirilldonenko <kirill.donenko@gmail.com>
2018-04-12 22:33:03 +0300

    fix spread

--- src/index.js
+++ src/index.js
@@ -13,116 +13,30 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   blog: [
     {
-      title: {
-        content: 'How to Hire the Best Employees to Your Company?',
-        type: 'heading',
-      },
-      description: {
-        content: 'Our HR Director shares his experience how to fill positions with the best candidates, where to find talents, and how to attract professionals to your business. ',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
+      ...Component.defaultContent.blog[0],
       picture: {
+        ...Component.defaultContent.blog[0].picture,
         resourceRef: 'img-1.jpg',
-        alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'How to Achieve Higher Profits in Retail with One Product?',
-        type: 'heading',
-      },
-      description: {
-        content: 'Do you want to achieve higher profits this year? Our new product will help you get what you want. In this article, you will find out how to use it to get more benefits.',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
+      ...Component.defaultContent.blog[1],
       picture: {
+        ...Component.defaultContent.blog[1].picture,
         resourceRef: 'img-2.jpg',
-        alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'Top 5 Tips to Improve Your Engineering Department.',
-        type: 'heading',
-      },
-      description: {
-        content: 'You engineers can bring you better results! Get to know how to improve engineering department to make a new step for your company growth. ',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
+      ...Component.defaultContent.blog[2],
       picture: {
+        ...Component.defaultContent.blog[2].picture,
         resourceRef: 'img-3.jpg',
-        alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
       },
     },
   ],
   topIcon: {
-    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
+    svg:
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
     fill: 'red',
   },
   title: {
@@ -152,12 +66,24 @@ ExtendedWireframe.defaultContent = {
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
   'block-button': {defaultValue: true, label: 'Button', type: 'checkbox'},
-  'post-description': {defaultValue: true, label: 'Post main text', type: 'checkbox'},
+  'post-description': {
+    defaultValue: true,
+    label: 'Post main text',
+    type: 'checkbox',
+  },
   'post-image': {defaultValue: true, label: 'Post image', type: 'checkbox'},
   'post-link': {defaultValue: true, label: 'Post link', type: 'checkbox'},
-  'post-publish-date': {defaultValue: true, label: 'Date of publishing', type: 'checkbox'},
+  'post-publish-date': {
+    defaultValue: true,
+    label: 'Date of publishing',
+    type: 'checkbox',
+  },
   subtitle: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
-  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
+  'top-icon': {
+    defaultValue: false,
+    label: 'Top icon decorator',
+    type: 'hidden',
+  },
 }
 
 export default ExtendedWireframe
