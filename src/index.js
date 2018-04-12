fbf73c84547c31a51a332b4652be70a9459aac68
Sabina <sabina.mamedova90@gmail.com>
2018-04-12 16:26:37 +0300

    revise

--- src/index.js
+++ src/index.js
@@ -10,84 +10,63 @@ const {enhancers: {withProps}, hoistStatics} = $editor
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
 ExtendedWireframe.defaultContent = {
-  title: {
-    content: 'Projects',
-    type: 'blockTitle',
-  },
-  subtitle: {
-    content: 'Over more than 15 years of our practice, we have completed a number of projects for our clients and have helped them generate more than $15 million in revenues. Browse through our selected projects to get the idea of what we do on a daily basis.',
-    type: 'subtitle',
-  },
+  ...Component.defaultContent,
   projects: [
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[0],
         resourceRef: 'alexandra-gorn-260991-unsplash.jpg',
-        alt: 'Illustration for project',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[1],
         resourceRef: 'alisha-hieb-249784-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[2],
         resourceRef: 'barrett-ward-551977-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[3],
         resourceRef: 'emma-tsui-492731-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[4],
         resourceRef: 'galina-n-200668-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[5],
         resourceRef: 'ian-dooley-281897-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[6],
         resourceRef: 'jess-watters-515757-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[7],
         resourceRef: 'helena-hertz-295994-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
     {
       picture: {
+        ...Component.defaultContent.projects[0].picture[8],
         resourceRef: 'pierre-chatel-innocenti-490529-unsplash.jpg',
-        alt: 'Picture about the company',
       },
     },
   ],
-  button: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn more',
-    type: 'secondary',
-  },
 }
 
 ExtendedWireframe.modifierScheme = {
