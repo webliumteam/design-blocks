f49fcaf3c869374f4bdf47124e6c522fc4b577af
Igor Dejak <i.dejak@betinvest.com>
2018-04-13 09:14:48 +0300

    title description for pricing in poliwag

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ ExtendedWireframe.defaultContent = {
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
