39f5e0c4d14a03498aee35812aedacb614aaed12
KonstantinKH <kkhomenko9@gmail.com>
2018-04-16 13:35:56 +0300

    changed index.js

--- src/index.js
+++ src/index.js
@@ -13,59 +13,17 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   services: [
     {
-      title: {
-        content: 'Quantum E-commerce',
-        type: 'heading',
-      },
-      text: {
-        content: 'Multipurpose program that will perfectly suit online stores and online retail business. You can add up to 2000 items with a detailed description, characteristics, and photos. Also, the shopping cart, clients’ feedback, rating, and related items option are available.',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[0],
       picture: {
+        ...Component.defaultContent.services[0].picture,
         resourceRef: 'jack-kaminski-129845-unsplash.jpg',
-        alt: 'Quantum E-commerce illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Request a quote',
-        type: 'primary',
       },
     },
     {
-      title: {
-        content: 'Quantum Aftersales',
-        type: 'heading',
-      },
-      text: {
-        content: 'Aftersales assistance that includes marketing campaigns, additional digital solutions, and educational courses. Our specialists will help to find out what will work for your business and decide on the steps that are to be taken immediately. ',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[1],
       picture: {
+        ...Component.defaultContent.services[1].picture,
         resourceRef: 'julien-pianetti-230463-unsplash.jpg',
-        alt: 'Quantum Aftersales illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Request a quote',
-        type: 'primary',
       },
     },
   ],
