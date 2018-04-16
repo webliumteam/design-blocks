f3844f915f910b7895130296e6cfe714cb996a85
KonstantinKH <kkhomenko9@gmail.com>
2018-04-16 13:42:48 +0300

    changes index.js

--- src/index.js
+++ src/index.js
@@ -13,59 +13,17 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   services: [
     {
-      title: {
-        content: 'Manufacturing',
-        type: 'heading',
-      },
-      text: {
-        content: 'We use only high-quality materials for good manufacturing. All products are designed and manufactured in line with your preferences. Just choose the materials, send us design requirements, and we will contact with you shortly to discuss the details.',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[0],
       picture: {
+        ...Component.defaultContent.services[0].picture,
         resourceRef: 'joanna-kosinska-340749-unsplash.jpg',
-        alt: 'Manufacturing illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Request a quote',
-        type: 'primary',
       },
     },
     {
-      title: {
-        content: 'Delivery and Support',
-        type: 'heading',
-      },
-      text: {
-        content: 'We provide a package of services that ensure the aftersales technical service and diagnostics. We guarantee that our goods will please you for many years, and you will enjoy decent support of our specialists.',
-        type: 'text',
-      },
+      ...Component.defaultContent.services[1],
       picture: {
+        ...Component.defaultContent.services[1].picture,
         resourceRef: 'lior-mazliah-111379-unsplash.jpg',
-        alt: 'Delivery and Support illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Request a quote',
-        type: 'primary',
       },
     },
   ],
