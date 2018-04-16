ff30aa9b882d3d9610b436113baec2ee3a254b81
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-16 18:34:49 +0300

    design

--- src/index.js
+++ src/index.js
@@ -13,93 +13,45 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   gallery: [
     {
-      title: {
-        content: 'Quantum CEO & our partners',
-        type: 'text',
-      },
+      ...Component.defaultContent.gallery[0],
       picture: {
+        ...Component.defaultContent.gallery[0].picture,
         resourceRef: 'gallery-img-1.jpg',
-        alt: 'Quantum CEO & our partners',
-        galleryId: 'gallery',
-      },
-      caption: {
-        content: 'Design & build',
-        type: 'caption',
       },
     },
     {
-      title: {
-        content: 'Digital Marketing workshop lecturer',
-        type: 'text',
-      },
+      ...Component.defaultContent.gallery[1],
       picture: {
+        ...Component.defaultContent.gallery[1].picture,
         resourceRef: 'gallery-img-2.jpg',
-        alt: 'Digital Marketing workshop lecturer',
-        galleryId: 'gallery',
-      },
-      caption: {
-        content: 'Design & build',
-        type: 'caption',
       },
     },
     {
-      title: {
-        content: 'Quantum Company at WebSummit',
-        type: 'text',
-      },
+      ...Component.defaultContent.gallery[2],
       picture: {
+        ...Component.defaultContent.gallery[2].picture,
         resourceRef: 'gallery-img-3.jpg',
-        alt: 'Quantum Company at WebSummit',
-        galleryId: 'gallery',
-      },
-      caption: {
-        content: 'Design & build',
-        type: 'caption',
       },
     },
     {
-      title: {
-        content: 'UX workshop attendees',
-        type: 'text',
-      },
+      ...Component.defaultContent.gallery[3],
       picture: {
+        ...Component.defaultContent.gallery[3].picture,
         resourceRef: 'gallery-img-4.jpg',
-        alt: 'UX workshop attendees',
-        galleryId: 'gallery',
-      },
-      caption: {
-        content: 'Design & build',
-        type: 'caption',
       },
     },
     {
-      title: {
-        content: 'UX workshop lecturer',
-        type: 'text',
-      },
+      ...Component.defaultContent.gallery[4],
       picture: {
+        ...Component.defaultContent.gallery[4].picture,
         resourceRef: 'gallery-img-5.jpg',
-        alt: 'UX workshop lecturer',
-        galleryId: 'gallery',
-      },
-      caption: {
-        content: 'Design & build',
-        type: 'caption',
       },
     },
     {
-      title: {
-        content: 'Quantum team',
-        type: 'text',
-      },
+      ...Component.defaultContent.gallery[5],
       picture: {
+        ...Component.defaultContent.gallery[5].picture,
         resourceRef: 'gallery-img-6.jpg',
-        alt: 'Quantum team',
-        galleryId: 'gallery',
-      },
-      caption: {
-        content: 'Design & build',
-        type: 'caption',
       },
     },
   ],
