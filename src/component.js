86a39c21011cb5cf1bcc893622524b7506166121
nmelnychenko <mykola.melnichenko@gmail.com>
2018-04-16 15:22:58 +0300

    start work with cover-w5

--- src/component.js
+++ src/component.js
@@ -7,21 +7,47 @@ class Wireframe extends React.Component {
   }
 
   render() {
-    const {components: {Text}, style} = this.props
+    const {components: {Text, Background}, style} = this.props
+    const items = _.times(Number, this.getModifierValue('imagesQuantity'))
 
     return (
-      <Text bind="title" className={style.title} tagName="h1" />
+      <section className={style.section}>
+        <div className={style.section__inner}>
+          <div className={style['items-wrapper']}>
+            {_.map(n => (
+              <Background key={n}>
+                <Text />
+              </Background>
+            ), items)}
+          </div>
+        </div>
+      </section>
     )
   }
 }
 
-Wireframe.components = _.pick(['Text'])($editor.components)
+Wireframe.components = _.pick(['Text', 'Background'])($editor.components)
 
 Wireframe.defaultContent = {
   title: {
     content: 'Hello world',
     type: 'blockTitle',
   },
+}
+
+Wireframe.modifierScheme = {
+  imagesQuantity: {
+    children: [
+      {id: '1', label: '1'},
+      {id: '2', label: '2'},
+      {id: '3', label: '3'},
+    ],
+    defaultValue: '3',
+    name: 'Items in block',
+    type: 'radio-button-group',
+    style: 'buttons',
+  },
+  heading: {defaultValue: true, label: 'Heading', type: 'hidden'},
 }
 
 export default Wireframe
