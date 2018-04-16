9200d1a118ee14808d290f5532316d250f6680ef
Vladimir Zelik <kazantip81v@gmail.com>
2018-04-16 21:40:56 +0300

    add photo & style

--- src/index.js
+++ src/index.js
@@ -11,6 +11,64 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  testimonials: [
+    {
+      ...Component.defaultContent.testimonials[0],
+      image: {
+        ...Component.defaultContent.testimonials[0].image,
+        resourceRef: 'cristian-newman-458458-unsplash.jpg',
+      },
+      socialIcons: {
+        ...Component.defaultContent.testimonials[0].socialIcons,
+        design: {
+          ...Component.defaultContent.testimonials[0].socialIcons.design,
+          color: 'dark-accent-color',
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.testimonials[1],
+      image: {
+        ...Component.defaultContent.testimonials[1].image,
+        resourceRef: 'michael-dam-258165-unsplash.jpg',
+      },
+      socialIcons: {
+        ...Component.defaultContent.testimonials[1].socialIcons,
+        design: {
+          ...Component.defaultContent.testimonials[1].socialIcons.design,
+          color: 'dark-accent-color',
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.testimonials[2],
+      image: {
+        ...Component.defaultContent.testimonials[2].image,
+        resourceRef: 'marco-w-397245-unsplash.jpg',
+      },
+      socialIcons: {
+        ...Component.defaultContent.testimonials[2].socialIcons,
+        design: {
+          ...Component.defaultContent.testimonials[2].socialIcons.design,
+          color: 'dark-accent-color',
+        },
+      },
+    },
+    {
+      ...Component.defaultContent.testimonials[3],
+      image: {
+        ...Component.defaultContent.testimonials[3].image,
+        resourceRef: 'cristian-newman-458458-unsplash.jpg',
+      },
+      socialIcons: {
+        ...Component.defaultContent.testimonials[3].socialIcons,
+        design: {
+          ...Component.defaultContent.testimonials[3].socialIcons.design,
+          color: 'dark-accent-color',
+        },
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
