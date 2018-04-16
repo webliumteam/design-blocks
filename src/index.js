cfd5d9a0473685923582000a1509d81c66b85470
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-16 18:53:15 +0300

    design

--- src/index.js
+++ src/index.js
@@ -13,163 +13,31 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   events: [
     {
-      title: {
-        content: 'Digital marketing workshop',
-        type: 'heading',
-      },
-      date: {
-        content: 'October 24, 2017',
-        type: 'subtitle',
-      },
-      badge: {
-        content: 'members only',
-        type: 'caption',
-      },
-      time: {
-        content: '10:30am - 1:30pm',
-        type: 'text',
-      },
-      location: {
-        content: 'The station',
-        type: 'caption',
-      },
+      ...Component.defaultContent.events[0],
       picture: {
+        ...Component.defaultContent.events[0].picture,
         resourceRef: 'events-img-1.jpg',
-        alt: 'Digital marketing workshop illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'Blogging for beginners',
-        type: 'heading',
-      },
-      date: {
-        content: 'November 1, 2017',
-        type: 'subtitle',
-      },
-      badge: {
-        content: 'members only',
-        type: 'caption',
-      },
-      time: {
-        content: '10:30am - 1:30pm',
-        type: 'text',
-      },
-      location: {
-        content: 'The station',
-        type: 'caption',
-      },
+      ...Component.defaultContent.events[1],
       picture: {
+        ...Component.defaultContent.events[1].picture,
         resourceRef: 'events-img-2.jpg',
-        alt: 'Blogging for beginners illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'How to find insight',
-        type: 'heading',
-      },
-      date: {
-        content: 'November 25, 2017',
-        type: 'subtitle',
-      },
-      badge: {
-        content: 'members only',
-        type: 'caption',
-      },
-      time: {
-        content: '10:30am - 1:30pm',
-        type: 'text',
-      },
-      location: {
-        content: 'The station',
-        type: 'caption',
-      },
+      ...Component.defaultContent.events[2],
       picture: {
+        ...Component.defaultContent.events[2].picture,
         resourceRef: 'events-img-3.jpg',
-        alt: 'How to find insight illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-        type: 'link',
       },
     },
     {
-      title: {
-        content: 'UX workshop',
-        type: 'heading',
-      },
-      date: {
-        content: 'December 3, 2017',
-        type: 'subtitle',
-      },
-      badge: {
-        content: 'members only',
-        type: 'caption',
-      },
-      time: {
-        content: '10:30am - 1:30pm',
-        type: 'text',
-      },
-      location: {
-        content: 'The station',
-        type: 'caption',
-      },
+      ...Component.defaultContent.events[3],
       picture: {
+        ...Component.defaultContent.events[3].picture,
         resourceRef: 'events-img-4.jpg',
-        alt: 'UX workshop illustration',
-      },
-      cta: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Know more',
-        type: 'link',
       },
     },
   ],
