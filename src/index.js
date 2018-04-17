90cb9b0129f1d05b62f61bdd65c6972db1488e7b
Igor Dejak <i.dejak@betinvest.com>
2018-04-17 17:07:27 +0300

    pricing

--- src/index.js
+++ src/index.js
@@ -13,45 +13,25 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   steps: [
     {
-      heading: {
-        content: 'Step 1. Choose a plan',
-        type: 'heading',
-      },
-      description: {
-        content: 'We provide you with full information about each plan. There are a few available options to choose from.',
-        type: 'text',
-      },
+      ...Component.defaultContent.steps[0],
       picture: {
+        ...Component.defaultContent.steps[0].picture,
         resourceRef: 'process01.jpg',
-        alt: 'Illustraion for step 1',
       },
     },
     {
-      heading: {
-        content: 'Step 2. Proceed with the payment',
-        type: 'heading',
-      },
-      description: {
-        content: 'We guarantee the security of all payments.',
-        type: 'text',
-      },
+      ...Component.defaultContent.steps[1],
       picture: {
+        ...Component.defaultContent.steps[1].picture,
         resourceRef: 'process02.jpg',
-        alt: 'Illustraion for step 2',
       },
     },
     {
-      heading: {
-        content: 'Step 3. Enjoy our products',
-        type: 'heading',
-      },
-      description: {
-        content: 'You will get access to all options immediately.',
-        type: 'text',
-      },
+      ...Component.defaultContent.steps[2],
       picture: {
+        ...Component.defaultContent.steps[2].picture,
         resourceRef: 'process03.jpg',
-        alt: 'Illustraion for step 3',
+
       },
     },
   ],
