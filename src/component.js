b8ad44b01b048a3e10f66360ac4431bb2169193c
nmelnychenko <mykola.melnichenko@gmail.com>
2018-04-17 11:35:20 +0300

    add modifications

--- src/component.js
+++ src/component.js
@@ -3,40 +3,157 @@ import $editor from 'weblium/editor'
 class Wireframe extends React.Component {
   static propTypes = {
     components: PropTypes.object.isRequired,
+    $block: PropTypes.object.isRequired,
     style: PropTypes.object.isRequired,
   }
 
+  getOptionValue = (path, defaultValue = false) =>
+    _.getOr(defaultValue, ['options', path], this.props.$block)
+
+  getModifierValue = path => _.get(['modifier', path], this.props.$block)
+
+  collectionItem = ({index, children, className, modifier}) => {
+    const {components: {Text, Button}, style} = this.props
+
+    return (
+      <div className={classNames(style.item, className)}>
+        {children}
+        <Text tagName="h2" className={style.item__title} bind={`items[${index}].title`} />
+        {this.getModifierValue('body') && (
+          <Text tagName="p" className={style.item__descv} bind={`items[${index}].desc`} />
+        )}
+        {this.getModifierValue('item-button') && (
+          <Button
+            linkClassName={style.link}
+            buttonClassName={style.button}
+            className={style.item__button}
+            bind={`items[${index}].cta`}
+          />
+        )}
+      </div>
+    )
+  }
+
   render() {
-    const {components: {Text, Background}, style} = this.props
-    const items = _.times(Number, this.getModifierValue('imagesQuantity'))
+    const {components: {Collection, Button}, style, $block} = this.props
 
     return (
       <section className={style.section}>
         <div className={style.section__inner}>
-          <div className={style['items-wrapper']}>
-            {_.map(n => (
-              <Background key={n}>
-                <Text />
-              </Background>
-            ), items)}
-          </div>
+          <Collection
+            className={style['items-wrapper']}
+            bind="items"
+            Item={this.collectionItem}
+            itemProps={{
+              modifier: $block.modifier,
+            }}
+          />
+          {this.getModifierValue('button') && (
+            <div className={style['btns-group']}>
+              <Button
+                className={style['btns-group__button']}
+                linkClassName={style.link}
+                buttonClassName={style.button}
+                bind="cta"
+              />
+            </div>
+          )}
         </div>
       </section>
     )
   }
 }
 
-Wireframe.components = _.pick(['Text', 'Background'])($editor.components)
+Wireframe.components = _.pick(['Text', 'Collection', 'Button'])($editor.components)
 
 Wireframe.defaultContent = {
+  background: {
+    type: 'color',
+    color: '#d8d8d8',
+  },
   title: {
     content: 'Hello world',
     type: 'blockTitle',
   },
+  items: [
+    {
+      background: {
+        type: 'image',
+        position: {
+          type: 'cover',
+          cover: '50% 50%',
+        },
+        src: 'https://weblium-prod.storage.googleapis.com/res/weblium/5ad5a3826678f100250985e4.jpeg',
+        imageColor: '#fafafa',
+      },
+      title: {
+        type: 'heading',
+        content: 'Spring flowers',
+      },
+      desc: {
+        type: 'text',
+        content: 'Take a look at our best collection of beautiful flowers',
+      },
+      cta: {
+        textValue: 'Order spring flowers',
+        type: 'primary',
+      },
+    },
+    {
+      background: {
+        type: 'image',
+        position: {
+          type: 'cover',
+          cover: '50% 50%',
+        },
+        src: 'https://weblium-prod.storage.googleapis.com/res/weblium/5ad5a3826678f100250985e4.jpeg',
+        imageColor: '#fafafa',
+      },
+      title: {
+        type: 'heading',
+        content: 'Summer flowers',
+      },
+      desc: {
+        type: 'text',
+        content: 'Take a look at our best collection of beautiful flowers',
+      },
+      cta: {
+        textValue: 'Order summer flowers',
+        type: 'primary',
+      },
+    },
+    {
+      background: {
+        type: 'image',
+        position: {
+          type: 'cover',
+          cover: '50% 50%',
+        },
+        src: 'https://weblium-prod.storage.googleapis.com/res/weblium/5ad5a3826678f100250985e4.jpeg',
+        imageColor: '#fafafa',
+      },
+      title: {
+        type: 'heading',
+        content: 'Autumn flowers',
+      },
+      desc: {
+        type: 'text',
+        content: 'Take a look at our best collection of beautiful flowers',
+      },
+      cta: {
+        textValue: 'Order autumn flowers',
+        type: 'primary',
+      },
+    },
+  ],
+  cta: {
+    textValue: 'More about our flowers',
+    type: 'secondary',
+  },
 }
 
 Wireframe.modifierScheme = {
-  imagesQuantity: {
+  itemsQuantity: {
     children: [
       {id: '1', label: '1'},
       {id: '2', label: '2'},
@@ -47,7 +164,9 @@ Wireframe.modifierScheme = {
     type: 'radio-button-group',
     style: 'buttons',
   },
-  heading: {defaultValue: true, label: 'Heading', type: 'hidden'},
+  body: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  'item-button': {defaultValue: true, label: 'Primary button', type: 'checkbox'},
+  cta: {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default Wireframe
