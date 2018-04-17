49ceb85071275998ad5aa1c144d7d66223dcf3d2
isergienko <igorserhiyenko@gmail.com>
2018-04-17 18:36:56 +0300

    a

--- src/index.js
+++ src/index.js
@@ -12,27 +12,24 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   partners: {
+    ...Component.defaultContent.partners,
     items: [
       {
-        ...Component.defaultContent.partners[0],
+        ...Component.defaultContent.partners.items[0],
         logo: {
-          ...Component.defaultContent.partners[0].logo,
-          resourceRef: 'cribspot.jpg',
-          alt: 'sumsung',
-          width: 220,
-          height: 122,
+          ...Component.defaultContent.partners.items[0].logo,
+          image: {
+            resourceRef: 'cribspot.jpg',
+            alt: 'sumsung',
+            width: 220,
+            height: 122,
+          },
         },
       },
       {
-        title: {
-          content: 'Ford',
-          type: 'heading',
-        },
-        desc: {
-          content: '<span style="font-weight: bold">Ford.</span> This is our moving force, literally! 100% reliable and credible.',
-          type: 'text',
-        },
+        ...Component.defaultContent.partners.items[1],
         logo: {
+          ...Component.defaultContent.partners.items[1].logo,
           image: {
             resourceRef: 'fresh.jpg',
             alt: 'ford',
@@ -40,31 +37,11 @@ ExtendedWireframe.defaultContent = {
             height: 122,
           },
         },
-        cta: {
-          actionConfig: {
-            action: 'link',
-            actions: {
-              link: {
-                type: '',
-                innerPage: '',
-                url: '',
-              },
-            },
-          },
-          textValue: 'Learn more',
-          type: 'link',
-        },
       },
       {
-        title: {
-          content: 'Medical Family',
-          type: 'heading',
-        },
-        desc: {
-          content: '<span style="font-weight: bold">Medical Family.</span> Ensuring good health and medical care for our team for 5 years.',
-          type: 'text',
-        },
+        ...Component.defaultContent.partners.items[2],
         logo: {
+          ...Component.defaultContent.partners.items[2].logo,
           image: {
             resourceRef: 'leap.jpg',
             alt: 'medical',
@@ -72,31 +49,11 @@ ExtendedWireframe.defaultContent = {
             height: 122,
           },
         },
-        cta: {
-          actionConfig: {
-            action: 'link',
-            actions: {
-              link: {
-                type: '',
-                innerPage: '',
-                url: '',
-              },
-            },
-          },
-          textValue: 'Learn more',
-          type: 'link',
-        },
       },
       {
-        title: {
-          content: 'Coffee Break Now',
-          type: 'heading',
-        },
-        desc: {
-          content: '<span style="font-weight: bold">Coffee Break Now.</span> We love their delicious coffee and flavored cakes!',
-          type: 'text',
-        },
+        ...Component.defaultContent.partners.items[3],
         logo: {
+          ...Component.defaultContent.partners.items[3].logo,
           image: {
             resourceRef: 'nothingmagical.jpg',
             alt: 'Coffee',
@@ -104,48 +61,8 @@ ExtendedWireframe.defaultContent = {
             height: 122,
           },
         },
-        cta: {
-          actionConfig: {
-            action: 'link',
-            actions: {
-              link: {
-                type: '',
-                innerPage: '',
-                url: '',
-              },
-            },
-          },
-          textValue: 'Learn more',
-          type: 'link',
-        },
       },
     ],
-  },
-  title: {
-    content: 'Partners',
-    type: 'blockTitle',
-  },
-  subtitle: {
-    content: 'Here are some of our trusted partners who help us deliver the best services and solutions for clients. Want to become our partner? Contact us, and we’ll discuss the details of possible cooperation.',
-    type: 'subtitle',
-  },
-  button: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn more',
-    type: 'secondary',
-  },
-  'top-caption': {
-    content: 'vitae sapien ut libero',
-    type: 'text',
   },
 }
 
