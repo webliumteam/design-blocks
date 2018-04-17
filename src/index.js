987a35c90afeaf5cdf215a43a6942dc627c0c37f
KonstantinKH <kkhomenko9@gmail.com>
2018-04-17 17:52:29 +0300

    changed modifier

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ ExtendedWireframe.defaultContent = {
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  subtitle: {defaultValue: true},
+  subtitle: {defaultValue: true, label: 'Service description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
