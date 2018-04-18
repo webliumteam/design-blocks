e65e86b9a6b332111f84fd4ee23102ae301215b2
nmelnychenko <mykola.melnichenko@gmail.com>
2018-04-18 18:05:00 +0300

    init on co

--- src/component.js
+++ src/component.js
@@ -3,25 +3,89 @@ import $editor from 'weblium/editor'
 class Wireframe extends React.Component {
   static propTypes = {
     components: PropTypes.object.isRequired,
+    $block: PropTypes.object.isRequired,
     style: PropTypes.object.isRequired,
   }
 
-  render() {
-    const {components: {Text}, style} = this.props
+  getModifierValue = path => _.get(['modifier', path], this.props.$block)
 
+  render() {
+    const {components: {Text, ContactForm, Background}, style} = this.props
     return (
-      <Text bind="title" className={style.title} tagName="h1" />
+      <section className={style.section}>
+        <Background className={style.section__inner} bind="formBackground">
+          <header className={style.section__header}>
+            {this.getModifierValue('title') && <Text tagName="h1" className={style.title} bind="title" />}
+            {this.getModifierValue('subtitle') && <Text tagName="p" className={style.subtitle} bind="subtitle" />}
+          </header>
+          <ContactForm
+            bind="contactForm"
+            className={style.form}
+            labelClassName={style.form__item}
+            fieldClassName={style.form__field}
+            buttonClassName={style.form__button}
+          />
+        </Background>
+      </section>
     )
   }
 }
 
-Wireframe.components = _.pick(['Text'])($editor.components)
+Wireframe.components = _.pick(['Text', 'ContactForm', 'Background'])($editor.components)
 
 Wireframe.defaultContent = {
+  background: {
+    type: 'color',
+    color: '#d8d8d8',
+  },
+  formBackground: {
+    type: 'color',
+    color: 'light-shade-color',
+  },
   title: {
-    content: 'Hello world',
+    content: 'Want to increase profits?',
     type: 'blockTitle',
   },
+  subtitle: {
+    content: 'Complete the form below and receive a full analysis of your business within 2 days!',
+    type: 'subtitle',
+  },
+  contactForm: {
+    fields: [
+      {
+        type: 'text',
+        id: 'contactForm_name',
+        title: 'Name',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'phone',
+        id: 'contactForm_phoneNumber ',
+        title: 'Phone number ',
+        placeholder: '',
+        required: true,
+      },
+      {
+        type: 'email',
+        id: 'contactForm_email',
+        title: 'Email',
+        placeholder: '',
+        required: true,
+      },
+    ],
+    submitButton: {
+      title: 'Request a quote',
+      type: 'primary',
+    },
+    className: 'form',
+    buttonClassName: 'button button--size-md button--primary form__button',
+  },
+}
+
+Wireframe.modifierScheme = {
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
 }
 
 export default Wireframe
