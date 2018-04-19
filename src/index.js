f3aa7636aa06764194af5c5e938166fd2aefa135
max-sindi <britva.okkamaa@gmail.com>
2018-04-19 22:12:50 +0300

    fixed index.js code style

--- src/index.js
+++ src/index.js
@@ -13,66 +13,21 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   contacts: [
     {
-      title: {
-        content: 'Head office in London',
-        type: 'heading',
-      },
-      address: {
-        content: 'Level 17, Dashwood House<br>London EC2M 1QS ',
-        type: 'text',
-      },
-      phone: {
-        content: '<a href="tel:2221111111">(222) 111 11 11</a>',
-        type: 'text',
-      },
-      email: {
-        content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
-        type: 'text',
-      },
+      ...Component.defaultContent.contacts[0],
       picture: {
         resourceRef: 'item-1.jpg',
         alt: 'Location illustration photo',
       },
     },
     {
-      title: {
-        content: 'Vienna office ',
-        type: 'heading',
-      },
-      address: {
-        content: 'Kaerntner Ring 5-7<br>1010 Vienna',
-        type: 'text',
-      },
-      phone: {
-        content: '<a href="tel:2221111111">(222) 111 11 11</a>',
-        type: 'text',
-      },
-      email: {
-        content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
-        type: 'text',
-      },
+      ...Component.defaultContent.contacts[0],
       picture: {
         resourceRef: 'item-2.jpg',
         alt: 'Location illustration photo',
       },
     },
     {
-      title: {
-        content: 'Amsterdam office',
-        type: 'heading',
-      },
-      address: {
-        content: 'Keizersgracht 62-64<br>1015 CS Amsterdam',
-        type: 'text',
-      },
-      phone: {
-        content: '<a href="tel:2221111111">(222) 111 11 11</a>',
-        type: 'text',
-      },
-      email: {
-        content: '<a href="mailto:mywebsite@a.weblium.com">mywebsite@a.weblium.com</a>',
-        type: 'text',
-      },
+      ...Component.defaultContent.contacts[0],
       picture: {
         resourceRef: 'item-3.jpg',
         alt: 'Location illustration photo',
