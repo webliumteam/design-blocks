fde92c23ababd244073c263a930dc79ff56b5eb8
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-04-23 17:24:53 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,6 +11,67 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  cover: [
+    {
+      ...Component.defaultContent.cover[0],
+      title: {
+        ...Component.defaultContent.cover[0].title,
+        content: 'General Contractor',
+      },
+      subtitle: {
+        ...Component.defaultContent.cover[0].subtitle,
+        content: 'Driven by Passion for Craft and Design',
+        color: 'dark-shade-color',
+        brightness: 50,
+      },
+      cta: {
+        ...Component.defaultContent.cta,
+        textValue: 'View projects',
+      },
+    },
+    {
+      ...Component.defaultContent.cover[1],
+      title: {
+        ...Component.defaultContent.cover[0].title,
+        content: 'General Contractor',
+      },
+      subtitle: {
+        ...Component.defaultContent.cover[0].subtitle,
+        content: 'Driven by Passion for Craft and Design',
+        color: 'dark-shade-color',
+        brightness: 50,
+      },
+      cta: {
+        ...Component.defaultContent.cta,
+        textValue: 'View projects',
+      },
+    },
+    {
+      ...Component.defaultContent.cover[2],
+      title: {
+        ...Component.defaultContent.cover[0].title,
+        content: 'General Contractor',
+      },
+      subtitle: {
+        ...Component.defaultContent.cover[0].subtitle,
+        content: 'Driven by Passion for Craft and Design',
+        color: 'dark-shade-color',
+        brightness: 50,
+      },
+      cta: {
+        ...Component.defaultContent.cta,
+        textValue: 'View projects',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
