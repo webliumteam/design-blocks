a6b5ed5442e1d773b5ffb3bfeebeef02d5199d0c
nataliadiadkova <nataliadiadkova@gmail.com>
2018-04-26 11:33:00 +0300

    design

--- src/index.js
+++ src/index.js
@@ -13,446 +13,246 @@ ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
   services: [
     {
+      ...Component.defaultContent.services[0],
       title: {
         content: '24-hour emergency service',
         type: 'heading',
       },
       description: {
         content: 'From $50',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '1.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[1],
       title: {
         content: 'Interior and exterior lighting',
         type: 'heading',
       },
       description: {
         content: 'From $30',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '2.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[2],
       title: {
         content: 'Data network & telephone installations',
         type: 'heading',
       },
       description: {
         content: 'From $50',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '3.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[3],
       title: {
         content: 'Power meter & electrical panel service',
         type: 'heading',
       },
       description: {
         content: 'From $25',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '4.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[4],
       title: {
         content: 'Lightning protection system<br>installation',
         type: 'heading',
       },
       description: {
         content: 'From $30',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '5.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[5],
       title: {
         content: 'Residential and commercial wiring',
         type: 'heading',
       },
       description: {
         content: 'From $50',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '6.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[6],
       title: {
         content: 'Underfloor heating and thermostat<br>installation',
         type: 'heading',
       },
       description: {
         content: 'From $35',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '7.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[7],
       title: {
         content: 'Air conditioning and heating systems',
         type: 'heading',
       },
       description: {
         content: 'From $30',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '8.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[8],
       title: {
         content: 'Fuse box installation and replacement',
         type: 'heading',
       },
       description: {
         content: 'From $50',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '9.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[9],
       title: {
         content: 'Voltage regulators installation<br>service',
         type: 'heading',
       },
       description: {
         content: 'From $45',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '10.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[10],
       title: {
         content: 'Data network and service facilities for<br>businesses',
         type: 'heading',
       },
       description: {
         content: 'From $30',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '11.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[11],
       title: {
         content: 'Security surveillance system<br>installation',
         type: 'heading',
       },
       description: {
         content: 'From $50',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '12.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[12],
       title: {
         content: 'Video intercom systems installation',
         type: 'heading',
       },
       description: {
         content: 'From $25',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '13.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[13],
       title: {
         content: 'Satellite TV installations',
         type: 'heading',
       },
       description: {
         content: 'From $30',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '14.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.services[14],
       title: {
         content: 'Emergency & backup generator<br> Instllation',
         type: 'heading',
       },
       description: {
         content: 'From $30',
-        type: 'text',
+        type: 'subheading',
         color: 'brand-color',
       },
       picture: {
         src: '15.png',
         alt: 'Service illustration photo',
       },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
     },
   ],
-  topIcon: {
-    svg:
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
-    fill: 'red',
-  },
   title: {
     content: 'Services',
     type: 'blockTitle',
@@ -461,45 +261,13 @@ ExtendedWireframe.defaultContent = {
     content: "If you can't find the service you are looking for, feel free to contact us. We will be glad to answer any service-related questions. We look forward to meeting your electrical needs. Here are some services we offer:",
     type: 'subtitle',
   },
-  'button-1': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Request a quote',
-    type: 'primary',
-  },
-  'button-2': {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
-        },
-      },
-    },
-    textValue: 'Learn more',
-    type: 'secondary',
-  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  body: {defaultValue: true, label: 'Service main text', type: 'checkbox'},
-  'button-primary': {defaultValue: false, label: 'Additional button', type: 'checkbox'},
   'button-secondary': {defaultValue: false, label: 'Button', type: 'checkbox'},
-  heading: {defaultValue: true, label: 'Service title', type: 'checkbox'},
   link: {defaultValue: false, label: 'Link', type: 'checkbox'},
   subtitle: {defaultValue: true, label: 'Services description', type: 'checkbox'},
-  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
 }
 
 export default ExtendedWireframe
