3d5bf968831a55f3af80ffa70bd5f587536c458f
Vladimir Zelik <kazantip81v@gmail.com>
2018-04-27 22:25:22 +0300

    add bg

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'sasha-dasha-bakani-427546-unsplash.jpg',
+    overlay: {
+      type: 'color',
+      color: 'dark-shade-color',
+      opacity: 0.3,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
