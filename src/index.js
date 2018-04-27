ffddeb25780d2214ce66e0b07b1c183b18725e3b
kirilldonenko <kirill.donenko@gmail.com>
2018-04-27 14:08:47 +0300

    styles of block

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-series-3-contacts/src/component'
 import style from './style.css'
 
 import options from './options.json'
@@ -11,10 +11,21 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  map: {
+    ...Component.defaultContent.map,
+    preset: 'default',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: false, label: 'Block title', type: 'hidden'},
+  heading: {defaultValue: false, label: 'Location title', type: 'hidden'},
+  address: {defaultValue: false, label: 'Location address', type: 'hidden'},
+  email: {defaultValue: false, label: 'E-mail', type: 'hidden'},
+  phone: {defaultValue: false, label: 'Phone', type: 'hidden'},
+  social: {defaultValue: false, label: 'Social Media Buttons', type: 'hidden'},
+  button: {defaultValue: false, label: 'Button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
