9697ff5b73372d6dd8a765fb420956606303c0be
nmelnychenko <mykola.melnichenko@gmail.com>
2018-04-27 10:48:48 +0300

    extend structure

--- src/component.js
+++ src/component.js
@@ -3,14 +3,27 @@ import $editor from 'weblium/editor'
 class Wireframe extends React.Component {
   static propTypes = {
     components: PropTypes.object.isRequired,
+    $block: PropTypes.object.isRequired,
     style: PropTypes.object.isRequired,
   }
+
+  getModifierValue = path => _.get(['modifier', path], this.props.$block)
+
+  getOptionValue = (path, defaultValue = false) =>
+    _.getOr(defaultValue, ['options', path], this.props.$block)
 
   render() {
     const {components: {Text}, style} = this.props
 
     return (
-      <Text bind="title" className={style.title} tagName="h1" />
+      <section className={style.section}>
+        <div className={style.section__inner}>
+          <Text bind="title" className={style.title} tagName="h2" />
+          {this.getModifierValue('subtitle') && (
+            <Text bind="subtitle" className={style.subtitle} tagName="p" />
+          )}
+        </div>
+      </section>
     )
   }
 }
@@ -22,6 +35,14 @@ Wireframe.defaultContent = {
     content: 'Hello world',
     type: 'blockTitle',
   },
+  subtitle: {
+    content: 'Type here something',
+    type: 'subtitle',
+  },
+}
+
+Wireframe.modifierScheme = {
+  subtitle: {defaultValue: true, label: 'Block description', type: 'checkbox'},
 }
 
 export default Wireframe
