0be43dd80537a70ec00884a51e2811e6ef811841
JulBeet <julbeet@gmail.com>
2018-05-02 09:36:45 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,28 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  collection: {
+    ...Component.defaultContent.collection,
+    background: {
+      'nth-child': [
+        ['n', {
+          type: 'color',
+          color: 'light-accent-color',
+        }],
+        ['2n', {
+          type: 'color',
+          color: 'dark-accent-color',
+        }],
+      ],
+    },
+  },
+
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  'primary-button': {defaultValue: true, label: 'Primary button', type: 'checkbox'},
+  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
