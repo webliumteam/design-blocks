1365936fdc76f6cc47749c8c4f971820583af41a
JulBeet <julbeet@gmail.com>
2018-05-03 16:03:17 +0300

    design

--- src/index.js
+++ src/index.js
@@ -11,10 +11,48 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'color',
+    color: 'brand-color',
+  },
+  awards: [
+    {
+      ...Component.defaultContent.awards[0],
+      picture: {
+        ...Component.defaultContent.awards[0].picture,
+        resourceRef: 'picture1.png',
+      },
+    },
+    {
+      ...Component.defaultContent.awards[1],
+      picture: {
+        ...Component.defaultContent.awards[1].picture,
+        resourceRef: 'picture2.png',
+      },
+    },
+    {
+      ...Component.defaultContent.awards[2],
+      picture: {
+        ...Component.defaultContent.awards[2].picture,
+        resourceRef: 'picture3.png',
+      },
+    },
+    {
+      ...Component.defaultContent.awards[3],
+      picture: {
+        ...Component.defaultContent.awards[3].picture,
+        resourceRef: 'picture4.png',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: false, label: 'Block title', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Block description', type: 'hidden'},
+  heading: {defaultValue: false, label: 'Awards title', type: 'hidden'},
+  button: {defaultValue: false, label: 'Button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
