aa9e23647bb2f8c0a20ddbc59d860336f555bcd9
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-05-03 18:42:17 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,6 +11,97 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  logo: {
+    image: {
+      resourceRef: 'logo.png',
+      alt: 'Logo',
+      width: 171,
+      height: 44,
+    },
+  },
+  menu: [
+    {
+      id: 'home',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Home',
+      },
+    },
+    {
+      id: 'careers',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Careers',
+      },
+    },
+    {
+      id: 'blog',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Blog',
+      },
+    },
+    {
+      id: 'about',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'About Us',
+      },
+    },
+    {
+      id: 'testimonials',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Testimonials',
+      },
+    },
+    {
+      id: 'contact',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Contact Us',
+      },
+    },
+    {
+      id: 'services',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Services',
+      },
+    },
+    {
+      id: 'gallery',
+      metadata: {
+        ...Component.defaultContent.menu[0].metadata,
+        displayName: 'Gallery',
+      },
+    },
+  ],
+  socialIcons: {
+    ...Component.defaultContent.socialIcons,
+    networks: [
+      {
+        ...Component.defaultContent.socialIcons.networks[1],
+      },
+      {
+        ...Component.defaultContent.socialIcons.networks[0],
+      },
+      {
+        ...Component.defaultContent.socialIcons.networks[3],
+      },
+    ],
+    design: {
+      ...Component.defaultContent.socialIcons.design,
+      border: 'circle',
+      color: 'dark-shade-color',
+      size: 40,
+      offset: 10,
+    },
+  },
+  description: {
+    ...Component.defaultContent.description,
+    content: 'We work to promote the use of natural, organic food, growing our crops and raising our animals with the help of traditional chemical-free farming methods to improve the health of our community.',
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
