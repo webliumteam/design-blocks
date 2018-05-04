bf593d557937ed1b0c07246e912d4d37097c1340
t-nick <nickolaychuk.t@gmail.com>
2018-05-04 13:54:46 +0300

    fix icon

--- src/index.js
+++ src/index.js
@@ -37,6 +37,11 @@ ExtendedWireframe.defaultContent = {
     ...Component.defaultContent.cta,
     textValue: 'Get a quote',
     type: 'primary',
+    iconEnabled: true,
+    iconAlignment: 'right',
+    icon: {
+      svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125"><path d="M50 66.138l3.2-2.7 26-22-6.4-7.7-22.8 19.4-22.8-19.3-6.4 7.7 26 22 3.2 2.6z"/></svg>',
+    },
   },
 }
 
