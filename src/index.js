157377feabbcfd92a08f7c88404528ec48b63b33
artemvasilkin <artemvasilkin14@gmail.com>
2018-05-04 15:53:19 +0300

    fix collection, modifies, content, images

--- src/index.js
+++ src/index.js
@@ -11,68 +11,78 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  team: [
-    {
-      ...Component.defaultContent.team[0],
-      name: {
-        content: 'Dave Reez',
-        type: 'heading',
-      },
-      position: {
-        content: 'President',
-        type: 'caption',
-      },
-      description: {
-        content: 'Dave is a business owner and a second generation electrician. He has over 20 years of experience in the electrical industry. He strives to provide the excellent customer service where the quality comes first.',
-        type: 'text',
-      },
-      picture: {
-        src: 'image1.jpg',
-        alt: 'Dave Reez photo',
-      },
+  team: {
+    background: {
+      'nth-child': [
+        ['n', {
+          color: 'light-accent-color?lighten=4.71',
+          type: 'color',
+        }],
+      ],
     },
-    {
-      ...Component.defaultContent.team[1],
-      name: {
-        content: 'John Davidson',
-        type: 'heading',
+    items: [
+      {
+        ...Component.defaultContent.team[0],
+        name: {
+          ...Component.defaultContent.team[0].name,
+          content: 'Dave Reez',
+        },
+        position: {
+          ...Component.defaultContent.team[0].position,
+          content: 'President',
+        },
+        description: {
+          ...Component.defaultContent.team[0].description,
+          content: 'Dave is a business owner and a second generation electrician. He has over 20 years of experience in the electrical industry. He strives to provide the excellent customer service where the quality comes first.',
+        },
+        picture: {
+          ...Component.defaultContent.team[0].picture,
+          resourceRef: 'image1.jpg',
+        },
       },
-      position: {
-        content: 'Master Electrician',
-        type: 'caption',
+      {
+        ...Component.defaultContent.team[1],
+        name: {
+          ...Component.defaultContent.team[0].name,
+          content: 'John Davidson',
+        },
+        position: {
+          ...Component.defaultContent.team[0].position,
+          content: 'Master Electrician',
+        },
+        description: {
+          ...Component.defaultContent.team[0].description,
+          content: 'John is committed to delivering excellence in everything he does. He is highly trained, very knowledgeable, and experienced. John is focused on the complete customer satisfaction with the job. ',
+        },
+        picture: {
+          ...Component.defaultContent.team[0].picture,
+          resourceRef: 'image2.jpg',
+        },
       },
-      description: {
-        content: 'John is committed to delivering excellence in everything he does. He is highly trained, very knowledgeable, and experienced. John is focused on the complete customer satisfaction with the job. ',
-        type: 'text',
+      {
+        ...Component.defaultContent.team[2],
+        name: {
+          ...Component.defaultContent.team[0].name,
+          content: 'Anthony Taylor',
+        },
+        position: {
+          ...Component.defaultContent.team[0].position,
+          content: 'Founder',
+        },
+        description: {
+          ...Component.defaultContent.team[0].description,
+          content: 'Anthony has over 28 years of experience in the electrical contracting industry. His knowledge in various aspects of the industry places him at the forefront to deliver the experience and high quality of end product.',
+        },
+        picture: {
+          ...Component.defaultContent.team[0].picture,
+          resourceRef: 'image3.jpg',
+        },
       },
-      picture: {
-        src: 'image2.jpg',
-        alt: 'John Davidson photo',
-      },
-    },
-    {
-      ...Component.defaultContent.team[2],
-      name: {
-        content: 'Anthony Taylor',
-        type: 'heading',
-      },
-      position: {
-        content: 'Founder',
-        type: 'caption',
-      },
-      description: {
-        content: 'Anthony has over 28 years of experience in the electrical contracting industry. His knowledge in various aspects of the industry places him at the forefront to deliver the experience and high quality of end product.',
-        type: 'text',
-      },
-      picture: {
-        src: 'image3.jpg',
-        alt: 'Anthony Taylor photo',
-      },
-    },
-  ],
+    ],
+  },
   title: {
+    ...Component.defaultContent.title,
     content: 'Our team',
-    type: 'blockTitle',
   },
   background: {
     type: 'color',
@@ -82,11 +92,11 @@ ExtendedWireframe.defaultContent = {
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
-  'block-button': {defaultValue: false, label: 'Button', type: 'checkbox'},
-  'member-email': {defaultValue: false, label: 'Team member email', type: 'checkbox'},
-  'member-link': {defaultValue: false, label: 'Team member link', type: 'checkbox'},
-  subtitle: {defaultValue: false, label: 'Team description', type: 'checkbox'},
-  'member-social': {defaultValue: false, label: 'Team social buttons', type: 'checkbox'},
+  'block-button': {defaultValue: false, label: 'Button', type: 'hidden'},
+  'member-email': {defaultValue: false, label: 'Team member email', type: 'hidden'},
+  'member-link': {defaultValue: false, label: 'Team member link', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Team description', type: 'hidden'},
+  'member-social': {defaultValue: false, label: 'Team social buttons', type: 'hidden'},
 }
 
 export default ExtendedWireframe
