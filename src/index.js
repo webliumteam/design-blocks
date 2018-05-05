2f665054c63ea955181fb7d254fd7c4336c0c8d3
kirilldonenko <kirill.donenko@gmail.com>
2018-05-05 22:34:28 +0300

    style of block

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import style from './style.css'
 
 import options from './options.json'
 
-const {enhancers: {withProps}, hoistStatics} = $editor
+const {
+  enhancers: {withProps},
+  hoistStatics,
+} = $editor
 
 const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
 
@@ -14,12 +17,48 @@ ExtendedWireframe.defaultContent = {
   title: {
     ...Component.defaultContent.title,
     content: 'Get in Touch',
-    type: 'blockTitle',
   },
   subtitle: {
     ...Component.defaultContent.subtitle,
-    content: 'We are available 24/7 by fax, e-mail, or by phone. You can also use our quick contact form to ask a question<br/>about our services and products. We would be happy to answer your questions or offer any help.',
-    type: 'subtitle',
+    content:
+      'We are available 24/7 by fax, e-mail, or by phone. You can also use our quick contact form to ask a question about our services and products. We would be happy to answer your questions or offer any help.',
+  },
+  contactForm: {
+    ...Component.defaultContent.contactForm,
+    fields: [
+      {
+        ...Component.defaultContent.contactForm.fields[0],
+        id: 'contactForm_firstName',
+        title: 'First Name',
+        required: false,
+      },
+      {
+        ...Component.defaultContent.contactForm.fields[1],
+        type: 'text',
+        id: 'contactForm_lastName',
+        title: 'Last Name',
+        placeholder: '',
+        required: false,
+      },
+      {
+        ...Component.defaultContent.contactForm.fields[2],
+        title: 'E-mail',
+        required: false,
+      },
+      {
+        type: 'text',
+        id: 'contactForm_message',
+        title: 'Message',
+        placeholder: '',
+        required: false,
+      },
+    ],
+    submitButton: {
+      ...Component.defaultContent.submitButton,
+      title: 'Send',
+      size: 'sm',
+    },
+    submitButtonClassName: 'button button--size-sm button--primary form__button',
   },
 }
 
