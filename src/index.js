3fb4942f3d7f26e127b42d9bc88e0beee55ea8c3
artemvasilkin <artemvasilkin14@gmail.com>
2018-05-07 15:40:17 +0300

    add defaultOptions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import $editor from 'weblium/editor'
 import Component from 'wireframe-zapdos-careers/src/component'
 import style from './style.css'
 
+import defaultOptions from 'wireframe-zapdos-careers/src/options.json'
 import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
 
-const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', options))))(Component)
+const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _.set('$block.options', {...defaultOptions, ...options}))))(Component)
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
