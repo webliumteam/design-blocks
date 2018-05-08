7777b0a27a24060e20b2ecd879873ba1f9b12d35
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-05-08 13:45:44 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,33 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'bg.jpg',
+  },
+  title: {
+    ...Component.defaultContent.title,
+    color: 'dark-shade-color',
+  },
+  subtitle: {
+    ...Component.defaultContent.subtitle,
+    color: 'dark-shade-color',
+    brightness: 45,
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  align: {
+    ...Component.modifierScheme.align,
+    defaultValue: 'center',
+  },
+  'primary-btn': {defaultValue: false, label: 'Button', type: 'hidden'},
+  'secondary-btn': {defaultValue: false, label: 'Additional button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
