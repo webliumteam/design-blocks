6cd7989249b3078c68714fd6175402c0797df612
Vladimir Zelik <kazantip81v@gmail.com>
2018-05-09 02:13:50 +0300

    add style & images

--- src/index.js
+++ src/index.js
@@ -11,10 +11,55 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'pic1@2x.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[1],
+      picture: {
+        ...Component.defaultContent.gallery[1].picture,
+        resourceRef: 'pic2@2x.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[2],
+      picture: {
+        ...Component.defaultContent.gallery[2].picture,
+        resourceRef: 'pic3@2x.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[3],
+      picture: {
+        ...Component.defaultContent.gallery[3].picture,
+        resourceRef: 'pic4@2x.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[4],
+      picture: {
+        ...Component.defaultContent.gallery[4].picture,
+        resourceRef: 'pic5@2x.png',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[5],
+      picture: {
+        ...Component.defaultContent.gallery[5].picture,
+        resourceRef: 'pic6@2x.png',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: false, label: 'Gallery title', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
