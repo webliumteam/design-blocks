d1bb087b3c99519db532f6d042445674f139d549
JulBeet <julbeet@gmail.com>
2018-05-08 17:05:52 +0300

    design markup added

--- src/index.js
+++ src/index.js
@@ -11,10 +11,60 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  gallery: [
+    {
+      ...Component.defaultContent.gallery[0],
+      picture: {
+        ...Component.defaultContent.gallery[0].picture,
+        resourceRef: 'picture1.jpg',
+      },
+
+    },
+    {
+      ...Component.defaultContent.gallery[1],
+      picture: {
+        ...Component.defaultContent.gallery[1].picture,
+        resourceRef: 'picture2.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[2],
+      picture: {
+        ...Component.defaultContent.gallery[2].picture,
+        resourceRef: 'picture3.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[3],
+      picture: {
+        ...Component.defaultContent.gallery[3].picture,
+        resourceRef: 'picture4.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[4],
+      picture: {
+        ...Component.defaultContent.gallery[4].picture,
+        resourceRef: 'picture5.jpg',
+      },
+    },
+    {
+      ...Component.defaultContent.gallery[5],
+      picture: {
+        ...Component.defaultContent.gallery[5].picture,
+        resourceRef: 'picture6.jpg',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  title: {defaultValue: false, label: 'Gallery title', type: 'hidden'},
+  subtitle: {defaultValue: false, label: 'Gallery description', type: 'hidden'},
+  'body-text': {defaultValue: false, label: 'Image description', type: 'hidden'},
+  caption: {defaultValue: false, label: 'Image caption', type: 'hidden'},
+  'secondary-button': {defaultValue: false, label: 'Button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
