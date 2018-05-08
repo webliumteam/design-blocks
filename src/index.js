175c82835a3a67b7b2aca169b95a5af0ca8ca90f
tangerine69 <mrtangerine69@gmail.com>
2018-05-08 20:42:54 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,39 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  cta: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Request a quote',
+    type: 'primary',
+    size: 'lg',
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '90% 78%',
+    },
+    resourceRef: 'bg-img.jpg',
+    overlay: {
+      type: 'color',
+      color: 'dark-shade-color',
+      opacity: 0.35,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
