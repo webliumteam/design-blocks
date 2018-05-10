6593f0a0b14305da6e71bbd45d6014ad27c2a2a3
GlushchukOl <olga.glushchuk@weblium.com>
2018-05-10 11:02:58 +0300

    upd modifier

--- src/component.js
+++ src/component.js
@@ -136,11 +136,11 @@ Block.modifierScheme = {
     type: 'radio-button-group',
     style: 'buttons',
   },
-  title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
+  title: {defaultValue: true, label: 'Hero title', type: 'checkbox'},
   subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
-  'header-button': {defaultValue: false, label: 'Header button', type: 'hidden'},
   'form-title': {defaultValue: true, label: 'Form title', type: 'checkbox'},
   'form-description': {defaultValue: true, label: 'Form description', type: 'checkbox'},
+  'header-button': {defaultValue: false, label: 'Header button', type: 'hidden'},
 }
 
 export default Block
