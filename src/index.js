771e0add5830078946f8c93dea0c7c5aa1ec3445
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2018-05-11 14:55:38 +0300

    finish style design block

--- src/index.js
+++ src/index.js
@@ -11,10 +11,66 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  title: {
+    ...Component.defaultContent.title,
+    content: 'Testimonials',
+  },
+  team: [
+    {
+      ...Component.defaultContent.team[0],
+      name: {
+        ...Component.defaultContent.team[0].name,
+        content: 'Amanda Peterson',
+      },
+      description: {
+        ...Component.defaultContent.team[0].description,
+        content: '“I was happy to work with Quantum because their efficient solutions helped us improve plenty of business processes. As a result, our clients receive their products faster and are more satisfied with our services. They deserve the highest grade!”',
+      },
+      picture: {
+        ...Component.defaultContent.team[0].picture,
+        resourceRef: 'pic1.png',
+      },
+    },
+    {
+      ...Component.defaultContent.team[1],
+      name: {
+        ...Component.defaultContent.team[1].name,
+        content: 'Amanda Peterson',
+      },
+      description: {
+        ...Component.defaultContent.team[1].description,
+        content: '“I was happy to work with Quantum because their efficient solutions helped us improve plenty of business processes. As a result, our clients receive their products faster and are more satisfied with our services. They deserve the highest grade!”',
+      },
+      picture: {
+        ...Component.defaultContent.team[1].picture,
+        resourceRef: 'pic2.png',
+      },
+    },
+    {
+      ...Component.defaultContent.team[2],
+      name: {
+        ...Component.defaultContent.team[2].name,
+        content: 'Amanda Peterson',
+      },
+      description: {
+        ...Component.defaultContent.team[2].description,
+        content: '“I was happy to work with Quantum because their efficient solutions helped us improve plenty of business processes. As a result, our clients receive their products faster and are more satisfied with our services. They deserve the highest grade!”',
+      },
+      picture: {
+        ...Component.defaultContent.team[2].picture,
+        resourceRef: 'pic3.png',
+      },
+    },
+  ],
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: false, label: 'Team description', type: 'hidden'},
+  'member-link': {defaultValue: false, label: 'Link', type: 'hidden'},
+  'member-email': {defaultValue: false, label: 'Team member contacts', type: 'hidden'},
+  'member-social': {defaultValue: false, label: 'Social buttons', type: 'hidden'},
+  'block-button': {defaultValue: false, label: 'Secondary button', type: 'hidden'},
 }
 
 export default ExtendedWireframe
