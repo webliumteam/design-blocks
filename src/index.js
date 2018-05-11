4bfa7b50cf5afb7c24dbb1bf6ae5bc9c6ad606ef
nataliadiadkova <nataliadiadkova@gmail.com>
2018-05-11 17:53:04 +0300

    design

--- src/index.js
+++ src/index.js
@@ -12,12 +12,9 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  title: {
-    content: 'My Partners',
-    type: 'blockTitle',
-  },
   partners: [
     {
+      ...Component.defaultContent.partners[0],
       logo: {
         image: {
           resourceRef: 'award-1.png',
@@ -25,30 +22,9 @@ ExtendedWireframe.defaultContent = {
           height: 73,
         },
       },
-      title: {
-        content: 'Samsung',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Back in 1995, we started cooperation with this brand. Samsung became our main supplier of high-quality tech products. Today, Samsung is our main partner who supports each initiative and actively participates in company development.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[1],
       logo: {
         image: {
           resourceRef: 'award-2.png',
@@ -56,30 +32,9 @@ ExtendedWireframe.defaultContent = {
           height: 66,
         },
       },
-      title: {
-        content: 'Ford ',
-        type: 'heading',
-      },
-      desc: {
-        content: 'The company that has always put reliability first. We started to work with Ford in 2002, and now this is one of the partners who’ve been with us for more than 15 years! We believe that this partnership is one of the achievements we can be proud of.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[2],
       logo: {
         image: {
           resourceRef: 'award-3.png',
@@ -87,58 +42,15 @@ ExtendedWireframe.defaultContent = {
           height: 50,
         },
       },
-      title: {
-        content: 'Medical Family',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Health of our team members is a top priority for us. Therefore, we work with the best clinics only, like Medical Family. We entrust our health to them and we entrust our business as well.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
-      },
     },
     {
+      ...Component.defaultContent.partners[3],
       logo: {
         image: {
           resourceRef: 'award-4.png',
           width: 184,
           height: 39,
         },
-      },
-      title: {
-        content: 'Coffee Break Now',
-        type: 'heading',
-      },
-      desc: {
-        content: 'Coffee and cakes won’t sell themselves. Whenever we are hungry or just need to restore energy, Coffee Break Now saves us. Also, they cater all our events, from workshops and seminars to everyday meals for our team members.',
-        type: 'text',
-      },
-      button: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Partner’s website',
-        type: 'link',
       },
     },
   ],
