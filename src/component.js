efa28cd5faf790368e9ce132dedf62ea6b7869d3
t-nick <nickolaychuk.t@gmail.com>
2018-05-21 17:19:19 +0300

    add bcg collection

--- src/component.js
+++ src/component.js
@@ -15,19 +15,19 @@ class Block extends React.Component {
   itemHeader = (itemNumber) => {
     const {components: {Text, Image}, style} = this.props
     return [
-      this.getModifierValue('post-image') && (
+      this.getModifierValue('item_image') && (
         <Image
           wrapperClassName={style['article__picture-wrapper']}
           pictureClassName={style.article__picture}
           imgClassName={style.article__image}
-          bind={`blog[${itemNumber}].picture`}
+          bind={`collection[${itemNumber}].item_image`}
           size={{'min-width: 768px': 570, 'min-width: 480px': 768, 'min-width: 320px': 480}}
         />
       ),
-      this.getModifierValue('post-publish-date') && (
+      this.getModifierValue('item_date') && (
         <small className={style.article__meta}>
-          {!this.getOptionValue('hidden-category') && <Text tagName="span" bind={`blog[${itemNumber}].category`} className={style.article__category} /> }
-          {!this.getOptionValue('hidden-date') && <Text tagName="span" bind={`blog[${itemNumber}].date`} className={style.article__date} /> }
+          {!this.getOptionValue('hidden-category') && <Text tagName="span" bind={`collection[${itemNumber}].item_category`} className={style.article__category} /> }
+          {!this.getOptionValue('hidden-date') && <Text tagName="span" bind={`collection[${itemNumber}].item_date`} className={style.article__date} /> }
         </small>
       ),
     ]
@@ -39,16 +39,16 @@ class Block extends React.Component {
       <article className={classNames(style.article, className)}>
         {children}
         {this.getOptionValue('picture-with-date') ? <div className={style.article__header}>{this.itemHeader(index)}</div> : this.itemHeader(index)}
-        <Text tagName="h2" className={style.article__title} bind={`blog[${index}].title`} />
-        {this.getModifierValue('post-description') && (
-        <Text tagName="p" className={style.article__text} bind={`blog[${index}].description`} />
+        <Text tagName="h2" className={style.article__title} bind={`collection[${index}].item_title`} />
+        {this.getModifierValue('item_body') && (
+        <Text tagName="p" className={style.article__text} bind={`collection[${index}].item_body`} />
         )}
-        {this.getModifierValue('post-link') && (
+        {this.getModifierValue('item_button') && (
           <Button
             className={style.article__link}
             buttonClassName={style.button}
             linkClassName={style.link}
-            bind={`blog[${index}].link`}
+            bind={`collection[${index}].item_button`}
           />
         )}
       </article>
@@ -60,8 +60,8 @@ class Block extends React.Component {
     return (
       <section className={style.section}>
         <div className={style.section__inner}>
-          {this.getModifierValue('top-icon') && (
-            <Icon className={style['top-icon']} bind="topIcon" />
+          {this.getModifierValue('icon_decorator') && (
+            <Icon className={style['top-icon']} bind="icon_decorator" />
           )}
           <Text tagName="h1" className={style.title} bind="title" />
           {this.getModifierValue('subtitle') && (
@@ -69,19 +69,19 @@ class Block extends React.Component {
             )}
           <Collection
             className={style['articles-wrapper']}
-            bind="blog"
+            bind="collection"
             Item={this.collectionItem}
             fakeHelpers={{
               count: 2,
               className: style.fake,
             }}
           />
-          {this.getModifierValue('block-button') && (
+          {this.getModifierValue('button') && (
             <div className={style['btns-group']}>
               <Button
                 className={style.button}
                 linkClassName={style.link}
-                bind="cta"
+                bind="button"
               />
             </div>
           )}
@@ -94,117 +94,7 @@ class Block extends React.Component {
 Block.components = _.pick(['Collection', 'Text', 'Button', 'Image', 'Icon'])($editor.components)
 
 Block.defaultContent = {
-  blog: [
-    {
-      title: {
-        content: 'How to Hire the Best Employees to Your Company?',
-        type: 'heading',
-      },
-      description: {
-        content: 'Our HR Director shares his experience how to fill positions with the best candidates, where to find talents, and how to attract professionals to your business. ',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
-      picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-    },
-    {
-      title: {
-        content: 'How to Achieve Higher Profits in Retail with One Product?',
-        type: 'heading',
-      },
-      description: {
-        content: 'Do you want to achieve higher profits this year? Our new product will help you get what you want. In this article, you will find out how to use it to get more benefits.',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
-      picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-    },
-    {
-      title: {
-        content: 'Top 5 Tips to Improve Your Engineering Department.',
-        type: 'heading',
-      },
-      description: {
-        content: 'You engineers can bring you better results! Get to know how to improve engineering department to make a new step for your company growth. ',
-        type: 'text',
-      },
-      category: {
-        content: 'Creative Process',
-        type: 'caption',
-      },
-      date: {
-        content: 'September 22, 2017',
-        type: 'caption',
-      },
-      picture: {
-        src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        alt: 'Article illustration photo',
-      },
-      link: {
-        actionConfig: {
-          action: 'link',
-          actions: {
-            link: {
-              type: '',
-              innerPage: '',
-              url: '',
-            },
-          },
-        },
-        textValue: 'Learn more',
-        type: 'link',
-      },
-    },
-  ],
-  topIcon: {
+  icon_decorator: {
     svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42"><path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"/></svg>',
     fill: 'red',
   },
@@ -216,30 +106,103 @@ Block.defaultContent = {
     content: 'Subtitle here',
     type: 'subtitle',
   },
-  cta: {
-    actionConfig: {
-      action: 'link',
-      actions: {
-        link: {
-          type: '',
-          innerPage: '',
-          url: '',
+  collection: {
+    background: {},
+    items: [
+      {
+        item_title: {
+          content: 'How to Hire the Best Employees to Your Company?',
+          type: 'heading',
+        },
+        item_body: {
+          content: 'Our HR Director shares his experience how to fill positions with the best candidates, where to find talents, and how to attract professionals to your business. ',
+          type: 'text',
+        },
+        item_category: {
+          content: 'Creative Process',
+          type: 'caption',
+        },
+        item_date: {
+          content: 'September 22, 2017',
+          type: 'caption',
+        },
+        item_image: {
+          src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+          alt: 'Article illustration photo',
+        },
+        item_button: {
+          textValue: 'Learn more',
+          type: 'link',
         },
       },
-    },
+      {
+        item_title: {
+          content: 'How to Achieve Higher Profits in Retail with One Product?',
+          type: 'heading',
+        },
+        item_body: {
+          content: 'Do you want to achieve higher profits this year? Our new product will help you get what you want. In this article, you will find out how to use it to get more benefits.',
+          type: 'text',
+        },
+        item_category: {
+          content: 'Creative Process',
+          type: 'caption',
+        },
+        item_date: {
+          content: 'September 22, 2017',
+          type: 'caption',
+        },
+        item_image: {
+          src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+          alt: 'Article illustration photo',
+        },
+        item_button: {
+          textValue: 'Learn more',
+          type: 'link',
+        },
+      },
+      {
+        item_title: {
+          content: 'Top 5 Tips to Improve Your Engineering Department.',
+          type: 'heading',
+        },
+        item_body: {
+          content: 'You engineers can bring you better results! Get to know how to improve engineering department to make a new step for your company growth. ',
+          type: 'text',
+        },
+        item_category: {
+          content: 'Creative Process',
+          type: 'caption',
+        },
+        item_date: {
+          content: 'September 22, 2017',
+          type: 'caption',
+        },
+        item_image: {
+          src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+          alt: 'Article illustration photo',
+        },
+        item_button: {
+          textValue: 'Learn more',
+          type: 'link',
+        },
+      },
+    ],
+  },
+  button: {
     textValue: 'Learn more',
     type: 'secondary',
   },
 }
 
 Block.modifierScheme = {
-  'post-image': {defaultValue: true, label: 'Image', type: 'checkbox'},
-  'post-publish-date': {defaultValue: true, label: 'Date of publishing', type: 'checkbox'},
-  'post-description': {defaultValue: true, label: 'Post main text', type: 'checkbox'},
-  'post-link': {defaultValue: true, label: 'Link', type: 'checkbox'},
-  'block-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
+  icon_decorator: {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
   subtitle: {defaultValue: false, label: 'Subtitle', type: 'hidden'},
-  'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden'},
+  item_image: {defaultValue: true, label: 'Image', type: 'checkbox'},
+  item_date: {defaultValue: true, label: 'Date of publishing', type: 'checkbox'},
+  item_body: {defaultValue: true, label: 'Post main text', type: 'checkbox'},
+  item_button: {defaultValue: true, label: 'Link', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 
