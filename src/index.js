3095e2278f66613fc8c6f215d265fc2df8a4fcea
tangerine69 <mrtangerine69@gmail.com>
2018-05-24 17:25:26 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,55 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  collection: {
+    background: {
+      'nth-child': [
+        ['3n+2', {
+          color: 'brand-color',
+          type: 'color',
+        }],
+        ['3n+3', {
+          color: 'brand-color',
+          type: 'color',
+        }],
+      ],
+    },
+    items: [
+      {
+        ...Component.defaultContent.collection.items[0],
+        image: {
+          resourceRef: 'image-1.jpg',
+          alt: 'Service illustration photo',
+        },
+      },
+      {
+        ...Component.defaultContent.collection.items[1],
+        image: {
+          resourceRef: 'image-2.jpg',
+          alt: 'Service illustration photo',
+        },
+      },
+      {
+        ...Component.defaultContent.collection.items[2],
+        image: {
+          resourceRef: 'image-3.jpg',
+          alt: 'Service illustration photo',
+        },
+      },
+      {
+        ...Component.defaultContent.collection.items[3],
+        image: {
+          resourceRef: 'image-4.jpg',
+          alt: 'Service illustration photo',
+        },
+      },
+    ],
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  cta: {defaultValue: true, label: 'Primary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
