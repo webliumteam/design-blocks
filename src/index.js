6400142ba9395667c6006de124c7093d47e278b1
isergienko <igorserhiyenko@gmail.com>
2018-05-24 14:37:47 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -11,10 +11,47 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  collection: {
+    background: {
+      'nth-child': [
+        ['1', {
+          ...Component.defaultContent.collection.background['nth-child'][0][1],
+          src: '1.jpg',
+          imageColor: [216, 216, 216],
+        }],
+        ['2', {
+          ...Component.defaultContent.collection.background['nth-child'][0][1],
+          src: '2.jpg',
+          imageColor: [216, 216, 216],
+        }],
+        ['3', {
+          ...Component.defaultContent.collection.background['nth-child'][0][1],
+          src: '3.jpg',
+          imageColor: [216, 216, 216],
+        }],
+        ['4', {
+          ...Component.defaultContent.collection.background['nth-child'][0][1],
+          src: '4.jpg',
+          imageColor: [216, 216, 216],
+        }],
+        ['5', {
+          ...Component.defaultContent.collection.background['nth-child'][0][1],
+          src: '5.jpg',
+          imageColor: [216, 216, 216],
+        }],
+        ['6', {
+          ...Component.defaultContent.collection.background['nth-child'][0][1],
+          src: '6.jpg',
+          imageColor: [216, 216, 216],
+        }],
+      ],
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  button: {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
