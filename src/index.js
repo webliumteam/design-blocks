fd0e2959c832837481a868de308430daf3a1a211
nataliadiadkova <nataliadiadkova@gmail.com>
2018-05-24 18:02:36 +0300

    init d/poliwag/services_w10_1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-default-master/src/component'
+import Component from 'wireframe-series-10-services/src/component'
 import style from './style.css'
 
 import options from './options.json'
