1162616f93d804fb5108c4cbf2d0d68812a009be
t-nick <nickolaychuk.t@gmail.com>
2018-05-25 12:22:46 +0300

    fix bg

--- src/index.js
+++ src/index.js
@@ -12,129 +12,140 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  team: [
-    {
-      ...Component.defaultContent.team[0],
-      picture: {
-        ...Component.defaultContent.team[0].picture,
-        resourceRef: 'photo1.png',
-        size: {
-          'min-width: 320px': 240,
-        },
-      },
-      socialIcons: {
-        networks: [
-          {
-            id: 'twitter',
-            name: 'Twitter',
-            url: 'https://twitter.com/',
-          },
-          {
-            id: 'facebook',
-            name: 'Facebook',
-            url: 'https://facebook.com/',
-          },
-          {
-            id: 'linkedin',
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/',
-          },
-        ],
-        target: '_blank',
-        design: {
-          border: 'circle',
-          innerFill: true,
-          preset: 'preset001',
-          color: '',
-          sizes: [15, 25, 35, 45],
-          size: 25,
-          offset: 5,
-        },
-      },
+  team: {
+    background: {
+      'nth-child': [
+        ['n', {
+          color: 'darkShade?lighten=95.53',
+          type: 'color',
+        }],
+      ],
     },
-    {
-      ...Component.defaultContent.team[1],
-      picture: {
-        ...Component.defaultContent.team[1].picture,
-        resourceRef: 'photo2.png',
-        size: {
-          'min-width: 320px': 240,
+    items: [
+      {
+        ...Component.defaultContent.team[0],
+        picture: {
+          ...Component.defaultContent.team[0].picture,
+          resourceRef: 'photo1.png',
+          size: {
+            'min-width: 320px': 240,
+          },
+        },
+        socialIcons: {
+          networks: [
+            {
+              id: 'twitter',
+              name: 'Twitter',
+              url: 'https://twitter.com/',
+            },
+            {
+              id: 'facebook',
+              name: 'Facebook',
+              url: 'https://facebook.com/',
+            },
+            {
+              id: 'linkedin',
+              name: 'LinkedIn',
+              url: 'https://www.linkedin.com/',
+            },
+          ],
+          target: '_blank',
+          design: {
+            border: 'circle',
+            innerFill: true,
+            preset: 'preset001',
+            color: 'brand-color',
+            sizes: [15, 25, 35, 45],
+            size: 25,
+            offset: 5,
+          },
         },
       },
-      socialIcons: {
-        networks: [
-          {
-            id: 'twitter',
-            name: 'Twitter',
-            url: 'https://twitter.com/',
+      {
+        ...Component.defaultContent.team[1],
+        picture: {
+          ...Component.defaultContent.team[1].picture,
+          resourceRef: 'photo2.png',
+          size: {
+            'min-width: 320px': 240,
           },
-          {
-            id: 'facebook',
-            name: 'Facebook',
-            url: 'https://facebook.com/',
+        },
+        socialIcons: {
+          networks: [
+            {
+              id: 'twitter',
+              name: 'Twitter',
+              url: 'https://twitter.com/',
+            },
+            {
+              id: 'facebook',
+              name: 'Facebook',
+              url: 'https://facebook.com/',
+            },
+            {
+              id: 'linkedin',
+              name: 'LinkedIn',
+              url: 'https://www.linkedin.com/',
+            },
+          ],
+          target: '_blank',
+          design: {
+            border: 'circle',
+            innerFill: true,
+            preset: 'preset001',
+            color: 'brand-color',
+            sizes: [15, 25, 35, 45],
+            size: 25,
+            offset: 5,
           },
-          {
-            id: 'linkedin',
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/',
-          },
-        ],
-        target: '_blank',
-        design: {
-          border: 'circle',
-          innerFill: true,
-          preset: 'preset001',
-          color: '',
-          sizes: [15, 25, 35, 45],
-          size: 25,
-          offset: 5,
         },
       },
-    },
-    {
-      ...Component.defaultContent.team[2],
-      picture: {
-        ...Component.defaultContent.team[2].picture,
-        resourceRef: 'photo3.png',
-        size: {
-          'min-width: 320px': 240,
+      {
+        ...Component.defaultContent.team[2],
+        picture: {
+          ...Component.defaultContent.team[2].picture,
+          resourceRef: 'photo3.png',
+          size: {
+            'min-width: 320px': 240,
+          },
+        },
+        socialIcons: {
+          networks: [
+            {
+              id: 'twitter',
+              name: 'Twitter',
+              url: 'https://twitter.com/',
+            },
+            {
+              id: 'facebook',
+              name: 'Facebook',
+              url: 'https://facebook.com/',
+            },
+            {
+              id: 'linkedin',
+              name: 'LinkedIn',
+              url: 'https://www.linkedin.com/',
+            },
+          ],
+          target: '_blank',
+          design: {
+            border: 'circle',
+            innerFill: true,
+            preset: 'preset001',
+            color: 'brand-color',
+            sizes: [15, 25, 35, 45],
+            size: 25,
+            offset: 5,
+          },
         },
       },
-      socialIcons: {
-        networks: [
-          {
-            id: 'twitter',
-            name: 'Twitter',
-            url: 'https://twitter.com/',
-          },
-          {
-            id: 'facebook',
-            name: 'Facebook',
-            url: 'https://facebook.com/',
-          },
-          {
-            id: 'linkedin',
-            name: 'LinkedIn',
-            url: 'https://www.linkedin.com/',
-          },
-        ],
-        target: '_blank',
-        design: {
-          border: 'circle',
-          innerFill: true,
-          preset: 'preset001',
-          color: '',
-          sizes: [15, 25, 35, 45],
-          size: 25,
-          offset: 5,
-        },
-      },
-    },
-  ],
+    ],
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
+  subtitle: {defaultValue: true, label: 'Team description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
