5c92fda596b2ef2a84f498e66e067eb5bc8dd62e
Vladimir Zelik <kazantip81v@gmail.com>
2018-05-28 23:03:02 +0300

    add style & background-image

--- src/index.js
+++ src/index.js
@@ -11,6 +11,114 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
+  collection: {
+    ...Component.defaultContent.collection,
+    items: [
+      {
+        ...Component.defaultContent.collection.items[0],
+        icon: {
+          ...Component.defaultContent.collection.items[0].icon,
+          fill: 'brand-color',
+        },
+        advantages: [
+          {
+            ...Component.defaultContent.collection.items[0].advantages[0],
+            icon: {
+              ...Component.defaultContent.collection.items[0].advantages[0].icon,
+              fill: 'brand-color',
+            },
+          },
+          {
+            ...Component.defaultContent.collection.items[0].advantages[1],
+            icon: {
+              ...Component.defaultContent.collection.items[0].advantages[1].icon,
+              fill: 'brand-color',
+            },
+          },
+          {
+            ...Component.defaultContent.collection.items[0].advantages[2],
+            icon: {
+              ...Component.defaultContent.collection.items[0].advantages[2].icon,
+              fill: 'brand-color',
+            },
+          },
+        ],
+      },
+      {
+        ...Component.defaultContent.collection.items[1],
+        icon: {
+          ...Component.defaultContent.collection.items[1].icon,
+          fill: 'brand-color',
+        },
+        advantages: [
+          {
+            ...Component.defaultContent.collection.items[1].advantages[0],
+            icon: {
+              ...Component.defaultContent.collection.items[1].advantages[0].icon,
+              fill: 'brand-color',
+            },
+          },
+          {
+            ...Component.defaultContent.collection.items[1].advantages[1],
+            icon: {
+              ...Component.defaultContent.collection.items[1].advantages[1].icon,
+              fill: 'brand-color',
+            },
+          },
+          {
+            ...Component.defaultContent.collection.items[1].advantages[2],
+            icon: {
+              ...Component.defaultContent.collection.items[1].advantages[2].icon,
+              fill: 'brand-color',
+            },
+          },
+        ],
+      },
+      {
+        ...Component.defaultContent.collection.items[2],
+        icon: {
+          ...Component.defaultContent.collection.items[2].icon,
+          fill: 'brand-color',
+        },
+        advantages: [
+          {
+            ...Component.defaultContent.collection.items[2].advantages[0],
+            icon: {
+              ...Component.defaultContent.collection.items[2].advantages[0].icon,
+              fill: 'brand-color',
+            },
+          },
+          {
+            ...Component.defaultContent.collection.items[2].advantages[1],
+            icon: {
+              ...Component.defaultContent.collection.items[2].advantages[1].icon,
+              fill: 'brand-color',
+            },
+          },
+          {
+            ...Component.defaultContent.collection.items[2].advantages[2],
+            icon: {
+              ...Component.defaultContent.collection.items[2].advantages[2].icon,
+              fill: 'brand-color',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  background: {
+    type: 'image',
+    position: {
+      type: 'cover',
+      cover: '50% 50%',
+    },
+    resourceRef: 'karim-ghantous-329207-unsplash.jpg',
+    overlay: {
+      type: 'color',
+      color: 'light-shade-color',
+      opacity: 0.75,
+    },
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
