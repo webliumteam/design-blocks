a3200f2749db90c09f5334a980950101a3941dc4
nmelnychenko <mykola.melnichenko@gmail.com>
2018-05-30 18:10:39 +0300

    update version

--- src/index.js
+++ src/index.js
@@ -12,66 +12,95 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  background: {
-    type: 'image',
-    position: {
-      type: 'cover',
-      cover: '50% 50%',
-    },
-    resourceRef: 'bg.png',
-    overlay: {
-      type: 'color',
-      color: 'dark-shade-color',
-      opacity: 0.3,
-    },
+  collection: {
+    ...Component.defaultContent.collection,
+    items: [
+      {
+        ...Component.defaultContent.collection.items[0],
+        background: {
+          type: 'image',
+          position: {
+            type: 'cover',
+            cover: '50% 50%',
+          },
+          resourceRef: 'bg.png',
+          overlay: {
+            type: 'color',
+            color: 'dark-shade-color',
+            opacity: 0.3,
+          },
+        },
+        item_heading: {
+          ...Component.defaultContent.collection.items[0].item_heading,
+          content: '<span style="font-weight: bold;">QUANTUM</span> COMPANY',
+        },
+        item_button: {
+          ...Component.defaultContent.collection.items[0].item_button,
+          type: 'secondary',
+        },
+        item_button_additional: {
+          ...Component.defaultContent.collection.items[0].item_button_additional,
+          type: 'primary-alt',
+        },
+      },
+      {
+        ...Component.defaultContent.collection.items[0],
+        background: {
+          type: 'image',
+          position: {
+            type: 'cover',
+            cover: '50% 50%',
+          },
+          resourceRef: 'bg.png',
+          overlay: {
+            type: 'color',
+            color: 'dark-shade-color',
+            opacity: 0.3,
+          },
+        },
+        item_heading: {
+          ...Component.defaultContent.collection.items[0].item_heading,
+          content: '<span style="font-weight: bold;">QUANTUM</span> COMPANY',
+        },
+        item_button: {
+          ...Component.defaultContent.collection.items[0].item_button,
+          type: 'secondary',
+        },
+        item_button_additional: {
+          ...Component.defaultContent.collection.items[0].item_button_additional,
+          type: 'primary-alt',
+        },
+      },
+      {
+        ...Component.defaultContent.collection.items[0],
+        background: {
+          type: 'image',
+          position: {
+            type: 'cover',
+            cover: '50% 50%',
+          },
+          resourceRef: 'bg.png',
+          overlay: {
+            type: 'color',
+            color: 'dark-shade-color',
+            opacity: 0.3,
+          },
+        },
+        item_heading: {
+          ...Component.defaultContent.collection.items[0].item_heading,
+          content: '<span style="font-weight: bold;">QUANTUM</span> COMPANY',
+        },
+        item_button: {
+          ...Component.defaultContent.collection.items[0].item_button,
+          type: 'secondary',
+        },
+        item_button_additional: {
+          ...Component.defaultContent.collection.items[0].item_button_additional,
+          type: 'primary-alt',
+        },
+      },
+    ],
   },
-  cover: [
-    {
-      ...Component.defaultContent.cover[0],
-      title: {
-        ...Component.defaultContent.cover[0].title,
-        content: '<span style="font-weight: bold;">QUANTUM</span><br>COMPANY',
-      },
-      cta: {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'secondary',
-      },
-      'cta-2': {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'primary-alt',
-      },
-    },
-    {
-      ...Component.defaultContent.cover[0],
-      title: {
-        ...Component.defaultContent.cover[0].title,
-        content: '<span style="font-weight: bold;">QUANTUM</span><br>COMPANY',
-      },
-      cta: {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'secondary',
-      },
-      'cta-2': {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'primary-alt',
-      },
-    },
-    {
-      ...Component.defaultContent.cover[0],
-      title: {
-        ...Component.defaultContent.cover[0].title,
-        content: '<span style="font-weight: bold;">QUANTUM</span><br>COMPANY',
-      },
-      cta: {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'secondary',
-      },
-      'cta-2': {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'primary-alt',
-      },
-    },
-  ],
 }
 
 ExtendedWireframe.modifierScheme = {
