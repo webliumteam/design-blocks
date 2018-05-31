f90c8c1fd3b88425a88cb3c589cb058475c90e22
nmelnychenko <mykola.melnichenko@gmail.com>
2018-05-31 12:03:16 +0300

    rewrite into full height slider

--- src/index.js
+++ src/index.js
@@ -19,68 +19,71 @@ ExtendedWireframe.defaultContent = {
     },
     resourceRef: 'slide.jpg',
   },
-  cover: [
-    {
-      ...Component.defaultContent.cover[0],
-      subtitle: {
-        ...Component.defaultContent.cover[0].subtitle,
-        content: '<span style="font-weight: bold;">WE IMPLEMENT INNOVATIVE PROJECTS</span>',
+  collection: {
+    ...Component.defaultContent.collection,
+    items: [
+      {
+        ...Component.defaultContent.collection.items[0],
+        item_subheading: {
+          ...Component.defaultContent.collection.items[0].item_subheading,
+          content: '<span style="font-weight: bold;">WE IMPLEMENT INNOVATIVE PROJECTS</span>',
+        },
+        item_button: {
+          ...Component.defaultContent.collection.items[0].item_button,
+          type: 'secondary',
+          size: 'lg',
+        },
+        item_button_additional: {
+          ...Component.defaultContent.collection.items[0].item_button_additional,
+          type: 'primary-alt',
+          size: 'lg',
+        },
       },
-      cta: {
-        ...Component.defaultContent.cover[0].cta,
-        type: 'secondary',
-        size: 'lg',
+      {
+        ...Component.defaultContent.collection.items[1],
+        item_subheading: {
+          ...Component.defaultContent.collection.items[1].item_subheading,
+          content: '<span style="font-weight: bold;">WE IMPLEMENT INNOVATIVE PROJECTS</span>',
+        },
+        item_button: {
+          ...Component.defaultContent.collection.items[1].item_button,
+          type: 'secondary',
+          size: 'lg',
+        },
+        item_button_additional: {
+          ...Component.defaultContent.collection.items[1].item_button_additional,
+          type: 'primary-alt',
+          size: 'lg',
+        },
       },
-      'cta-2': {
-        ...Component.defaultContent.cover[0]['cta-2'],
-        type: 'primary-alt',
-        size: 'lg',
+      {
+        ...Component.defaultContent.collection.items[2],
+        item_subheading: {
+          ...Component.defaultContent.collection.items[2].item_subheading,
+          content: '<span style="font-weight: bold;">WE IMPLEMENT INNOVATIVE PROJECTS</span>',
+        },
+        item_button: {
+          ...Component.defaultContent.collection.items[2].item_button,
+          type: 'secondary',
+          size: 'lg',
+        },
+        item_button_additional: {
+          ...Component.defaultContent.collection.items[2].item_button_additional,
+          type: 'primary-alt',
+          size: 'lg',
+        },
       },
-    },
-    {
-      ...Component.defaultContent.cover[1],
-      subtitle: {
-        ...Component.defaultContent.cover[1].subtitle,
-        content: '<span style="font-weight: bold;">WE IMPLEMENT INNOVATIVE PROJECTS</span>',
-      },
-      cta: {
-        ...Component.defaultContent.cover[1].cta,
-        type: 'secondary',
-        size: 'lg',
-      },
-      'cta-2': {
-        ...Component.defaultContent.cover[1]['cta-2'],
-        type: 'primary-alt',
-        size: 'lg',
-      },
-    },
-    {
-      ...Component.defaultContent.cover[2],
-      subtitle: {
-        ...Component.defaultContent.cover[2].subtitle,
-        content: '<span style="font-weight: bold;">WE IMPLEMENT INNOVATIVE PROJECTS</span>',
-      },
-      cta: {
-        ...Component.defaultContent.cover[2].cta,
-        type: 'secondary',
-        size: 'lg',
-      },
-      'cta-2': {
-        ...Component.defaultContent.cover[2]['cta-2'],
-        type: 'primary-alt',
-        size: 'lg',
-      },
-    },
-  ],
+    ],
+  },
 }
 
 
 ExtendedWireframe.modifierScheme = {
+  item_subheading: {defaultValue: true, label: 'Title description', type: 'checkbox'},
+  item_button: {defaultValue: true, label: 'Primary button', type: 'checkbox'},
+  item_button_additional: {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
   arrows: {defaultValue: false, label: 'Navigation arrows', type: 'hidden'},
   dots: {defaultValue: true, label: 'Navigation indicators', type: 'hidden'},
-  'primary-button': {defaultValue: true, label: 'Primary button', type: 'checkbox'},
-  'secondary-button': {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
-  subtitle: {defaultValue: true, label: 'Title description', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
