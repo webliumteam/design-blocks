910f6576e6e47ed588fba3c8b6391c373835256c
nmelnychenko <mykola.melnichenko@gmail.com>
2018-06-01 10:54:59 +0300

    update version

--- src/index.js
+++ src/index.js
@@ -12,22 +12,33 @@ const ExtendedWireframe = hoistStatics(withProps(_.pipe(_.set('style', style), _
 
 ExtendedWireframe.defaultContent = {
   ...Component.defaultContent,
-  services: [
-    {
-      ...Component.defaultContent.services.items[0],
-      picture: {
-        ...Component.defaultContent.services.items[0].picture,
-        resourceRef: 'jack-kaminski-129845-unsplash.jpg',
+  services: {
+    ...Component.defaultContent.services,
+    items: [
+      {
+        ...Component.defaultContent.services.items[0],
+        background: {
+          type: 'image',
+          position: {
+            type: 'cover',
+            cover: '50% 50%',
+          },
+          src: 'jack-kaminski-129845-unsplash.jpg',
+        },
       },
-    },
-    {
-      ...Component.defaultContent.services.items[1],
-      picture: {
-        ...Component.defaultContent.services.items[1].picture,
-        resourceRef: 'julien-pianetti-230463-unsplash.jpg',
+      {
+        ...Component.defaultContent.services.items[1],
+        background: {
+          type: 'image',
+          position: {
+            type: 'cover',
+            cover: '50% 50%',
+          },
+          src: 'julien-pianetti-230463-unsplash.jpg',
+        },
       },
-    },
-  ],
+    ],
+  },
 }
 
 ExtendedWireframe.modifierScheme = {
