ded8aa7d178b231c9491bd04fd7e9e623e51b0d3
nmelnychenko <mykola.melnichenko@gmail.com>
2018-06-07 12:57:14 +0300

    create markup

--- src/component.js
+++ src/component.js
@@ -31,6 +31,17 @@ class Block extends React.Component {
       this.getModifierValue('text')
     )
 
+    const articleHeader = this.getModifierValue('category') ||
+      this.getModifierValue('title') ||
+      this.getModifierValue('subtitle')
+
+    const authorText = this.getModifierValue('name') ||
+      this.getModifierValue('date') ||
+      this.getModifierValue('divider') ||
+      this.getModifierValue('time')
+
+    const authorInfo = this.getModifierValue('avatar') || authorText
+
     const getMinResize = this.getOptionValue('min-resize') ? this.getOptionValue('min-resize') : 46
     const getMaxResize = this.getOptionValue('max-resize') ? this.getOptionValue('max-resize') : 100
 
@@ -39,7 +50,7 @@ class Block extends React.Component {
     return (
       <section className={classNames(style.section, {[style['section--column']]: columnLayout}, arrange && style['section--reverse'])}>
         <div className={style.section__inner}>
-          <div className={style.article}>
+          <article className={style.article}>
             {this.getModifierValue('article-picture') && (
               <Image
                 wrapperClassName={style['article__picture-wrapper']}
@@ -50,20 +61,52 @@ class Block extends React.Component {
                 resize={{min: getMinResize, max: getMaxResize, disable: this.getOptionValue('disable-resizer')}}
               />
             )}
-            {!onlyImage && (
-              <div className={style.article__content}>
-                {this.getModifierValue('title') &&
-                  <Text tagName="h1" className={style.article__title} bind="title" />
-                }
-                {this.getModifierValue('subtitle') && (
-                  <Text tagName="p" className={style.article__subtitle} bind="subtitle" />
-                )}
-                {this.getModifierValue('text') && (
-                  <Text tagName="p" className={style.article__text} bind="text" />
-                )}
-              </div>
-            )}
-          </div>
+            {!onlyImage && [
+              articleHeader && (
+                <header className={style.article__header}>
+                  {this.getModifierValue('category') &&
+                    <Text tagName="p" className={style.article__category} bind="category" />
+                  }
+                  {this.getModifierValue('title') &&
+                    <Text tagName="h1" className={style.article__title} bind="title" />
+                  }
+                  {this.getModifierValue('subtitle') && (
+                    <Text tagName="p" className={style.article__subtitle} bind="subtitle" />
+                  )}
+                  {authorInfo && (
+                    <div className={style.author}>
+                      {this.getModifierValue('avatar') && (
+                        <Image
+                          wrapperClassName={style['author__picture-wrapper']}
+                          pictureClassName={style.author__picture}
+                          imgClassName={style.author__image}
+                          bind="author_picture"
+                          size={this.getImageSize(columnLayout)}
+                          resize={{disable: true}}
+                        />
+                      )}
+                      {authorText && (
+                        <div className={style.author__info}>
+                          {this.getModifierValue('name') && (
+                            <Text tagName="span" className={style.author__name} bind="author_name" />
+                          )}
+                          {this.getModifierValue('date') && (
+                            <Text tagName="time" className={classNames(style.author__date, this.getModifierValue('divider') && style['author__date--decorated'])} bind="author_date" />
+                          )}
+                          {this.getModifierValue('time') && (
+                            <Text tagName="span" className={style.author__time} />
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </header>),
+              this.getModifierValue('body') && (
+                <div className={style.article__content}>
+                  <Text tagName="p" className={style.article__body} bind="body" />
+                </div>),
+            ]}
+          </article>
         </div>
       </section>
     )
@@ -73,15 +116,19 @@ class Block extends React.Component {
 Block.components = _.pick(['Text', 'Image', 'Button', 'SocialIcons', 'Icon'])($editor.components)
 
 Block.defaultContent = {
+  category: {
+    content: 'LIFE',
+    type: 'caption',
+  },
   title: {
-    content: 'About The Company',
+    content: 'Humans And Aliens',
     type: 'blockTitle',
   },
   subtitle: {
     content: 'We provide a wide range of services to meet even the most daring requirements. ',
     type: 'subtitle',
   },
-  text: {
+  body: {
     content: 'Our team consists of highly motivated and skilled specialists who know how to deal with any issue that you may come across. This creates a basis for lasting relationships with our clients built on trust and mutual understanding. We are devoted to creating unique and innovative solutions along with the high-quality supporting services. ',
     type: 'text',
   },
@@ -99,7 +146,7 @@ Block.modifierScheme = {
   },
   title: {defaultValue: true, label: 'Block title', type: 'checkbox'},
   subtitle: {defaultValue: false, label: 'Subtitle', type: 'checkbox'},
-  text: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
+  body: {defaultValue: true, label: 'Company main text', type: 'checkbox'},
   'article-picture': {defaultValue: true, label: 'Article picture', type: 'checkbox'},
 }
 
