d7473ca5b9a2b43d1665f87aae1cc4fe4a741a34
artemvasilkin <artemvasilkin14@gmail.com>
2018-06-21 11:51:15 +0300

    add theme components

--- src/component.js
+++ src/component.js
@@ -15,8 +15,8 @@ class Block extends React.Component {
       <section className={style.section}>
         <Background className={style.section__inner} bind="formBackground">
           <header className={style.section__header}>
-            {this.getModifierValue('title') && <Text tagName="h1" className={style.title} bind="title" />}
-            {this.getModifierValue('subtitle') && <Text tagName="p" className={style.subtitle} bind="subtitle" />}
+            {this.getModifierValue('title') && <Text tagName="h1" className={classNames(style.title, 'title')} bind="title" />}
+            {this.getModifierValue('subtitle') && <Text tagName="p" className={classNames(style.subtitle, 'subtitle')} bind="subtitle" />}
           </header>
           <ContactForm
             bind="contactForm"
