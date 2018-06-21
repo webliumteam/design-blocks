c55e5b57b88b367ab657e6e6adc885257c5fbce8
artemvasilkin <artemvasilkin14@gmail.com>
2018-06-21 13:51:44 +0300

    add theme components

--- src/component.js
+++ src/component.js
@@ -133,10 +133,10 @@ class Wireframe extends React.Component {
           {header && (
             <div className={style.section__header}>
               {this.getModifierValue('title') && (
-                <Text tagName="h1" className={style.title} bind="title" />
+                <Text tagName="h1" className={classNames(style.title, 'title')} bind="title" />
               )}
               {this.getModifierValue('subtitle') && (
-                <Text tagName="p" className={style.subtitle} bind="subtitle" />
+                <Text tagName="p" className={classNames(style.subtitle, 'subtitle')} bind="subtitle" />
               )}
             </div>
           )}
