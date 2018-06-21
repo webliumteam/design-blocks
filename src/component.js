3c034d07da34910eae1c4b394ecad274a1235da6
artemvasilkin <artemvasilkin14@gmail.com>
2018-06-21 13:02:50 +0300

    add theme components

--- src/component.js
+++ src/component.js
@@ -57,8 +57,8 @@ class Block extends React.Component {
         <div className={css.section__inner}>
           <div className={css.section__header}>
             {this.getModifierValue('title') &&
-              <Text tagName="h1" className={css.title} bind="title" />}
-            {this.getModifierValue('subtitle') && <Text tagName="p" className={css.subtitle} bind="subtitle" />}
+              <Text tagName="h1" className={classNames(css.title, 'title')} bind="title" />}
+            {this.getModifierValue('subtitle') && <Text tagName="p" className={classNames(css.subtitle, 'subtitle')} bind="subtitle" />}
           </div>
           <Collection
             className={css['items-wrapper']}
