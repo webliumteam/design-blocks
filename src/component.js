c4aaa176a1d6f387768edc11c70fc4424e55fb8b
t-nick <nickolaychuk.t@gmail.com>
2018-06-26 13:25:56 +0300

    add first content

--- src/component.js
+++ src/component.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-class Wireframe extends React.Component {
+class Block extends React.Component {
   static propTypes = {
     components: PropTypes.object.isRequired,
     $block: PropTypes.object.isRequired,
@@ -12,15 +12,102 @@ class Wireframe extends React.Component {
   getOptionValue = (path, defaultValue = false) =>
     _.getOr(defaultValue, ['options', path], this.props.$block)
 
+  getImageSize = fullWidth =>
+    fullWidth
+      ? {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 1170}
+      : {'min-width: 320px': 480, 'min-width: 480px': 768, 'min-width: 768px': 570}
+
   render() {
-    const {components: {Text}, style} = this.props
+    const {components: {Text, Image, Button}, style: css} = this.props
+    const columnLayout = !(
+      this.getModifierValue('heading') ||
+      this.getModifierValue('date') ||
+      this.getModifierValue('category') ||
+      this.getModifierValue('body') ||
+      this.getModifierValue('button')
+    )
+
+    const onlyImage = !(
+      this.getModifierValue('heading') ||
+      this.getModifierValue('date') ||
+      this.getModifierValue('category') ||
+      this.getModifierValue('body') ||
+      this.getModifierValue('button')
+    )
+
+    const getMinResize = this.getOptionValue('min-resize') ? this.getOptionValue('min-resize') : 46
+    const getMaxResize = this.getOptionValue('max-resize') ? this.getOptionValue('max-resize') : 100
+
+    const arrange = this.getModifierValue('arrange-elements')
+    const categoryShow =
+      this.getModifierValue('category') || this.getModifierValue('date')
 
     return (
-      <section className={style.section}>
-        <div className={style.section__inner}>
-          <Text bind="title" className={style.title} tagName="h2" />
-          {this.getModifierValue('subtitle') && (
-            <Text bind="subtitle" className={style.subtitle} tagName="p" />
+      <section className={classNames(css.section, {[css['section--column']]: columnLayout}, arrange && css['section--reverse'])}>
+        <div className={css.section__inner}>
+          <div className={css.section__header}>
+            {this.getModifierValue('title') && (
+            <Text bind="title" className={css.title} tagName="h2" />)}
+            {this.getModifierValue('subtitle') && (
+              <Text bind="subtitle" className={css.subtitle} tagName="p" />
+            )}
+          </div>
+          <div className={css.article}>
+            {this.getModifierValue('image') && (
+              <Image
+                wrapperClassName={css['article__picture-wrapper']}
+                pictureClassName={css.article__picture}
+                imgClassName={css.article__image}
+                bind="image"
+                size={this.getImageSize(columnLayout)}
+                resize={{min: getMinResize, max: getMaxResize, disable: this.getOptionValue('disable-resizer')}}
+              />
+            )}
+            {!onlyImage && (
+              <div className={css.article__content}>
+                {categoryShow && (
+                  <div className={classNames(css['article__category-wrapper'])}>
+                    {this.getModifierValue('date') && (
+                      <Text
+                        tagName="p"
+                        className={css.article__date}
+                        bind="date"
+                      />
+                    )}
+                    {this.getModifierValue('category') && (
+                      <Text
+                        tagName="p"
+                        className={css.article__category}
+                        bind="category"
+                      />
+                    )}
+                  </div>
+                )}
+                {this.getModifierValue('heading') && (
+                  <Text tagName="p" className={classNames(css.article__heading)} bind="heading" />
+                )}
+                {this.getModifierValue('body') && (
+                  <Text tagName="p" className={css.article__text} bind="body" />
+                )}
+                {this.getModifierValue('button') && (
+                  <Button
+                    className={css.article__link}
+                    buttonClassName={css.button}
+                    linkClassName={css.link}
+                    bind="button"
+                  />
+                )}
+              </div>
+            )}
+          </div>
+          {this.getModifierValue('button_additional') && (
+            <div className={css['btns-group']}>
+              <Button
+                buttonClassName={css.button}
+                linkClassName={css.link}
+                bind="button_additional"
+              />
+            </div>
           )}
         </div>
       </section>
@@ -28,21 +115,82 @@ class Wireframe extends React.Component {
   }
 }
 
-Wireframe.components = _.pick(['Text'])($editor.components)
+Block.components = _.pick(['Text', 'Image', 'Button'])($editor.components)
 
-Wireframe.defaultContent = {
+Block.defaultContent = {
   title: {
-    content: 'Hello world',
+    content: 'Blog',
     type: 'blockTitle',
   },
   subtitle: {
-    content: 'Type here something',
+    content: 'We provide a wide range of services to meet even the most daring requirements. ',
     type: 'subtitle',
+  },
+  heading: {
+    content: 'How to hire the best employees to your company?',
+    type: 'heading',
+  },
+  category: {
+    content: 'Category1',
+    type: 'caption',
+  },
+  date: {
+    content: 'September 16, 2017',
+    type: 'caption',
+  },
+  body: {
+    content: 'Our HR Director shares his experience how to fill positions with the best candidates, where to find talents, and how to attract professionals to your business.',
+    type: 'text',
+  },
+  image: {
+    src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
+    alt: 'Picture about the company',
+  },
+  button: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Read more',
+    type: 'link',
+  },
+  button_additional: {
+    actionConfig: {
+      action: 'link',
+      actions: {
+        link: {
+          type: '',
+          innerPage: '',
+          url: '',
+        },
+      },
+    },
+    textValue: 'Learn more',
+    type: 'secondary',
   },
 }
 
-Wireframe.modifierScheme = {
-  subtitle: {defaultValue: true, label: 'Block description', type: 'checkbox'},
+Block.modifierScheme = {
+  'arrange-elements': {
+    defaultValue: false,
+    name: 'Arrange elements',
+    type: 'swap',
+  },
+  title: {defaultValue: true, label: 'Blog title', type: 'checkbox'},
+  subtitle: {defaultValue: false, label: 'Blog subtitle', type: 'checkbox'},
+  image: {defaultValue: true, label: 'Article picture', type: 'hidden'},
+  date: {defaultValue: true, label: 'Blog date', type: 'checkbox'},
+  category: {defaultValue: true, label: 'Blog category', type: 'checkbox'},
+  heading: {defaultValue: true, label: 'Blog heading', type: 'checkbox'},
+  body: {defaultValue: true, label: 'Blog main text', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Blog button', type: 'checkbox'},
+  button_additional: {defaultValue: true, label: 'Button', type: 'checkbox'},
 }
 
-export default Wireframe
+export default Block
