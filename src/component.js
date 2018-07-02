1f89174d2139edb78e1d26ff82256da672dbc4a0
t-nick <nickolaychuk.t@gmail.com>
2018-07-02 15:37:47 +0300

    add text-center

--- src/component.js
+++ src/component.js
@@ -77,8 +77,8 @@ class Block extends React.Component {
       <section className={style.section}>
         <div className={style.section__inner}>
           <div className={style.section__header}>
-            {this.getModifierValue('title') && <Text tagName="h1" className={classNames(style.title, 'title')} bind="title" />}
-            {this.getModifierValue('subtitle') && <Text tagName="p" className={classNames(style.subtitle, 'subtitle')} bind="subtitle" />}
+            {this.getModifierValue('title') && <Text tagName="h1" className={classNames(style.title, 'title', 'text-center')} bind="title" />}
+            {this.getModifierValue('subtitle') && <Text tagName="p" className={classNames(style.subtitle, 'subtitle', 'text-center')} bind="subtitle" />}
           </div>
           <Slider
             className={style['preview-slider']}
