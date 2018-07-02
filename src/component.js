716bd7528492974ac2df24c4fa35e54d527f6bd7
t-nick <nickolaychuk.t@gmail.com>
2018-07-02 13:07:48 +0300

    add text-center

--- src/component.js
+++ src/component.js
@@ -60,10 +60,10 @@ class Block extends React.Component {
           {header && (
             <header className={style.section__header}>
               {this.getModifierValue('title') && (
-                <Text bind="title" className={classNames(style.title, 'title')} tagName="h1" />
+                <Text bind="title" className={classNames(style.title, 'title', 'text-center')} tagName="h1" />
               )}
               {this.getModifierValue('subtitle') && (
-                <Text bind="subtitle" className={classNames(style.subtitle, 'subtitle')} tagName="p" />
+                <Text bind="subtitle" className={classNames(style.subtitle, 'subtitle', 'text-center')} tagName="p" />
               )}
             </header>
           )}
