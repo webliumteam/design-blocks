b1df59b799be410833ef65ae98d21c75582b6ec2
t-nick <nickolaychuk.t@gmail.com>
2018-07-03 12:51:22 +0300

    add theme typography

--- src/component.js
+++ src/component.js
@@ -10,40 +10,59 @@ class Block extends React.Component {
   getModifierValue = path => _.get(['modifier', path], this.props.$block)
 
   render() {
-    const {components: {Text, Button, SocialIcons, Icon}, style} = this.props
+    const {
+      components: {Text, Button, SocialIcons, Icon},
+      style,
+    } = this.props
 
-    const showButtonGroups = this.getModifierValue('button') || this.getModifierValue('additional-button')
+    const showButtonGroups =
+      this.getModifierValue('button') || this.getModifierValue('additional-button')
 
-    const alignClass = this.getModifierValue('align') !== 'left'
-      ? style[`section--${this.getModifierValue('align')}`]
-      : ''
-    const textAlignmentClass = this.getModifierValue('block-alignment') !== 'right' ? `text-${alignClass}` : ''
+    const alignClass =
+      this.getModifierValue('align') !== 'left'
+        ? style[`section--${this.getModifierValue('align')}`]
+        : ''
+    const textAlignmentClass =
+      this.getModifierValue('block-alignment') !== 'right' ? `text-${alignClass}` : ''
 
     return (
       <section className={classNames(style.section, alignClass)}>
         <div className={style.section__inner}>
           {this.getModifierValue('top-icon') && (
-            <Icon
-              className={style['top-icon']}
-              bind="topIcon"
-            />
+            <Icon className={style['top-icon']} bind="topIcon" />
           )}
 
           <article className={style.article}>
             {this.getModifierValue('top-caption') && (
-              <Text bind="top-caption" className={style['caption-decorator']} tagName="div" />
+              <Text bind="top-caption" className={classNames(style['caption-decorator'], 'body', textAlignmentClass)} tagName="div" />
             )}
             {this.getModifierValue('title') && (
-              <Text bind="title" className={classNames(style.article__title, 'title', textAlignmentClass)} tagName="h1" />
+              <Text
+                bind="title"
+                className={classNames(style.article__title, 'title', textAlignmentClass)}
+                tagName="h1"
+              />
             )}
             {this.getModifierValue('heading') && (
-              <Text bind="heading" className={style.article__heading} tagName="h2" />
+              <Text
+                bind="heading"
+                className={classNames(style.article__heading, 'heading', textAlignmentClass)}
+                tagName="h2"
+              />
             )}
             {this.getModifierValue('subheading') && (
-              <Text bind="subheading" className={style.article__subheading} tagName="p" />
+              <Text
+                bind="subheading"
+                className={classNames(style.article__subheading, 'subheading', textAlignmentClass)}
+                tagName="p"
+              />
             )}
             {this.getModifierValue('text') && (
-              <Text bind="text" className={style.article__text} tagName="p" />
+              <Text
+                bind="text"
+                className={classNames(style.article__text, 'body', textAlignmentClass)}
+                tagName="p"
+              />
             )}
             {this.getModifierValue('social-icons') && (
               <div className={style['socials-wrapper']}>
@@ -53,18 +72,10 @@ class Block extends React.Component {
             {showButtonGroups && (
               <div className={style['btns-group']}>
                 {this.getModifierValue('button') && (
-                  <Button
-                    className={style.button}
-                    linkClassName={style.link}
-                    bind="button-1"
-                  />
+                  <Button className={style.button} linkClassName={style.link} bind="button-1" />
                 )}
                 {this.getModifierValue('additional-button') && (
-                  <Button
-                    className={style.button}
-                    linkClassName={style.link}
-                    bind="button-2"
-                  />
+                  <Button className={style.button} linkClassName={style.link} bind="button-2" />
                 )}
               </div>
             )}
@@ -100,7 +111,8 @@ Block.defaultContent = {
     type: 'subheading',
   },
   text: {
-    content: 'We provide a whole bulk of services to ensure stable growth of the company. The profound expertise of our team, deep understanding of all business aspects, ideal knowledge of processes will help to deal with various issues.<br><br> We create unique and innovative solutions for our customers along with the high-quality support services and personal approach to any case. Feel free to entrust your business to our experts, and you’ll see the difference! ',
+    content:
+      'We provide a whole bulk of services to ensure stable growth of the company. The profound expertise of our team, deep understanding of all business aspects, ideal knowledge of processes will help to deal with various issues.<br><br> We create unique and innovative solutions for our customers along with the high-quality support services and personal approach to any case. Feel free to entrust your business to our experts, and you’ll see the difference! ',
     type: 'text',
   },
   'button-1': {
@@ -182,9 +194,19 @@ Block.modifierScheme = {
   heading: {defaultValue: false, label: 'Company title', type: 'checkbox', sortOrder: 30},
   subheading: {defaultValue: false, label: 'Company description', type: 'checkbox', sortOrder: 40},
   text: {defaultValue: true, label: 'Company main text', type: 'checkbox', sortOrder: 50},
-  'social-icons': {defaultValue: true, label: 'Social media buttons', type: 'checkbox', sortOrder: 60},
+  'social-icons': {
+    defaultValue: true,
+    label: 'Social media buttons',
+    type: 'checkbox',
+    sortOrder: 60,
+  },
   button: {defaultValue: false, label: 'Button', type: 'checkbox', sortOrder: 70},
-  'additional-button': {defaultValue: true, label: 'Additional button', type: 'checkbox', sortOrder: 80},
+  'additional-button': {
+    defaultValue: true,
+    label: 'Additional button',
+    type: 'checkbox',
+    sortOrder: 80,
+  },
   'top-caption': {defaultValue: false, label: 'Top caption', type: 'hidden', sortOrder: 90},
   'top-icon': {defaultValue: false, label: 'Top icon decorator', type: 'hidden', sortOrder: 100},
 }
