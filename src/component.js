8b16633421c6c8b64eb11485a1ef8ff5227343d6
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-03 17:54:18 +0300

    add alignment

--- src/component.js
+++ src/component.js
@@ -19,10 +19,10 @@ class Wireframe extends React.Component {
           {headerContent && (
             <div className={style.section__header}>
               {this.getModifierValue('title') && (
-                <Text tagName="h1" className={classNames(style.title, 'title')} bind="title" />
+                <Text tagName="h1" className={classNames(style.title, 'title', 'text-center', 'text-md-left')} bind="title" />
               )}
               {this.getModifierValue('subtitle') && (
-                <Text tagName="p" className={classNames(style.subtitle, 'subtitle')} bind="subtitle" />
+                <Text tagName="p" className={classNames(style.subtitle, 'subtitle', 'text-center', 'text-md-left')} bind="subtitle" />
               )}
             </div>
           )}
