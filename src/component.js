97be8ea391ff33af87e324f7bc20932ede59b8dd
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-04 18:49:24 +0300

    add theme images

--- src/component.js
+++ src/component.js
@@ -20,9 +20,9 @@ class Wireframe extends React.Component {
         {children}
         {this.getModifierValue('item-image') && (
           <Image
-            wrapperClassName={style['item__pic-wrapper']}
-            pictureClassName={style.item__pic}
-            imgClassName={style.item__img}
+            wrapperClassName={classNames(style['item__pic-wrapper'], 'picture-wrapper')}
+            pictureClassName={classNames(style.item__pic, 'picture')}
+            imgClassName={classNames(style.item__img, 'picture__image')}
             bind={`collection[${index}].image`}
             size={{
               'min-width: 320px': 120,
