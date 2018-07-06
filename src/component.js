5e238e0268002799b077018f599ca04fa48f0936
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-06 11:57:59 +0300

    add theme icon

--- src/component.js
+++ src/component.js
@@ -81,7 +81,7 @@ class Block extends React.Component {
       <section className={style.section}>
         <div className={style.section__inner}>
           {this.getModifierValue('top-icon') && (
-            <Icon className={style['top-icon']} bind="topIcon" />
+            <Icon className={classNames(style['top-icon'], 'icon')} bind="topIcon" />
           )}
           {(this.getModifierValue('title') || this.getModifierValue('subtitle')) && (
             <header className={style.section__header}>
