82f547952ecc3ba08d4905ff0c6ad9fa0464ba3a
t-nick <nickolaychuk.t@gmail.com>
2018-07-12 12:35:18 +0300

    add button

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ ExtendedWireframe.defaultContent = {
 ExtendedWireframe.modifierScheme = {
   ...Component.modifierScheme,
   subtitle: {defaultValue: true, label: 'Gallery description', type: 'checkbox'},
+  button: {defaultValue: true, label: 'Secondary button', type: 'checkbox'},
 }
 
 export default ExtendedWireframe
