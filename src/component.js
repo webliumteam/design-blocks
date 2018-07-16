9297bac4494215b7f4010daf572b66783b924e1d
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-16 16:27:10 +0300

    add wt-components

--- src/component.js
+++ src/component.js
@@ -25,7 +25,7 @@ class Block extends React.Component {
           {this.getModifierValue('item-icon') && (
             <div className={style['item__icon-wrapper']}>
               <Icon
-                className={classNames(style.item__icon, 'icon')}
+                className={classNames(style.item__icon, 'wt-icon')}
                 bind={`items[${index}].icon`}
               />
             </div>
@@ -34,20 +34,20 @@ class Block extends React.Component {
             <div className={style.item__info}>
               <Text
                 tagName="h2"
-                className={classNames(style.item__title, 'heading')}
+                className={classNames(style.item__title, 'wt-heading')}
                 bind={`items[${index}].heading`}
               />
               {this.getModifierValue('employment') && (
                 <Text
                   tagName="p"
-                  className={classNames(style.item__text, 'body')}
+                  className={classNames(style.item__text, 'wt-body')}
                   bind={`items[${index}].employment`}
                 />
               )}
               {this.getModifierValue('position') && (
                 <Text
                   tagName="p"
-                  className={classNames(style.item__text, 'body')}
+                  className={classNames(style.item__text, 'wt-body')}
                   bind={`items[${index}].location`}
                 />
               )}
@@ -80,13 +80,13 @@ class Block extends React.Component {
             )}
             <Text
               tagName="h1"
-              className={classNames(css.title, 'title', 'text-center')}
+              className={classNames(css.title, 'wt-title', 'wt-text-center')}
               bind="title"
             />
             {this.getModifierValue('subtitle') && (
               <Text
                 tagName="p"
-                className={classNames(css.subtitle, 'subtitle', 'text-center')}
+                className={classNames(css.subtitle, 'wt-subtitle', 'wt-text-center')}
                 bind="subtitle"
               />
             )}
@@ -102,7 +102,7 @@ class Block extends React.Component {
           }}
         />
         {this.getModifierValue('secondary-button') && (
-          <div className={classNames(css['btns-group'], 'btns-group')}>
+          <div className={classNames(css['btns-group'], 'wt-btns-group')}>
             <div className="btns-group__inner">
               <Button
                 className={classNames(css.button, 'button')}
