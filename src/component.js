f3ccc3721c648b60d00919af5081af4a40029498
zhtbcgtw <ivanov.i@weblium.com>
2018-07-19 17:43:39 +0300

    add 'ui' to src/component.js

--- src/component.js
+++ src/component.js
@@ -46,6 +46,8 @@ class Block extends React.Component {
     )
   }
 
+  ui = value => _.get('$block.modifier.__enableThemes', this.props) ? value : null
+
   render() {
     const {components: {Text, Collection, Button}, style, $block} = this.props
     const showButtonGroups = this.getModifierValue('main-button') || this.getModifierValue('additional-button')
