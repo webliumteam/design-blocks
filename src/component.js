b834b104ce70fde551394ec99f3e0392dc62a16e
zhtbcgtw <ivanov.i@weblium.com>
2018-07-19 17:42:27 +0300

    add 'ui' to src/component.js

--- src/component.js
+++ src/component.js
@@ -112,6 +112,8 @@ class Block extends React.Component {
     )
   }
 
+  ui = value => _.get('$block.modifier.__enableThemes', this.props) ? value : null
+
   render() {
     const {components: {Background}, style} = this.props
     const {opened} = this.state
