aff56d9b37cf4298cc986e9a26a3a3298fac7952
zhtbcgtw <ivanov.i@weblium.com>
2018-07-19 17:41:51 +0300

    add 'ui' to src/component.js

--- src/component.js
+++ src/component.js
@@ -80,6 +80,8 @@ class Block extends React.Component {
 
   companyCollectionItem = this.createCollectionItem('companies')
 
+  ui = value => _.get('$block.modifier.__enableThemes', this.props) ? value : null
+
   render() {
     const {
       components: {Collection, Text, Button, Icon},
