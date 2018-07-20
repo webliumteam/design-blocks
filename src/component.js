e2b50d95392a391aac1835c7887509f7bff7d8aa
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-20 10:47:44 +0300

    start

--- src/component.js
+++ src/component.js
@@ -12,9 +12,11 @@ class Wireframe extends React.Component {
   getOptionValue = (path, defaultValue = false) =>
     _.getOr(defaultValue, ['options', path], this.props.$block)
 
-
   collectionItem = ({index, children, className}) => {
-    const {components: {Image}, style} = this.props
+    const {
+      components: {Image},
+      style,
+    } = this.props
 
     return (
       <div className={classNames(style.item, className)}>
@@ -25,22 +27,75 @@ class Wireframe extends React.Component {
   }
 
   render() {
-    const {components: {Text, Button, Collection}, style} = this.props
+    const {
+      components: {Text, Button, Collection},
+      style,
+    } = this.props
 
     return (
       <section className={classNames(style.section, 'section')}>
         <div className={classNames(style.section__inner, 'section__inner')}>
           <header className={classNames(style.section__header, 'section__header')}>
-            <Text bind="title" className={classNames(style.title, 'title', 'text-center')} tagName="h2" />
+            <Text
+              bind="title"
+              className={classNames(style.title, 'title', 'text-center')}
+              tagName="h2"
+            />
             {this.getModifierValue('subtitle') && (
-              <Text bind="subtitle" className={classNames(style.subtitle, 'subtitle', 'text-center')} tagName="p" />
+              <Text
+                bind="subtitle"
+                className={classNames(style.subtitle, 'subtitle', 'text-center')}
+                tagName="p"
+              />
             )}
           </header>
           <div className={classNames('section__content')}>
-            <Collection
+            <Slider
+              className={style['preview-slider']}
+              bind="gallery"
+              Item={this.collectionItemPreview}
+              setRef={this.setSliderRef('nav1')}
+              settings={{
+                dots: false,
+                arrows: false,
+                asNavFor: this.state.nav2,
+              }}
+              itemProps={{
+                modifier: $block.modifier,
+              }}
+              disableControls
+            />
+            <Slider
               className={style['items-wrapper']}
-              bind="collection"
+              bind="gallery"
               Item={this.collectionItem}
+              setRef={this.setSliderRef('nav2')}
+              settings={{
+                dots: false,
+                arrows: true,
+                focusOnSelect: true,
+                slidesToScroll: 1,
+                slidesToShow: 5,
+                asNavFor: this.state.nav1,
+                responsive: [
+                  {
+                    breakpoint: 767,
+                    settings: {
+                      slidesToShow: 1,
+                    },
+                  },
+                  {
+                    breakpoint: 991,
+                    settings: {
+                      slidesToShow: 4,
+                    },
+                  },
+                ],
+                ...customArrows,
+              }}
+              itemProps={{
+                modifier: $block.modifier,
+              }}
             />
           </div>
           <footer className={classNames(style.section__footer, 'section__footer')}>
