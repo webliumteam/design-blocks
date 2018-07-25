8e7100d8cf87eaf06299ea35d09cc55e553efa1b
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-25 16:55:29 +0300

    update version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-series-4-careers/src/component'
+import Component from 'wireframe-series-4-careers_old/src/component'
 import style from './style.css'
 
 import options from './options.json'
