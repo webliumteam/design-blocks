cdf6bb3baa588e2ec9e80573a2f2f9f21c2199ff
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-25 15:18:01 +0300

    update version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-zapdos-cover/src/component'
+import Component from 'wireframe-zapdos-cover_old/src/component'
 import style from './style.css'
 
-import defaultOptions from 'wireframe-zapdos-cover/src/options.json'
+import defaultOptions from 'wireframe-zapdos-cover_old/src/options.json'
 import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
