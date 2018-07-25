40f02bd563ff8ddc8faa53bc9d49f8c17677a6c8
artemvasilkin <artemvasilkin14@gmail.com>
2018-07-25 17:52:35 +0300

    update version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import $editor from 'weblium/editor'
 
-import Component from 'wireframe-series-1-partners/src/component'
+import Component from 'wireframe-series-1-partners_old/src/component'
 import style from './style.css'
 
-import defaultOptions from 'wireframe-series-1-partners/src/options.json'
+import defaultOptions from 'wireframe-series-1-partners_old/src/options.json'
 import options from './options.json'
 
 const {enhancers: {withProps}, hoistStatics} = $editor
