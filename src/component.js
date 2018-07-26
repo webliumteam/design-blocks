bb6d761deae2421d31a047f973804fb453af36d2
Oleksandr Samohrai <sam_gray@ukr.net>
2018-07-26 18:38:38 +0300

    init

--- src/component.js
+++ src/component.js
@@ -1,4 +1,5 @@
 import $editor from 'weblium/editor'
+import DecoratorSvg from './decorator'
 
 class Wireframe extends React.Component {
   static propTypes = {
@@ -13,76 +14,52 @@ class Wireframe extends React.Component {
     _.getOr(defaultValue, ['options', path], this.props.$block)
 
 
-  collectionItem = ({index, children, className}) => {
-    const {components: {Image}, style} = this.props
-
-    return (
-      <div className={classNames(style.item, className)}>
-        {children}
-        <Image bind={`collection[${index}].image`} />
-      </div>
-    )
-  }
-
   render() {
-    const {components: {Text, Button, Collection}, style} = this.props
+    const {
+      components: {Text, Button, Collection},
+      style,
+    } = this.props
 
     return (
       <section className={classNames(style.section, 'section')}>
         <div className={classNames(style.section__inner, 'section__inner')}>
-          <header className={classNames(style.section__header, 'section__header')}>
-            <Text bind="title" className={classNames(style.title, 'title', 'text-center')} tagName="h2" />
-            {this.getModifierValue('subtitle') && (
-              <Text bind="subtitle" className={classNames(style.subtitle, 'subtitle', 'text-center')} tagName="p" />
-            )}
-          </header>
-          <div className={classNames('section__content')}>
-            <Collection
-              className={style['items-wrapper']}
-              bind="collection"
-              Item={this.collectionItem}
+          <div className={style.wrapper}>
+            <Text
+              bind="title"
+              className={classNames(style.title, 'title')}
+              tagName="h2"
             />
+            {this.getModifierValue('subtitle') && (
+              <Text
+                bind="subtitle"
+                className={classNames(style.subtitle, 'subtitle')}
+                tagName="p"
+              />
+            )}
+            <Button className={style.btn} bind="button" />
           </div>
-          <footer className={classNames(style.section__footer, 'section__footer')}>
-            <div className="btns-group">
-              <div className="btns-group__inner">
-                <Button className="btns-group__item" bind="button" />
-                <Button className="btns-group__item" bind="button_additional" />
-              </div>
-            </div>
-          </footer>
+          <div className={style['decor-wrapper']}>
+            <DecoratorSvg />
+          </div>
         </div>
       </section>
     )
   }
 }
 
-Wireframe.components = _.pick(['Text', 'Button', 'Collection', 'Image'])($editor.components)
+Wireframe.components = _.pick(['Text', 'Button', 'Collection', 'Image', 'Background'])($editor.components)
 
 Wireframe.defaultContent = {
   title: {
-    content: 'Hello world',
+    content: 'Your digital agency for 1/10 of cost',
     type: 'blockTitle',
   },
   subtitle: {
-    content: 'Type here something',
+    content: 'Finally, the website you need at a price you can afford!',
     type: 'subtitle',
   },
   button: {
-    textValue: 'Request a quote',
-  },
-  button_additional: {
-    type: 'secondary',
-    textValue: 'Learn More',
-  },
-  collection: {
-    items: [
-      {
-        image: {
-          src: 'https://www.vms.ro/wp-content/uploads/2015/04/mobius-placeholder-2.png',
-        },
-      },
-    ],
+    textValue: 'Get started for free',
   },
 }
 
