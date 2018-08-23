9c37347ec2b1589aa00f4c6f15f37192feb32844
zhtbcgtw <ivanov.i@weblium.com>
2018-08-23 12:14:18 +0300

    enhancer-fix

--- src/enhancer/theme.js
+++ src/enhancer/theme.js
@@ -16,28 +16,7 @@ class ThemeWrapper extends React.Component {
 
   enabled = value => (this.checkEnableThemes() ? value : null)
 
-  components = evolve(
-    {
-      Text: mapProps(_.update('textClassNames', list =>
-        _.mergeWith(
-          (a, b) => classNames(a, b),
-          {
-            'text-block': this.enabled('ui-text-block'),
-            'text-content': this.enabled('ui-text-content'),
-            'text-left': this.enabled('ui-text-left'),
-            'text-center': this.enabled('ui-text-center'),
-            'text-right': this.enabled('ui-text-right'),
-          },
-          list,
-        ))),
-    },
-    this.props.components,
-  )
-
-  mapProps = _.pipe(
-    _.set('$theme.enabled', this.enabled),
-    _.set('components', this.components),
-  )
+  mapProps = _.set('$theme.enabled', this.enabled)
 
   render() {
     return this.props.children(this.mapProps)
