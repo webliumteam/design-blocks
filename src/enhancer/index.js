5ce46f8dd38b851b793269b8b708a550d3db5faa
Ivan Ivanov <i.ivanov@Ivans-MacBook-Pro.local>
2018-09-17 18:59:26 +0300

    DEV-129

--- src/enhancer/index.js
+++ src/enhancer/index.js
@@ -1,8 +1,10 @@
+import withFeatures from './features'
 import withOptions from './options'
 import withStyle from './style'
 import withTheme from './theme'
 
 export default _.compose(
+  withFeatures,
   withOptions,
   withStyle,
   withTheme,
